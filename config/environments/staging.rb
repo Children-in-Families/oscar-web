56bee0a22a5dfcde05237433a055a9892328eae3
Pirun SENG <sengpirun.rain@gmail.com>
2016-08-05 09:15:18 +0700

    Change sender email to CIF email for reset password instruction

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
   config.assets.prefix = "/assets"
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.action_mailer.asset_host = "http://cif-web-staging.rotati.com"
-  config.action_mailer.default_url_options = { host: 'cif-web.rotati.com' }
+  config.action_mailer.asset_host = "http://52.77.244.89/"
+  config.action_mailer.default_url_options = { host: '52.77.244.89' }
   config.assets.digest = true
   config.assets.enabled = true
   config.assets.initialize_on_precompile = true
