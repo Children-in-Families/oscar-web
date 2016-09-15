97260b1eb2ca0ce27f9a9d4f553d17f15f1202fc
Pirun SENG <sengpirun.rain@gmail.com>
2016-09-15 12:17:09 +0700

    Add default scope order descending

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -1,6 +1,8 @@
 class Changelog < ActiveRecord::Base
   belongs_to :user, counter_cache: true
 
+  default_scope { order(created_at: :desc) }
+
   validates :version, presence: true, uniqueness: true
   validates :description, presence: true
 end
