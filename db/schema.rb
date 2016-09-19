345e5e141d1c70c9e1431be8cfe4adc31e445bed
pitouly <pitouly189@gmail.com>
2016-09-19 08:59:30 +0700

    group filter domains

--- db/schema.rb
+++ db/schema.rb
@@ -644,17 +644,6 @@ ActiveRecord::Schema.define(version: 20160908074157) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "versions", force: :cascade do |t|
-    t.string   "item_type",  null: false
-    t.integer  "item_id",    null: false
-    t.string   "event",      null: false
-    t.string   "whodunnit"
-    t.text     "object"
-    t.datetime "created_at"
-  end
-
-  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
-
   add_foreign_key "assessments", "clients"
   add_foreign_key "case_contracts", "cases"
   add_foreign_key "case_notes", "clients"
