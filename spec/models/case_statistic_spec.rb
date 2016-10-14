9481e77e76e70b50e24375e2a063c3a6419b7f15
PirunSeng <sengpirun.rain@gmail.com>
2016-10-14 09:53:53 +0700

    Fixes report spec

--- spec/models/case_statistic_spec.rb
+++ spec/models/case_statistic_spec.rb
@@ -17,10 +17,8 @@ describe CaseStatistic, 'statistic data' do
 
   it 'returns current active cases of clients with single case' do
     data = [
-              {:name=>"Active EC", :data=>{3.months.ago.end_of_month.to_date=>2}},
-              {:name=>"Active FC", :data=>{2.months.ago.end_of_month.to_date=>2}},
-              {:name=>"Active KC", :data=>{1.month.ago.end_of_month.to_date=>2}}
-            ]
+      ["Jul-16", "Aug-16", "Sep-16"],[{:name=>"Active EC", :data=>[2, nil, nil]},{:name=>"Active FC", :data=>[nil, 2, nil]},{:name=>"Active KC", :data=>[nil, nil, 2]}]
+    ]
     
     statistic = CaseStatistic.new(Client.all)
     expect(statistic.statistic_data).to eq(data)
@@ -29,10 +27,10 @@ describe CaseStatistic, 'statistic data' do
   it 'returns current active cases of clients with multiple cases' do
     FactoryGirl.create(:case, case_type: 'FC', client: ec_client, start_date: 2.months.ago)
     data = [
-              {:name=>"Active FC", :data=>{2.months.ago.end_of_month.to_date=>3}},
-              {:name=>"Active KC", :data=>{1.month.ago.end_of_month.to_date=>2}}
+              ["Aug-16", "Sep-16"],[{:name=>"Active FC", :data=>[3, nil]},{:name=>"Active KC", :data=>[nil, 2]}]
             ]
     expect(CaseStatistic.new(Client.all).statistic_data).to eq(data)
   end
 
 end
+
--- spec/models/csi_statistic_spec.rb
+++ spec/models/csi_statistic_spec.rb
@@ -10,8 +10,7 @@ describe CsiStatistic, 'statistic data' do
   let!(:other_assessment_domain){ create(:assessment_domain, assessment: other_assessment, domain: domain, note: 'test', score: 3, created_at: Date.today)}
 
   it 'returns average domain score without filter' do
-    data = [{:name=>"1A", :data=>{:"Assessment (1)"=>4.0,
-              :"Assessment (2)"=>3.0}}]
+    data = [["Assessment (1)", "Assessment (2)"], [{:name=>"1A", :data=>[4.0, 3.0]}]]
     
     statistic = CsiStatistic.new(Client.all)
     expect(statistic.assessment_domain_score).to eq(data)
