91c211e089d066564c8881d774d2213d0eb2027d
Panh Phanith <panhphanith.kh@gmail.com>
2016-10-18 11:35:04 +0700

    validate presence of question group if able screening question has stage

--- spec/models/able_screening_question_spec.rb
+++ spec/models/able_screening_question_spec.rb
@@ -3,8 +3,19 @@ RSpec.describe AbleScreeningQuestion, type: :model do
   describe 'Validation' do
     it { is_expected.to validate_presence_of(:question) }
     it { is_expected.to validate_presence_of(:mode) }
-
     it { is_expected.to validate_inclusion_of(:mode).in_array(['yes_no', 'free_text']) }
+
+    it 'validates group when it has stage' do
+      stage = create(:stage)
+      question_group = create(:question_group)
+      able_screening_question = create(:able_screening_question, stage: stage, question_group: question_group)
+      expect(able_screening_question).to validate_presence_of(:question_group)
+    end
+
+    it 'not validate group when it has stage' do
+      able_screening_question = create(:able_screening_question)
+      expect(able_screening_question).not_to validate_presence_of(:question_group)
+    end
   end
 
   describe 'Association' do
