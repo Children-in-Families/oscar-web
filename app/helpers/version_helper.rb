c7e19e3a41eb58c3d042c13fb84a92da78cf833c
PirunSeng <sengpirun.rain@gmail.com>
2016-10-19 10:53:14 +0700

    Adds PaperTrail to Case

--- app/helpers/version_helper.rb
+++ app/helpers/version_helper.rb
@@ -12,10 +12,14 @@ module VersionHelper
     provinces        = ['birth_province_id', 'province_id']
     referral_sources = ['referral_source_id']
     users            = ['received_by_id', 'followed_up_by_id', 'user_id']
-    booleans         = ['has_been_in_orphanage', 'has_been_in_government_care', 'able', 'dependable_income']
+    booleans         = ['has_been_in_orphanage', 'has_been_in_government_care', 'able', 'dependable_income','family_preservation']
     titleizeTexts    = ['gender', 'state', 'family_type', 'roles']
     departments      = ['department_id']
     domain_groups    = ['domain_group_id']
+    partners         = ['partner_id']
+    families         = ['family_id']
+    clients          = ['client_id']
+    quantitative_types = ['quantitative_type_id']
 
     if titleizeTexts.include?(k)
       if val == both_val[0]
@@ -27,20 +31,28 @@ module VersionHelper
       val = val.strftime('%d %B, %Y')
     elsif val.class == ActiveSupport::TimeWithZone
       val = val.in_time_zone.strftime('%d %B, %Y %H:%M:%S')
-    elsif provinces.include?(k)
-      val = Province.find(val).name if val.present?
-    elsif referral_sources.include?(k)
-      val = ReferralSource.find(val).name if val.present?
-    elsif users.include?(k)
-      val = User.find(val).name if val.present?
+    elsif provinces.include?(k) && val.present?
+      val = Province.find(val).name
+    elsif referral_sources.include?(k) && val.present?
+      val = ReferralSource.find(val).name
+    elsif users.include?(k) && val.present?
+      val = User.find(val).name
     elsif booleans.include?(k)
       val = human_boolean(val)
-    elsif departments.include?(k)
-      val = Department.find(val).name if val.present?
-    elsif domain_groups.include?(k)
-      val = DomainGroup.find(val).name if val.present?
+    elsif departments.include?(k) && val.present?
+      val = Department.find(val).name
+    elsif domain_groups.include?(k) && val.present?
+      val = DomainGroup.find(val).name
     elsif is_description?(k) && has_html?(val)
       val = strip_tags(val)
+    elsif partners.include?(k) && val.present?
+      val = Partner.find(val).name
+    elsif families.include?(k) && val.present?
+      val = Family.find(val).name
+    elsif clients.include?(k) && val.present?
+      val = Client.find(val).name
+    elsif quantitative_types.include?(k) && val.present?
+      val = QuantitativeType.find(val).name
     end
     val
   end
@@ -54,7 +66,7 @@ module VersionHelper
   end
 
   def version_keys_skipable?(k)
-    k == 'tokens' || k == 'encrypted_password' || k == 'uid' 
+    k == 'tokens' || k == 'encrypted_password' || k == 'uid' || k == 'exited'
   end
 
   private
