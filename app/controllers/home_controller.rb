e1bb16388ad62252344051f0de791001c9996448
pitouly <pitouly189@gmail.com>
2016-10-21 11:19:52 +0700

    working on notify tasks and translate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,6 @@
 class HomeController < AdminController
   def index
     @dashboard       = Dashboard.new(current_user)
-    @overdue_tasks   = Task.incomplete.overdue.of_user(current_user)
-    @due_today_tasks = Task.incomplete.today.of_user(current_user)
-    assessments      = current_user.assessment_either_overdue_or_due_today
-    @overdue_assessments_count   = assessments[0]
-    @due_today_assessments_count = assessments[1]
-    @notification_count = (@overdue_tasks.count + @due_today_tasks.count + @overdue_assessments_count + @due_today_assessments_count)
   end
 
   def robots
