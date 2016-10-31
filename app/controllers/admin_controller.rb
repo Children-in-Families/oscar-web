17fb28b157fd71f9d1a0416587143ed010f01337
PirunSeng <sengpirun.rain@gmail.com>
2016-10-31 09:26:32 +0700

    Fixes UserNotification

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,16 @@ class AdminController < ApplicationController
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :notify_user
+
+  private
+
+  def notify_user
+    notification                 = UserNotification.new(current_user)
+    @overdue_tasks               = notification.overdue_tasks
+    @due_today_tasks             = notification.due_today_tasks
+    @overdue_assessments_count   = notification.overdue_assessments_count
+    @due_today_assessments_count = notification.due_today_assessments_count
+    @notification_count          = notification.notification_count
+  end
 end
