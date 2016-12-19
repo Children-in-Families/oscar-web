bdceb3a8027d470b4c552f2204ba3ae309cd1409
PirunSeng <sengpirun.rain@gmail.com>
2016-12-19 08:42:35 +0700

    Adds ability spec

--- spec/features/ability_spec.rb
+++ spec/features/ability_spec.rb
@@ -1,17 +1,30 @@
 feature 'progress_note' do
   let!(:admin){ create(:user, roles: 'admin') }
+  let!(:ec_manager){ create(:user, roles: 'ec manager') }
   let!(:fc_manager){ create(:user, roles: 'fc manager') }
   let!(:kc_manager){ create(:user, roles: 'fc manager') }
-  let!(:client){ create(:client, able_state: Client::ABLE_STATES[0], user: fc_manager) }
-  let!(:other_client){ create(:client, able_state: Client::ABLE_STATES[0], user: kc_manager) }
-  let!(:progress_note){ create(:progress_note, client: client) }
-  let!(:other_progress_note){ create(:progress_note, client: other_client) }
+  let!(:ec_client){ create(:client, able_state: Client::ABLE_STATES[0], user: ec_manager) }
+  let!(:fc_client){ create(:client, able_state: Client::ABLE_STATES[0], user: fc_manager) }
+  let!(:kc_client){ create(:client, able_state: Client::ABLE_STATES[0], user: kc_manager) }
+  let!(:fc_progress_note){ create(:progress_note, client: fc_client) }
+  let!(:kc_progress_note){ create(:progress_note, client: kc_client) }
+
+  feature 'link on Client detail page' do
+    before do
+      login_as(ec_manager)
+      visit client_path(ec_client)
+    end
+
+    scenario 'is invisible logged in as EC Manager' do
+      expect(page).not_to have_link('Progress Note', client_progress_notes_path(ec_client))
+    end
+  end
 
   feature 'List' do
     feature 'logged in as Admin' do
       before do
         login_as(admin)
-        visit client_progress_notes_path(progress_note.client)
+        visit client_progress_notes_path(fc_progress_note.client)
       end
       scenario 'enabled add button' do
         expect(page).not_to have_css('.btn-add.disabled')
@@ -27,7 +40,7 @@ feature 'progress_note' do
     feature 'logged in as FC Manager' do
       before do
         login_as(fc_manager)
-        visit client_progress_notes_path(progress_note.client)
+        visit client_progress_notes_path(fc_progress_note.client)
       end
       scenario 'disabled add button' do
         expect(page).to have_css('.btn-add.disabled')
@@ -43,7 +56,7 @@ feature 'progress_note' do
     feature 'logged in as KC Manager' do
       before do
         login_as(kc_manager)
-        visit client_progress_notes_path(other_progress_note.client)
+        visit client_progress_notes_path(kc_progress_note.client)
       end
       scenario 'disabled add button' do
         expect(page).to have_css('.btn-add.disabled')
