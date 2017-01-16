bb360db6d7ca7750f2be98aff8e9017922f669d3
Choengteur Chhoy <choengteurchhoy@Choengteurs-MacBook-Pro.local>
2017-01-16 11:23:07 +0700

    filter users in active tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,17 @@
 class TasksController < AdminController
   def index
     @tasks = current_user.admin? ? Task.incomplete.filter(params) : Task.incomplete.of_user(current_user)
-    @users = User.order(:first_name, :last_name)
+    @users = get_user
     @user  = User.find(params[:user_id]) if params[:user_id]
+  end
+
+	private
+
+  def get_user
+  	if current_user.admin?
+  		User.order(:first_name, :last_name)
+  	else
+  		User.in_department(current_user.department_id)
+  	end
   end
 end
