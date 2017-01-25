f592cc0344a3f54f796855827ff1e5f42d822bc5
PirunSeng <sengpirun.rain@gmail.com>
2017-01-25 17:22:16 +0700

    Style checkbox and radio buttons

--- app/validators/custom_field_numericality_validator.rb
+++ app/validators/custom_field_numericality_validator.rb
@@ -5,7 +5,7 @@ class CustomFieldNumericalityValidator < ActiveModel::Validator
   end
 
   def validate
-    CustomField.find_by(entity_name: @record.class.name).field_objs.each do |field|
+    CustomField.find_by(entity_name: @record.class.name).try(:field_objs).try(:each) do |field|
       if field['type'] == 'number' && (field['min'].present? || field['max'].present?)
         if @record.properties_objs[field['name']].to_f < field['min'].to_f
           @record.errors.add(field['name'], "can't be lower then #{field['min']}")
