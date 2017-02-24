5d9ac7c54082f777add5a961c3cf38b5712d5af5
Choengteur <choengteurchhoy@gmail.com>
2017-02-24 18:20:07 +0700

    feature multi client_custom_field for each custom field

--- app/controllers/client_custom_fields_controller.rb
+++ app/controllers/client_custom_fields_controller.rb
@@ -1,7 +1,9 @@
 class ClientCustomFieldsController < AdminController
   load_and_authorize_resource
-  
-  before_action :set_client, :set_custom_field
+
+  before_action :set_client
+  before_action :set_custom_field, only: [:new, :index, :destroy]
+  before_action :set_client_custom_field, only: [:edit, :show, :destroy]
 
   def edit
   end
@@ -15,6 +17,20 @@ class ClientCustomFieldsController < AdminController
   end
 
   def show
+  end
+
+  def new
+    @client_custom_field = ClientCustomField.create(custom_field: @custom_field, client: @client)
+    redirect_to edit_client_client_custom_field_path(@client, @client_custom_field), notice: 'Succesfully create new form'
+  end
+
+  def index
+    @client_custom_field = @client.client_custom_fields.where(custom_field: @custom_field).order(created_at: :desc).page(params[:page]).per(4)
+  end
+
+  def destroy
+    @client_custom_field.destroy
+    redirect_to client_client_custom_fields_path(@client, custom_field_id: @custom_field.id), notice: t('.successfully_deleted')
   end
 
   private
@@ -36,6 +52,10 @@ class ClientCustomFieldsController < AdminController
     end
 
     def set_custom_field
+      @custom_field = CustomField.find(params['custom_field_id'])
+    end
+
+    def set_client_custom_field
       @client_custom_field = @client.client_custom_fields.find(params[:id])
     end
 end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -129,7 +129,7 @@ class ClientsController < AdminController
       collection.organization = value
     end
   end
-  
+
   private
 
   def find_client
@@ -137,7 +137,7 @@ class ClientsController < AdminController
   end
 
   def set_custom_field
-    @custom_field = CustomField.find_by(entity_name: 'Client')
+    @custom_field = CustomField.find_by(entity_type: 'Client')
   end
 
   def client_params
