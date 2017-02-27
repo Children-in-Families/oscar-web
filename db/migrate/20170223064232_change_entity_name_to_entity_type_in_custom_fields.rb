fca46bd51a4ae099b3235235211798ff1407675b
ks-nireak <kimsrour.sophalnireak@gmail.com>
2017-02-27 09:56:05 +0700

    fix custom field rename migration

--- db/migrate/20170223064232_change_entity_name_to_entity_type_in_custom_fields.rb
+++ db/migrate/20170223064232_change_entity_name_to_entity_type_in_custom_fields.rb
@@ -1,5 +1,5 @@
 class ChangeEntityNameToEntityTypeInCustomFields < ActiveRecord::Migration
   def change
-    rename_column :custom_fields, :entity_type, :entity_type
+    rename_column :custom_fields, :entity_name, :entity_type
   end
 end
