6884b5da3ec1e7e0269d280175436237e02182cf
ks-nireak <kimsrour.sophalnireak@gmail.com>
2017-02-27 11:21:54 +0700

    family custom field index and create multiple family_custom_fields

--- app/controllers/family_custom_fields_controller.rb
+++ app/controllers/family_custom_fields_controller.rb
@@ -1,20 +1,36 @@
 class FamilyCustomFieldsController < AdminController
   load_and_authorize_resource
-  
-  before_action :set_family, :set_custom_field
+
+  before_action :set_family
+  before_action :set_custom_field, only: [:new, :index, :destroy]
+  before_action :set_family_custom_field, only: [:edit, :show, :destroy]
 
   def edit
   end
 
   def update
     if @family_custom_field.update(merged_custom_field_params)
-      redirect_to family_family_custom_field_path(@family, @family_custom_field), notice: 'Succesfully save information'
+      redirect_to family_family_custom_field_path(@family, @family_custom_field), notice: t('.successfully_created')
     else
       render :edit
     end
   end
 
   def show
+  end
+
+  def new
+    @family_custom_field = FamilyCustomField.new(custom_field: @custom_field, family: @family)
+    @family_custom_field.save
+  end
+
+  def index
+    @family_custom_field = @family.family_custom_fields.where(custom_field: @custom_field).order(created_at: :desc).page(params[:page]).per(4)
+  end
+
+  def destroy
+    @family_custom_field.destroy
+    redirect_to family_family_custom_fields_path(@family, custom_field_id: @custom_field.id), notice: t('.successfully_deleted')
   end
 
   private
@@ -36,6 +52,10 @@ class FamilyCustomFieldsController < AdminController
     end
 
     def set_custom_field
+      @custom_field = CustomField.find(params['custom_field_id'])
+    end
+
+    def set_family_custom_field
       @family_custom_field = @family.family_custom_fields.find(params[:id])
     end
 end
