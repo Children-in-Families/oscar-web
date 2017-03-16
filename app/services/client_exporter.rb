4f69fb4a0f7b22c05bf984f7eb4f64db843b5217
pitouly <pitouly189@gmail.com>
2017-03-16 13:52:38 +0700

    Export advanced search xls. Add translation on select box and header.

--- app/services/client_exporter.rb
+++ app/services/client_exporter.rb
@@ -1,6 +1,8 @@
 class ClientExporter
+  extend AdvancedSearchHelper
+
   def self.to_xls(resources)
-    headers = format_header(resources.first.attributes.keys)
+    headers = rearrange_header(resources.first.attributes.keys)
     book = Spreadsheet::Workbook.new
     book.create_worksheet
     book.worksheet(0).insert_row(0, headers)
@@ -16,12 +18,17 @@ class ClientExporter
   end
 
   private
-  def self.format_header(columns)
-    new_column = ['ID', 'Name']
+
+  def self.rearrange_header(columns)
+    new_column = []
+    new_column.push format_header('slug')
+    new_column.push format_header('first_name')
+    
     columns.each do |col|
       next if col == 'id' || col == 'slug' || col =='first_name'
-      new_column.push col.humanize
+      new_column.push format_header(col)
     end
+    
     new_column
   end
 
@@ -36,6 +43,17 @@ class ClientExporter
         id_values.push v
       elsif k == 'first_name'
         name_values.push v
+      elsif k == 'has_been_in_orphanage' || k == 'has_been_in_government_care'
+        selected_values.push(v ? 'Yes' : 'No')
+      elsif ['birth_province_id', 'province_id'].include?(k)
+        selected_values.push Province.find_by(id: v).try(:name)
+      elsif ['referral_source_id'].include?(k)
+        selected_values.push ReferralSource.find_by(id: v).try(:name)
+      elsif ['received_by_id', 'followed_up_by_id', 'user_id'].include?(k)
+        selected_values.push(User.find_by(id: v).try(:name) || '')
+      elsif k == 'age'
+        client = Client.find_by(date_of_birth: v)
+        selected_values.push "#{client.age_as_years} #{'year'.pluralize(client.age_as_years)} #{client.age_extra_months} #{'month'.pluralize(client.age_extra_months)}" if client.present?
       else
         selected_values.push v.to_s
       end
