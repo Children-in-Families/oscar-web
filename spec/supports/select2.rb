3db735720a29744c7e808a5e7dd1ce84afbe5c10
pitouly <pitouly189@gmail.com>
2017-03-20 11:31:07 +0700

    spec on BaseFilter, CleintAdvancedFilterFields and AdvancedFilterTypes

--- spec/supports/select2.rb
+++ spec/supports/select2.rb
@@ -1,24 +1,10 @@
 module Select2
-  def select2_select(options={})
-    within options[:from] do
-      find('div.select2-container').click
-      find('span.select2-chosen').set(options[:with])
-    end
+  def select2_select(value, element_selector)
+    select2_container = first("#{element_selector}")
+    select2_container.find(".select2-choice").click
 
-    # has_css?('ul.select2-results div', text: options[:with])
-    # within '#select2-drop .select2-results' do
-    #   find("div", text: options[:with]).click
-    # end
-    #
-    # all('ul.select2-results__options li', text: options[:with]).each do |e|
-    #  e.click if e.text.include?(options[:with])
-    # end
-  end
-
-  def select2_search(options={})
-    within options[:from] do
-      find('span.select2.select2-container').click
-    end
-    find('input.select2-search__field').set(options[:with])
+    find(:xpath, "//body").find("input.select2-input").set(value)
+    drop_container = ".select2-results"
+    find(:xpath, "//body").find("#{drop_container} li", text: value).click
   end
 end
