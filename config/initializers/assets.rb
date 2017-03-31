04f18545fae25f6d7880e0f3aa1defbe1725617d
Someth Victory <victory.someth@gmail.com>
2017-03-31 11:26:42 +0700

    add rich text editor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
 # Rich text editor for the Domain
-Rails.application.config.assets.precompile += %w(ckeditor/* jquery.nicescroll.js animate.css toastr.min.css custom.css green.png)
+Rails.application.config.assets.precompile += %w(jquery.nicescroll.js animate.css toastr.min.css custom.css green.png)
