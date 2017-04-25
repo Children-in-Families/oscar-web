fd6fce7e39ebb590f179d51e23331577f2049aca
ks-nireak <kimsrour.sophalnireak@gmail.com>
2017-04-25 15:02:05 +0700

    validate on polymorphic custom field property.

--- app/models/custom_field_property.rb
+++ app/models/custom_field_property.rb
@@ -4,4 +4,12 @@ class CustomFieldProperty < ActiveRecord::Base
 
   scope :by_custom_field,                   -> (value) { where(custom_field:  value) }
   scope :most_recents,                      ->         { order('created_at desc') }
+
+  validates :custom_field_id, presence: true
+
+  validate do |obj|
+    CustomFieldPresentValidator.new(obj).validate
+    CustomFieldNumericalityValidator.new(obj).validate
+    CustomFieldEmailValidator.new(obj).validate
+  end
 end
--- app/validators/custom_field_present_validator.rb
+++ app/validators/custom_field_present_validator.rb
@@ -6,7 +6,7 @@ class CustomFieldPresentValidator < ActiveModel::Validator
   def validate
     return unless @record.properties
     @record.custom_field.field_objs.each do |field|
-      if field['required'] && (@record.properties_objs[field['label']].blank? || @record.properties_objs[field['label']][0].blank?)
+      if field['required'] && (@record.properties[field['label']].blank? || @record.properties[field['label']][0].blank?)
         @record.errors.add(field['label'], I18n.t('cannot_be_blank'))
       end
     end
