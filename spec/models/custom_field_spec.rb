d95dd52fe253e3e0b543ff3db6f32e426271bbad
Choengteur <Choengteurchhoy@gmail.com>
2017-04-25 11:02:23 +0700

    add spec validation custom form

--- spec/models/custom_field_spec.rb
+++ spec/models/custom_field_spec.rb
@@ -13,6 +13,7 @@ describe CustomField, 'validations' do
   it { is_expected.to validate_presence_of(:entity_type) }
   it { is_expected.to validate_presence_of(:form_title) }
   it { is_expected.to validate_uniqueness_of(:form_title).case_insensitive.scoped_to(:entity_type) }
+  it { is_expected.to validate_inclusion_of(:entity_type).in_array(CustomField::ENTITY_TYPES) }
   it 'validates presence of fields' do
     custom_field = CustomField.new
     custom_field.save
@@ -28,13 +29,25 @@ describe CustomField, 'validations' do
     custom_field.save
     expect(custom_field.errors[:time_of_frequency]).to include('must be an integer')
   end
+  it 'validates field label must be uniq' do
+    custom_field_unit = CustomField.new(fields: "[\r\n\t{\r\n\t\t\"type\": \"text\",\r\n\t\t\"label\": \"Text Field\",\r\n\t\t\"subtype\": \"text\",\r\n\t\t\"className\": \"form-control\",\r\n\t\t\"name\": \"text-1493090904302\"\r\n\t},\r\n\t{\r\n\t\t\"type\": \"text\",\r\n\t\t\"label\": \"Text Field\",\r\n\t\t\"subtype\": \"text\",\r\n\t\t\"className\": \"form-control\",\r\n\t\t\"name\": \"text-1493090904547\"\r\n\t}\r\n]")
+    custom_field_unit.save
+    expect(custom_field_unit.errors[:fields]).to include('field label must be uniq')
+  end
+  it "validates remove fields if fields haven't used" do
+    client = Client.create(given_name: 'Jonh')
+    custom_field_textarea = CustomField.create(entity_type: 'Client', form_title: 'Spec', fields: "[\r\n\t{\r\n\t\t\"type\": \"textarea\",\r\n\t\t\"label\": \"Text Area\",\r\n\t\t\"className\": \"form-control\",\r\n\t\t\"name\": \"textarea-1493086701693\"\r\n\t}\r\n]")
+    client_custom_field = client.client_custom_fields.create(properties: '{"Text Area":"Spec"}', custom_field_id: custom_field_textarea.id)
+    custom_field_textarea.update(fields: '[]')
+    expect(custom_field_textarea.errors[:fields]).to include('Text Area cannot be removed ')
+  end
 end
 
 describe CustomField, 'scopes' do
   context 'find custom forms by form title' do
-    let!(:custom_field) { create(:custom_field, form_title: 'Health Record') }
-    let!(:other_custom_field) { create(:custom_field, form_title: 'Prison Record') }
-    subject{ CustomField.by_form_title('health record') }
+    let!(:custom_field) { create(:custom_field, entity_type: 'Client', form_title: 'Health Record') }
+    let!(:other_custom_field) { create(:custom_field, entity_type: 'Client', form_title: 'Prison Record') }
+    subject { CustomField.by_form_title('health record') }
     it 'should include custom form like the given form title' do
       is_expected.to include(custom_field)
     end
@@ -46,7 +59,7 @@ describe CustomField, 'scopes' do
   context 'client forms' do
     let!(:custom_field) { create(:custom_field, entity_type: 'Client') }
     let!(:other_custom_field) { create(:custom_field, entity_type: 'Partner') }
-    subject{ CustomField.client_forms }
+    subject { CustomField.client_forms }
     it 'should include forms with client entity type' do
       is_expected.to include(custom_field)
     end
@@ -58,7 +71,7 @@ describe CustomField, 'scopes' do
   context 'family forms' do
     let!(:custom_field) { create(:custom_field, form_title: 'Health Record', entity_type: 'Family') }
     let!(:other_custom_field) { create(:custom_field, form_title: 'Prison Record', entity_type: 'Partner') }
-    subject{ CustomField.family_forms }
+    subject { CustomField.family_forms }
     it 'should include forms with family entity type' do
       is_expected.to include(custom_field)
     end
@@ -70,7 +83,7 @@ describe CustomField, 'scopes' do
   context 'partner forms' do
     let!(:custom_field) { create(:custom_field, form_title: 'Health Record', entity_type: 'Partner') }
     let!(:other_custom_field) { create(:custom_field, form_title: 'Prison Record', entity_type: 'Client') }
-    subject{ CustomField.partner_forms }
+    subject { CustomField.partner_forms }
     it 'should include forms with parnter entity type' do
       is_expected.to include(custom_field)
     end
@@ -82,7 +95,7 @@ describe CustomField, 'scopes' do
   context 'user forms' do
     let!(:custom_field) { create(:custom_field, form_title: 'Health Record', entity_type: 'User') }
     let!(:other_custom_field) { create(:custom_field, form_title: 'Prison Record', entity_type: 'Partner') }
-    subject{ CustomField.user_forms }
+    subject { CustomField.user_forms }
     it 'should include forms with user entity type' do
       is_expected.to include(custom_field)
     end
@@ -102,8 +115,8 @@ describe CustomField, 'CONSTANTS' do
 end
 
 describe CustomField, 'callbacks' do
-  let!(:frequency_custom_field){ create(:custom_field, frequency: 'Day', time_of_frequency: 12) }
-  let!(:no_frequency_custom_field){ create(:custom_field, frequency: '', form_title: 'Health Care') }
+  let!(:frequency_custom_field) { create(:custom_field, entity_type: 'Client', frequency: 'Day', time_of_frequency: 12) }
+  let!(:no_frequency_custom_field) { create(:custom_field, entity_type: 'Client', frequency: '', form_title: 'Health Care') }
   context 'set_time_of_frequency' do
     it 'any frequency is chosen then it should have time of frequency' do
       expect(frequency_custom_field.time_of_frequency).to eq(12)
