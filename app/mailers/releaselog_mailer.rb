f6815e00a0f08f6d770d03294e70f5d967a7a284
PirunSeng <sengpirun.rain@gmail.com>
2017-05-10 14:34:10 +0700

    Removes admin mailer of new release

--- app/mailers/releaselog_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ReleaselogMailer < ActionMailer::Base
-  default from: 'info@cambodianfamilies.com'
-
-  def notify_admins(admin_emails)
-    mail(to: admin_emails, subject: 'There is a new release today')
-  end
-end
