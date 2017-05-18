607529a11d4296f12fb5ecf5bf55bc83c25a3bdc
Choengteur <Choengteurchhoy@gmail.com>
2017-05-18 14:18:46 +0700

    import Mother's heart cambodia clients

--- app/services/mhc_importer.rb
+++ app/services/mhc_importer.rb
@@ -26,5 +26,36 @@ module MhcImporter
         User.create(first_name: first_name, last_name: last_name, email: email, password: "#{password}", roles: role)
       end
     end
+
+    def clients
+      ((workbook.first_row + 1)..workbook.last_row).each do |row|
+        user_first_name       = workbook.row(row)[headers['Case Worker ID']]
+        user                  = User.find_by(first_name: user_first_name)
+        given_name            = workbook.row(row)[headers['*First Name']]
+        family_name           = workbook.row(row)[headers['*Last Name']]
+        birth_province        = workbook.row(row)[headers['*Birth Province']]
+        birth_province_id     = birth_province.present? ? Province.find_by(name: birth_province).id : nil
+        current_province      = workbook.row(row)[headers['*Current Province']]
+        current_province_id   = current_province.present? ? Province.find_by(name: current_province).id : nil
+        gender                = case workbook.row(row)[headers['*Gender']]
+                                when 'Male'   then 'male'
+                                when 'Female' then 'female'
+                                end
+        relevant_referral_information = workbook.row(row)[headers['Relevant Referral Information']]
+        user_id          = user.id
+
+        client = Client.new(
+          given_name: given_name,
+          family_name: family_name,
+          gender: gender,
+          birth_province_id: birth_province_id,
+          state: 'accepted',
+          relevant_referral_information: relevant_referral_information,
+          province_id: current_province_id,
+          user_id: user_id
+        )
+        client.save
+      end
+    end
   end
 end
--- lib/tasks/import_mhc.rake
+++ lib/tasks/import_mhc.rake
@@ -1,10 +1,8 @@
 namespace :mhc do
   desc "Import Mother's Heart Cambodia users"
   task import: :environment do
-    org = Organization.create_and_build_tanent(short_name: 'mhc', full_name: "Mother's Heart Cambodia", logo: File.open(Rails.root.join('app/assets/images/MH-Logo.png')))
-    Organization.switch_to org.short_name
-
-    import = MhcImporter::Import.new('Case Workers')
-    import.users
+    Organization.switch_to 'mhc'
+    import = MhcImporter::Import.new('Clients')
+    import.clients
   end
 end
