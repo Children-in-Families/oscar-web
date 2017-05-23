b6449ed7707852f583ff107b9f85e940c4aa5cb2
Choengteur <Choengteurchhoy@gmail.com>
2017-05-23 16:09:11 +0700

    change query client_custom_form_options in advance search

--- app/classes/advanced_searches/client_fields.rb
+++ app/classes/advanced_searches/client_fields.rb
@@ -52,7 +52,7 @@ module AdvancedSearches
     end
 
     def client_custom_form_options
-      CustomField.joins(:custom_field_properties).where(entity_type: 'Client').uniq.map{|c| { c.id.to_s => c.form_title}}
+      CustomField.joins(:custom_field_properties).client_forms.uniq.map{ |c| { c.id.to_s => c.form_title }}
     end
 
     def client_status
