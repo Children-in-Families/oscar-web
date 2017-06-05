b277af1274d8c0430a898a2c3fd891c901b434d2
bunlong <obunlong@gmail.com>
2017-06-05 15:05:29 +0700

    add client nested association

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -35,7 +35,10 @@ class ClientSerializer < ActiveModel::Serializer
   end
 
   def additional_form
-    ActiveModel::ArraySerializer.new(object.custom_fields.uniq, each_serializer: CustomFieldSerializer)
+    custom_fields = object.custom_fields.uniq.sort_by(&:form_title)
+    custom_fields.map do |custom_field|
+      custom_field.as_json.merge(custom_field_properties: custom_field.custom_field_properties.where(custom_formable_id: object.id).as_json)
+    end
   end
 
   def tasks
@@ -52,5 +55,27 @@ class ClientSerializer < ActiveModel::Serializer
 
   def kinship_care
     CaseSerializer.new(object.cases.active.latest_kinship).serializable_hash
+  end
+
+  def assessments
+    object.assessments.map do |assessment|
+      formatted_assessment_domain = assessment.assessment_domains.map do |ad|
+        ad.as_json.merge(domain: ad.domain.as_json(only: [:name, :identity]))
+      end
+      assessment.as_json.merge(assessment_domain: formatted_assessment_domain)
+    end
+  end
+
+  def case_notes
+    object.case_notes.most_recents.map do |case_note|
+      formatted_case_note_domain_group = case_note.case_note_domain_groups.map do |cdg|
+        domain_scores = cdg.domain_group.domains.map do |domain|
+          ad = domain.assessment_domains.find_by(assessment_id: case_note.assessment_id)
+          ad.score
+        end
+        cdg.as_json.merge(domain_group_identities: cdg.domain_group.domain_identities, domain_scores: domain_scores, completed_tasks: cdg.completed_tasks)
+      end
+      case_note.as_json.merge(case_note_domain_group: formatted_case_note_domain_group)
+    end
   end
 end
