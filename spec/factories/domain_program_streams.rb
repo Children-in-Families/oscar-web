eaeb9d88e2b22ff6254ae683a23d5782ec1d3108
sophal <caili.cl29@gmail.com>
2017-06-05 15:16:19 +0700

    Admin program stream feature spec (#279)
    * working on program stream admin UI
    
    * working on program stream save data to hidden field
    
    * working on getting value from input
    
    * admin update program stream
    
    * create update and delete program stream
    
    * validate enrollment tracking and exit program uniqueness
    
    * working on program stream show page
    
    * workin on admin program stream show page
    
    * working on admin program stream show page inputs css and disable
    
    * working on enrollment
    
    * finished end user enrollment form
    
    * finished program stream exit form
    
    * finished program stream tracking form
    
    * worked on validate client program stream
    
    * view report for end-user program stream
    
    * view report on end-user program stream
    
    * add link form enrollment to program stream
    
    * worked on validation end use program stream
    
    * finished program stream model spec
    
    * working on modal spec
    
    * finished client enrollment modal spec
    
    * finished tracking and leave program model spec
    
    * WIP
    
    * resovle conflict
    
    * finished program stream feature spec

---
+++ spec/factories/domain_program_streams.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :domain_program_stream do
+    association :domain, factory: :domain
+    association :program_stream, factory: :program_stream
+  end
+end
--- spec/supports/select2.rb
+++ spec/supports/select2.rb
@@ -1,8 +1,9 @@
 module Select2
   def select2_select(value, element_selector)
     select2_container = first("#{element_selector}")
+    sleep 1
     select2_container.find(".select2-choice").click
-
+    sleep 1
     drop_container = ".select2-results"
     find(:xpath, "//body").find("#{drop_container} li", text: value).click
   end
