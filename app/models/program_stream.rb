42dd075b30b55e30532dbe2dc333f23ff719c1f8
sophal <caili.cl29@gmail.com>
2017-06-06 17:36:01 +0700

    WIP

--- app/models/program_stream.rb
+++ app/models/program_stream.rb
@@ -12,6 +12,7 @@ class ProgramStream < ActiveRecord::Base
   validates :name, :rules, :enrollment, :tracking, :exit_program, presence: true
   validates :name, uniqueness: true
   validate  :form_builder_field_uniqueness
+  validate  :validate_remove_field, if: -> { id.present? }
  
   def form_builder_field_uniqueness
     errors_massage = []
@@ -24,7 +25,56 @@ class ProgramStream < ActiveRecord::Base
     errors_massage
   end
 
+  def validate_remove_field
+    FORM_BUILDER_FIELDS.each do |field|
+      next unless send "#{ field }_changed?" 
+      column_change = send "#{ field }_change"
+      error_fields = []
+
+      if field == 'enrollment'
+        client_enrollment_properties.each do |property|
+          field_remove = column_change.first - column_change.last
+          field_remove.map{ |f| error_fields << f['label'] if property[f['label']].present? }
+        end
+        break unless error_fields.present?
+        error_message = "#{error_fields.join(', ')} #{I18n.t('cannot_remove_or_update')}"
+        errors.add(:enrollment, "#{error_message} ")
+      elsif field == 'tracking'
+        tracking_properties.each do |property|
+          field_remove = column_change.first - column_change.last
+          field_remove.map{ |f| error_fields << f['label'] if property[f['label']].present? }
+        end
+        break unless error_fields.present?
+        error_message = "#{error_fields.join(', ')} #{I18n.t('cannot_remove_or_update')}"
+        errors.add(:tracking, "#{error_message} ")
+      elsif field == 'exit_program'
+        leave_program_properties.each do |property|
+          field_remove = column_change.first - column_change.last
+          field_remove.map{ |f| error_fields << f['label'] if property[f['label']].present? }
+        end
+        break unless error_fields.present?
+        error_message = "#{error_fields.join(', ')} #{I18n.t('cannot_remove_or_update')}"
+        errors.add(:exit_program, "#{error_message} ")
+      end    
+    end
+    errors
+  end
+
   def last_enrollment
     client_enrollments.last
+  end
+
+  private
+
+  def client_enrollment_properties
+    client_enrollments.pluck(:properties).select(&:present?)
+  end
+
+  def tracking_properties
+    trackings.pluck(:properties).select(&:present?)
+  end
+
+  def leave_program_properties
+    leave_programs.pluck(:properties).select(&:present?)
   end
 end
