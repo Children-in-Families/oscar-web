ffd759f3a586a916815688d36ea2735ec8dab347
bunlong <obunlong@gmail.com>
2017-06-09 16:04:09 +0700

    add crud function for families

--- config/routes.rb
+++ config/routes.rb
@@ -174,6 +174,7 @@ Rails.application.routes.draw do
     namespace :v1, default: { format: :json } do
       resources :domain_groups, only: [:index]
       resources :users, only: [:update]
+      resources :families, only: [:index, :create, :update]
       resources :clients, except: [:edit, :new] do
         get :compare, on: :collection
         resources :assessments, only: [:create]
