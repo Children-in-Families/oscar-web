ab6ab6f1c495ad9d241c823559c77a16e2e77b5f
Bunlong Oung <obunlong@gmail.com>
2017-06-15 15:10:10 +0700

    api for program stream (#316)

---
+++ app/controllers/api/v1/client_enrollment_trackings_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  module V1
+    class ClientEnrollmentTrackingsController < Api::V1::BaseApiController
+
+      before_action :find_client, :find_enrollment
+      before_action :find_client_enrollment_tracking, only: :update
+
+      def create
+        @client_enrollment_tracking = @enrollment.client_enrollment_trackings.new(client_enrollment_tracking_params)
+
+        if @client_enrollment_tracking.save
+          render json: @client_enrollment_tracking
+        else
+          render json: @client_enrollment_tracking.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @client_enrollment_tracking.update_attributes(client_enrollment_tracking_params)
+          render json: @client_enrollment_tracking
+        else
+          render json: @client_enrollment_tracking.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def client_enrollment_tracking_params
+        params.require(:client_enrollment_tracking).permit({}).merge(properties: params[:client_enrollment_tracking][:properties], tracking_id: params[:tracking_id])
+      end
+
+      def find_client
+        @client = Client.accessible_by(current_ability).friendly.find params[:client_id]
+      end
+
+      def find_enrollment
+        @enrollment = @client.client_enrollments.find params[:client_enrollment_id]
+      end
+
+      def find_client_enrollment_tracking
+        @client_enrollment_tracking = @enrollment.client_enrollment_trackings.find params[:id]
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -186,7 +186,13 @@ Rails.application.routes.draw do
         scope module: 'client_tasks' do
           resources :tasks, only: [:create, :update, :destroy]
         end
+
+        resources :client_enrollments, only: [:create, :update] do
+          resources :client_enrollment_trackings, only: [:create, :update]
+          resources :leave_programs, only: [:create, :update]
+        end
       end
+      resources :program_streams, only: [:index]
       resources :provinces, only: [:index]
       resources :donors, only: [:index]
       resources :agencies, only: [:index]
