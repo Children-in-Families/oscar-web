b9872ff79032faf9cf9433c18ea2243eafc8c1c9
Bunlong Oung <obunlong@gmail.com>
2017-06-15 15:05:33 +0700

    add all association object (#307)
    * add all association object
    
    * add email to users

---
+++ spec/requests/api/v1/domains_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+RSpec.describe Api::V1::DomainGroupsController, type: :request do
+  let(:user) { create(:user) }
+  let!(:domains) { create_list(:domain, 12) }
+
+  describe 'GET #index' do
+    context 'when user not loged in' do
+      before do
+        get '/api/v1/domains'
+      end
+
+      it 'should be return status 401' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when user loged in' do
+      before do
+        sign_in(user)
+        get '/api/v1/domains', @auth_headers
+      end
+
+      it 'should be return status 200' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'should be returns the domains with the correct data' do
+        expect(json['domains'].size).to eq 12
+      end
+    end
+  end
+end
---
+++ spec/serializers/province_serializer_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe ProvinceSerializer, type: :serializer do
+  let!(:province) { create(:province)}
+  let(:serializer) { ProvinceSerializer.new(province).to_json }
+
+  it 'should be have attribute provinces as root path' do
+    expect(serializer).to have_json_size(1)
+    expect(serializer).to have_json_type(Object).at_path('province')
+  end
+
+  it 'should be have attribute id' do
+    expect(serializer).to have_json_path('province/id')
+    expect(serializer).to have_json_type(Integer).at_path('province/id')
+  end
+
+  it 'should be have attribute name' do
+    expect(serializer).to have_json_path('province/name')
+    expect(serializer).to have_json_type(String).at_path('province/name')
+  end
+
+  it 'should be have attribute description' do
+    expect(serializer).to have_json_path('province/description')
+    expect(serializer).to have_json_type(String).at_path('province/description')
+  end
+end
