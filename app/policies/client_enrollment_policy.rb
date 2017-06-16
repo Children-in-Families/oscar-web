4e898f753fc6cfb3c1edd357b00af49ecee24872
sophal <caili.cl29@gmail.com>
2017-06-16 16:05:26 +0700

    fixed bug when user copy create link to recreate enrollment

--- app/policies/client_enrollment_policy.rb
+++ app/policies/client_enrollment_policy.rb
@@ -4,11 +4,28 @@ class ClientEnrollmentPolicy < ApplicationPolicy
   end
 
   def create?
-    return true unless record.program_stream.quantity.present?
-    client_ids.size < record.program_stream.quantity
+    if get_client_enrollments.empty? || client_enrollment_status == 'Exited'
+      return true unless record.program_stream.quantity.present?
+      client_ids.size < record.program_stream.quantity
+    else
+      return false
+    end
   end
 
   def client_ids
     ClientEnrollment.active.where(program_stream_id: record.program_stream).pluck(:client_id).uniq
+  end
+
+  private
+
+  def client_enrollment_status
+    client_id = record.client_id
+    program_stream_id = record.program_stream_id
+
+    ClientEnrollment.where(client_id: client_id, program_stream_id: program_stream_id).order(:created_at).last.status
+  end
+
+  def get_client_enrollments
+    record.program_stream.client_enrollments
   end
 end
