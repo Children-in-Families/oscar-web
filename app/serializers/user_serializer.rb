d65986555f9c4b92b027d00537b7f34052342905
Bunlong Oung <obunlong@gmail.com>
2017-06-19 09:10:00 +0700

    improve user task api (#321)

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,27 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :roles, :mobile, :date_of_birth, :archived, :admin, :manager_id, :pin_number, :overdue_tasks, :today_tasks, :upcoming_tasks
+  attributes :id, :first_name, :last_name, :email, :roles, :mobile, :date_of_birth, :archived, :admin, :manager_id, :pin_number, :clients
 
-  def overdue_tasks
-    overdue_tasks = Task.incomplete.of_user(object).overdue.group_by{ |task| task.client }
-    overdue_tasks.map do |client, tasks|
-      serialized_tasks = ActiveModel::ArraySerializer.new(tasks, each_serializer: TaskSerializer)
-      { client: client.as_json(only: [:id, :given_name, :family_name, :local_given_name, :local_family_name]), tasks: serialized_tasks }
-    end
-  end
-
-  def today_tasks
-    today_tasks = Task.incomplete.of_user(object).today.group_by{ |task| task.client }
-    today_tasks.map do |client, tasks|
-      serialized_tasks = ActiveModel::ArraySerializer.new(tasks, each_serializer: TaskSerializer)
-      { client: client.as_json(only: [:id, :given_name, :family_name, :local_given_name, :local_family_name]), tasks: serialized_tasks }
-    end
-  end
-
-  def upcoming_tasks
-    upcoming_tasks = Task.incomplete.of_user(object).upcoming.group_by{ |task| task.client }
-    upcoming_tasks.map do |client, tasks|
-      serialized_tasks = ActiveModel::ArraySerializer.new(tasks, each_serializer: TaskSerializer)
-      { client: client.as_json(only: [:id, :given_name, :family_name, :local_given_name, :local_family_name]), tasks: serialized_tasks }
-    end
+  def clients
+    object.clients.map do |client|
+      incompleted_tasks = client.tasks.incomplete
+      next unless incompleted_tasks.present?
+      formatted_client = client.as_json(only: [:id, :given_name, :family_name, :local_given_name, :local_family_name])
+      formatted_client.merge(overdue: incompleted_tasks.overdue, today: incompleted_tasks.today, upcoming: incompleted_tasks.upcoming)
+    end.compact
   end
 end
