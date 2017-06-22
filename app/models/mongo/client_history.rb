8fa065f1fbb289fc675b715b94f2fb13929475bc
PirunSeng <sengpirun.rain@gmail.com>
2017-06-22 16:39:12 +0700

    Add ClientQuantitativeCaseHistory and move those histories to app/models/mongo/

--- app/models/mongo/client_history.rb
+++ app/models/mongo/client_history.rb
@@ -11,14 +11,17 @@ class ClientHistory
   embeds_many :client_family_histories
   embeds_many :case_client_histories
   embeds_many :client_custom_field_property_histories
+  embeds_many :client_quantitative_case_histories
 
   after_save :create_agency_client_history, if: 'object.key?("agency_ids")'
+  after_save :create_client_quantitative_case_history, if: 'object.key?("quantitative_case_ids")'
   after_save :create_case_client_history,   if: 'object.key?("case_ids")'
   after_save :create_client_family_history, if: 'object.key?("family_ids")'
   after_save :create_client_custom_field_property_history, if: 'object.key?("custom_field_property_ids")'
 
   def self.initial(client)
     attributes = client.attributes
+    attributes = attributes.merge('quantitative_case_ids' => client.quantitative_case_ids) if client.quantitative_case_ids.any?
     attributes = attributes.merge('agency_ids' => client.agency_ids) if client.agency_ids.any?
     attributes = attributes.merge('case_ids' => client.case_ids) if client.case_ids.any?
     attributes = attributes.merge('family_ids' => client.family_ids) if client.family_ids.any?
@@ -27,6 +30,13 @@ class ClientHistory
   end
 
   private
+
+  def create_client_quantitative_case_history
+    object['quantitative_case_ids'].each do |quantitative_case_id|
+      quantitative_case = QuantitativeCase.find_by(id: quantitative_case_id).try(:attributes)
+      client_quantitative_case_histories.create(object: quantitative_case)
+    end
+  end
 
   def create_agency_client_history
     object['agency_ids'].each do |agency_id|
