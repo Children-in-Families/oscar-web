73daa09e8f5a88e0fb232957277fade22bcb0a7f
Choengteur <Choengteurchhoy@gmail.com>
2017-06-23 09:36:25 +0700

    fix bug multi managers

--- app/classes/ability.rb
+++ app/classes/ability.rb
@@ -138,8 +138,9 @@ class Ability
       end
     elsif user.manager?
       can :manage, Client, user_id: user.id
-      can :manage, Client, user: User.where('manager_ids && ARRAY[?]', user.id)
+      can :manage, Client, user_id: User.where('manager_ids && ARRAY[?]', user.id).map(&:id)
       can :manage, User, id: User.where('manager_ids && ARRAY[?]', user.id).map(&:id)
+      can :manage, User, id: user.id
       can :manage, Case
       can :manage, Task
       can :manage, Assessment
