d0c5ef6d64ddec4651437d008af7378b5478653d
PirunSeng <sengpirun.rain@gmail.com>
2017-06-29 15:09:16 +0700

    Rename ngo usage report to OSCaR

--- app/mailers/ngo_usage_report_mailer.rb
+++ app/mailers/ngo_usage_report_mailer.rb
@@ -4,7 +4,7 @@ class NgoUsageReportMailer < ApplicationMailer
     @previous_month = previous_month
     emails    = ['chris@childreninfamilies.org']
     dev_email = ['sengpirun.rain@gmail.com']
-    attachments["cambodian-families-usage-report-#{date_time}.xls"] = File.read(Rails.root.join("tmp/cambodian-families-usage-report-#{date_time}.xls"))
+    attachments["OSCaR-usage-report-#{date_time}.xls"] = File.read(Rails.root.join("tmp/OSCaR-usage-report-#{date_time}.xls"))
     mail(to: emails, subject: "OSCaR Usage Report of #{@previous_month}", bcc: dev_email)
   end
 end
