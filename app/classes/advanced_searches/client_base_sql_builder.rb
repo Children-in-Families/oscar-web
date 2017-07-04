e246e1223227d3b990a9a2f2a20b3020037b58df
Ly Pitou <pitouly189@gmail.com>
2017-07-04 13:51:27 +0700

    combine custom form fields to client basic fields in advanced search (#406)

--- app/classes/advanced_searches/client_base_sql_builder.rb
+++ app/classes/advanced_searches/client_base_sql_builder.rb
@@ -16,14 +16,23 @@ module AdvancedSearches
         field    = rule['field']
         operator = rule['operator']
         value    = rule['value']
+        form_builder = field != nil ? field.split('_') : []
+
         if ASSOCIATION_FIELDS.include?(field)
           association_filter = AdvancedSearches::ClientAssociationFilter.new(@clients, field, operator, value).get_sql
           @sql_string << association_filter[:id]
           @values     << association_filter[:values]
 
+        elsif form_builder.first == 'formbuilder'
+          custom_form = CustomField.find_by(form_title: form_builder.second)
+          custom_field = AdvancedSearches::ClientCustomFormSqlBuilder.new(custom_form, rule).get_sql
+          @sql_string << custom_field[:id]
+          @values << custom_field[:values]
+
         elsif field != nil
           value = field == 'grade' ? validate_integer(value) : value
           base_sql(field, operator, value)
+
         else
           nested_query =  AdvancedSearches::ClientBaseSqlBuilder.new(@clients, rule).generate
           @sql_string << nested_query[:sql_string]
