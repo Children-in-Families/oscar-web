af441ad6f70a84f75ccb1d95eb3449633776596d
PirunSeng <sengpirun.rain@gmail.com>
2017-07-04 16:28:10 +0700

    Fix interceptor mailer

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -1,4 +1,3 @@
 if Rails.env.staging?
-  interceptor = MailInterceptor::Interceptor.new({ forward_emails_to: ENV['FORWARD_EMAIL'] })
-  ActionMailer::Base.register_interceptor(interceptor)
+  ActionMailer::Base.register_interceptor(RedirectOutgoingMails)
 end
