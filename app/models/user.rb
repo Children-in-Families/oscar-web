19137d5d467782c3c2230d16c2d056b06edf912f
Choengteur <Choengteurchhoy@gmail.com>
2017-07-05 09:58:06 +0700

    disable client enrollment trackings notification in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -141,7 +141,7 @@ class User < ActiveRecord::Base
   end
 
   def client_enrollment_tracking_overdue_or_due_today
-    client_enrollment_tracking_notification(clients)
+    client_enrollment_tracking_notification(clients) unless Rails.env.production?
   end
 
   def self.self_and_subordinates(user)
