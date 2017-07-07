a3b011f473e3cc968f93b320df2958ce68c99417
PirunSeng <sengpirun.rain@gmail.com>
2017-07-07 09:58:03 +0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622041205) do
+ActiveRecord::Schema.define(version: 20170706083521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 20170622041205) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "goal",           default: ""
-    t.string   "attachments",    default: [], array: true
   end
 
   create_table "assessment_domains_progress_notes", force: :cascade do |t|
@@ -192,7 +191,6 @@ ActiveRecord::Schema.define(version: 20170622041205) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.integer  "tracking_id"
-    t.integer  "program_stream_id"
   end
 
   add_index "client_enrollment_trackings", ["client_enrollment_id"], name: "index_client_enrollment_trackings_on_client_enrollment_id", using: :btree
@@ -208,14 +206,6 @@ ActiveRecord::Schema.define(version: 20170622041205) do
 
   add_index "client_enrollments", ["client_id"], name: "index_client_enrollments_on_client_id", using: :btree
   add_index "client_enrollments", ["program_stream_id"], name: "index_client_enrollments_on_program_stream_id", using: :btree
-
-  create_table "client_program_streams", force: :cascade do |t|
-    t.integer  "client_id"
-    t.integer  "program_stream_id"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
-    t.string   "status",            default: "Active"
-  end
 
   create_table "client_quantitative_cases", force: :cascade do |t|
     t.integer  "quantitative_case_id"
@@ -271,6 +261,8 @@ ActiveRecord::Schema.define(version: 20170622041205) do
     t.string   "live_with",                        default: ""
     t.integer  "id_poor",                          default: 0
     t.integer  "rice_support",                     default: 0
+    t.text     "exit_note",                        default: ""
+    t.date     "exit_date"
   end
 
   add_index "clients", ["donor_id"], name: "index_clients_on_donor_id", using: :btree
@@ -353,26 +345,6 @@ ActiveRecord::Schema.define(version: 20170622041205) do
     t.datetime "updated_at",               null: false
     t.string   "code",        default: ""
   end
-
-  create_table "enrollments", force: :cascade do |t|
-    t.jsonb    "properties"
-    t.integer  "client_program_stream_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  add_index "enrollments", ["client_program_stream_id"], name: "index_enrollments_on_client_program_stream_id", using: :btree
-
-  create_table "exit_programs", force: :cascade do |t|
-    t.jsonb    "properties"
-    t.integer  "client_program_stream_id"
-    t.integer  "enrollment_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  add_index "exit_programs", ["client_program_stream_id"], name: "index_exit_programs_on_client_program_stream_id", using: :btree
-  add_index "exit_programs", ["enrollment_id"], name: "index_exit_programs_on_enrollment_id", using: :btree
 
   create_table "families", force: :cascade do |t|
     t.string   "code"
@@ -539,17 +511,14 @@ ActiveRecord::Schema.define(version: 20170622041205) do
   create_table "program_streams", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.jsonb    "rules",             default: {}
-    t.jsonb    "enrollment",        default: {}
-    t.jsonb    "exit_program",      default: {}
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.jsonb    "rules",        default: {}
+    t.jsonb    "enrollment",   default: {}
+    t.jsonb    "exit_program", default: {}
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "quantity"
-    t.string   "ngo_name",          default: ""
-    t.integer  "time_of_frequency"
-    t.string   "frequency"
-    t.jsonb    "tracking"
-    t.boolean  "completed",         default: false
+    t.string   "ngo_name",     default: ""
+    t.boolean  "completed",    default: false
   end
 
   create_table "progress_note_types", force: :cascade do |t|
@@ -903,7 +872,7 @@ ActiveRecord::Schema.define(version: 20170622041205) do
     t.datetime "updated_at",                     null: false
   end
 
-  add_index "trackings", ["program_stream_id", "name"], name: "index_trackings_on_program_stream_id_and_name", unique: true, using: :btree
+  add_index "trackings", ["name", "program_stream_id"], name: "index_trackings_on_name_and_program_stream_id", unique: true, using: :btree
   add_index "trackings", ["program_stream_id"], name: "index_trackings_on_program_stream_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -940,8 +909,8 @@ ActiveRecord::Schema.define(version: 20170622041205) do
     t.integer  "organization_id"
     t.boolean  "disable",                default: false
     t.datetime "expires_at"
-    t.boolean  "task_notify",            default: true
     t.integer  "manager_id"
+    t.boolean  "task_notify",            default: true
     t.boolean  "calendar_integration",   default: false
     t.integer  "pin_number"
     t.integer  "manager_ids",            default: [],                         array: true
@@ -1001,9 +970,6 @@ ActiveRecord::Schema.define(version: 20170622041205) do
   add_foreign_key "clients", "donors"
   add_foreign_key "custom_field_properties", "custom_fields"
   add_foreign_key "domains", "domain_groups"
-  add_foreign_key "enrollments", "client_program_streams"
-  add_foreign_key "exit_programs", "client_program_streams"
-  add_foreign_key "exit_programs", "enrollments"
   add_foreign_key "interventions_progress_notes", "interventions"
   add_foreign_key "interventions_progress_notes", "progress_notes"
   add_foreign_key "leave_programs", "client_enrollments"
