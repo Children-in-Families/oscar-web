6863627e4158208199638d29538e5c5f2ec29815
PirunSeng <sengpirun.rain@gmail.com>
2017-07-17 16:24:13 +0700

    Fix families version action

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,7 +1,7 @@
 class FamiliesController < AdminController
   load_and_authorize_resource
 
-  before_action :find_association, except: [:index, :destroy]
+  before_action :find_association, except: [:index, :destroy, :version]
   before_action :find_family, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -35,7 +35,6 @@ class FamiliesController < AdminController
     @free_family_forms          = CustomField.family_forms.not_used_forms(custom_field_ids).order_by_form_title
     @group_family_custom_fields = @family.custom_field_properties.group_by(&:custom_field_id)
     @client_grid = ClientGrid.new(params.fetch(:client_grid, {}).merge!(family_id: @family.id))
-
     @results = @client_grid.assets.size
     @client_grid.scope { |scope| scope.page(params[:page]).per(5) }
   end
@@ -83,7 +82,7 @@ class FamiliesController < AdminController
   end
 
   def find_association
-    @clients  = Client.accessible_by(current_ability).joins('LEFT OUTER JOIN cases ON cases.client_id = clients.id').where('cases.family_id = ? OR (clients.status = ? AND clients.state = ?)', @family.id, 'Referred', 'accepted').order(:given_name, :family_name)
+    @clients  = Client.accessible_by(current_ability).joins('LEFT OUTER JOIN cases ON cases.client_id = clients.id').where('cases.family_id = ? OR (clients.status = ? AND clients.state = ?)', @family.id, 'Referred', 'accepted').order(:given_name, :family_name).uniq
     @province = Province.order(:name)
   end
 
