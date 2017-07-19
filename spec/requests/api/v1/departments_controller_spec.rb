903866ca6f51d5e3739f39eb5302bfee43a4e201
PirunSeng <sengpirun.rain@gmail.com>
2017-07-19 18:02:15 +0700

    Adds user validation to Client and fix specs

---
+++ spec/requests/api/v1/departments_controller_spec.rb
@@ -0,0 +1,31 @@
+describe Api::V1::DomainGroupsController do
+  let(:user) { create(:user) }
+  let!(:departments) { create_list(:department, 2) }
+
+  describe 'GET #index' do
+    context 'when user not loged in' do
+      before do
+        get '/api/v1/departments'
+      end
+
+      it 'should return unauthorized' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when user logged in' do
+      before do
+        sign_in(user)
+        get '/api/v1/departments', @auth_headers
+      end
+
+      it 'should be return status 200' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'should return json of departments' do
+        expect(json['departments'].size).to eq 2
+      end
+    end
+  end
+end
