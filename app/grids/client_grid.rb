5679906141695c0b1e65d57cae60167c1662bf13
Choengteur <Choengteurchhoy@gmail.com>
2017-07-19 11:20:29 +0700

    change query program_stream_options in client grid

--- app/grids/client_grid.rb
+++ app/grids/client_grid.rb
@@ -320,7 +320,7 @@ class ClientGrid
   end
 
   def program_stream_options
-    ProgramStream.joins(:clients).pluck(:name).uniq
+    ProgramStream.joins(:clients).completed.pluck(:name).uniq
   end
 
   column(:slug, order:'clients.id', header: -> { I18n.t('datagrid.columns.clients.id') })
