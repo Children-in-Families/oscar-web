d758695b0848ab3c03d644ba55f1356ec68b909d
sophal <caili.cl29@gmail.com>
2017-07-19 14:29:52 +0700

    fix api spec

--- spec/requests/api/v1/client_enrollments_controller_spec.rb
+++ spec/requests/api/v1/client_enrollments_controller_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe Api::V1::ClientEnrollmentsController, type: :request do
   let(:client)                    { create(:client, user: user) }
   let(:program_stream)            { create(:program_stream) }
   let(:client_enrollment)         { create(:client_enrollment, client: client, program_stream: program_stream) }
-  let(:valid_params)              { params(FFaker::Internet.email, "2", FFaker::Lorem.paragraph) }
-  let(:invalid_params)            { params(FFaker::Name.name, "7", nil) }
-  let(:valid_updated_params)      { params(FFaker::Internet.email, "2", FFaker::Lorem.paragraph) }
-  let(:invalid_updated_params)    { params(FFaker::Name.name, "7", nil) }
+  let(:valid_params)              { params(FFaker::Internet.email, "2", FFaker::Lorem.paragraph, FFaker::Time.date) }
+  let(:invalid_params)            { params(FFaker::Name.name, "7", nil, nil) }
+  let(:valid_updated_params)      { params(FFaker::Internet.email, "2", FFaker::Lorem.paragraph, FFaker::Time.date) }
+  let(:invalid_updated_params)    { params(FFaker::Name.name, "7", nil, nil) }
 
   describe 'POST #create' do
     context 'when user not loged in' do
@@ -112,7 +112,7 @@ RSpec.describe Api::V1::ClientEnrollmentsController, type: :request do
     params[:client_enrollment][:properties]
   end
 
-  def params(email, age, description)
-    { format: 'json', client_enrollment: { properties: { "e-mail" => email, "age" => age, "description" => description } } }
+  def params(email, age, description, enrollment_date)
+    { format: 'json', client_enrollment: {enrollment_date: enrollment_date, properties: { "e-mail" => email, "age" => age, "description" => description } } }
   end
 end
