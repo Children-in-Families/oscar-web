21b9fa4cef8a67653472ead6c2407c014b814a10
Choengteur <Choengteurchhoy@gmail.com>
2017-07-20 14:13:07 +0700

    add api upload attachment in case note

--- app/controllers/api/v1/case_notes_controller.rb
+++ app/controllers/api/v1/case_notes_controller.rb
@@ -5,7 +5,6 @@ module Api
 
       def create
         case_note = @client.case_notes.new(case_note_params)
-
         if case_note.save
           case_note.complete_tasks(params[:case_note][:case_note_domain_groups_attributes])
           render json: case_note
@@ -18,6 +17,9 @@ module Api
         case_note = @client.case_notes.find(params[:id])
 
         if case_note.update_attributes(case_note_params)
+          params[:case_note][:case_note_domain_groups_attributes].each do |d|
+            add_more_attachments(d.second[:attachments], d.second[:id])
+          end
           case_note.complete_tasks(params[:case_note][:case_note_domain_groups_attributes])
           render json: case_note
         else
@@ -28,7 +30,21 @@ module Api
       private
 
       def case_note_params
-        params.require(:case_note).permit(:meeting_date, :attendee, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids])
+        # params.require(:case_note).permit(:meeting_date, :attendee, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids])
+
+        default_params = params.require(:case_note).permit(:meetineg_date, :attendee, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids])
+        default_params = params.require(:case_note).permit(:meeting_date, :attendee, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids, attachments: []]) if action_name == 'create'
+        default_params
+      end
+
+      def add_more_attachments(new_file, case_note_domain_group_id)
+        if new_file.present?
+          case_note_domain_group = @case_note.case_note_domain_groups.find(case_note_domain_group_id)
+          files = case_note_domain_group.attachments
+          files += new_file
+          case_note_domain_group.attachments = files
+          case_note_domain_group.save
+        end
       end
     end
   end
