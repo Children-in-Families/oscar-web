c6517e67ef29237f2d88422f872f98af2d12606a
sophal <caili.cl29@gmail.com>
2017-07-21 11:32:57 +0700

    changed helper name and logic

--- app/helpers/client_enrollment_helper.rb
+++
@@ -1,6 +0,0 @@
-module ClientEnrollmentHelper
-  def redirect_path
-    client = params[:client]
-    (client.present? && program_streams.present?) ? request.referer : program_streams_path
-  end
-end
