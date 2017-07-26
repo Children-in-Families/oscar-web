405accf3a6012e2a69d6ae7e007fa15d5f46ec34
PirunSeng <sengpirun.rain@gmail.com>
2017-07-26 11:39:43 +0700

    Fix caseworker task association

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,10 +2,13 @@ FactoryGirl.define do
   factory :task do
     name FFaker::Name.name
     completion_date FFaker::Time.date
-    association :user, factory: :user
     association :case_note_domain_group, factory: :case_note_domain_group
     association :domain, factory: :domain
     association :client, factory: :client
+
+    before(:create) do |client|
+      client.users << FactoryGirl.create(:user)
+    end
   end
 end
 
