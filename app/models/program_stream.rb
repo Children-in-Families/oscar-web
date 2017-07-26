529efb134d883286c253ef1d4882c793cece4e9f
Choengteur <Choengteurchhoy@gmail.com>
2017-07-26 16:37:30 +0700

    comment validateremove enrollment field

--- app/models/program_stream.rb
+++ app/models/program_stream.rb
@@ -15,7 +15,7 @@ class ProgramStream < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
   validate  :form_builder_field_uniqueness
-  validate  :validate_remove_enrollment_field, if: -> { id.present? }
+  # validate  :validate_remove_enrollment_field, :validate_remove_exit_program_field, if: -> { id.present? }
 
   after_save :set_program_completed
 
@@ -49,22 +49,22 @@ class ProgramStream < ActiveRecord::Base
     errors_massage
   end
 
-  def validate_remove_enrollment_field
-    return unless enrollment_changed?
-    error_fields = []
-    properties = client_enrollments.pluck(:properties).select(&:present?)
-    properties.each do |property|
-      field_remove = enrollment_change.first - enrollment_change.last
-      field_remove.each do |field|
-        label_name = property[field['label']]
-        error_fields << field['label'] if label_name.present?
-      end
-    end
-    return unless error_fields.present?
-    error_message = "#{error_fields.uniq.join(', ')} #{I18n.t('cannot_remove_or_update')}"
-    errors.add(:enrollment, "#{error_message}")
-    errors.add(:tab, '3')
-  end
+  # def validate_remove_enrollment_field
+  #   return unless enrollment_changed?
+  #   error_fields = []
+  #   properties = client_enrollments.pluck(:properties).select(&:present?)
+  #   properties.each do |property|
+  #     field_remove = enrollment_change.first - enrollment_change.last
+  #     field_remove.each do |field|
+  #       label_name = property[field['label']]
+  #       error_fields << field['label'] if label_name.present?
+  #     end
+  #   end
+  #   return unless error_fields.present?
+  #   error_message = "#{error_fields.uniq.join(', ')} #{I18n.t('cannot_remove_or_update')}"
+  #   errors.add(:enrollment, "#{error_message}")
+  #   errors.add(:tab, '3')
+  # end
 
   # def validate_remove_exit_program_field
   #   return unless exit_program_changed?
