f0bfb38859d074ad69a80257d1f9317587ad03ab
PirunSeng <sengpirun.rain@gmail.com>
2017-07-28 16:43:27 +0700

    Deploy Mothers Heart

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,9 @@
 set :stage, 'production'
 
-server 'oscarhq.com', user: 'deployer', roles: %w{app web db}
+# TODO: Deploy one after another by commentting out one
+
+# OSCaR
+# server 'oscarhq.com', user: 'deployer', roles: %w{app web db}
+
+# Mother's Heart
+server '13.228.241.79', user: 'deployer', roles: %w{app web db}
