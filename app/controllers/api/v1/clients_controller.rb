acdded4a9259a800e619dc17f32de3fc18ae1458
PirunSeng <sengpirun.rain@gmail.com>
2017-07-28 14:18:13 +0700

    Fix api client controlelr

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -54,7 +54,7 @@ module Api
                 :house_number, :street_number, :village, :commune, :district,
                 :has_been_in_orphanage, :has_been_in_government_care,
                 :relevant_referral_information, :province_id, :donor_id,
-                :state, :rejected_note, :able, :able_state, :id_poor, :live_with, :accepted_date, :exit_note, :exit_date
+                :state, :rejected_note, :able, :able_state, :id_poor, :live_with, :accepted_date, :exit_note, :exit_date,
                 user_ids: [],
                 agency_ids: [],
                 quantitative_case_ids: [],
