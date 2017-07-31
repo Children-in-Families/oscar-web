1b400be66f894d88f688af149ac4743e9a48c8e0
sophal <caili.cl29@gmail.com>
2017-07-31 10:29:29 +0700

    fix param name (#533)

--- app/controllers/api/v1/case_notes_controller.rb
+++ app/controllers/api/v1/case_notes_controller.rb
@@ -39,7 +39,7 @@ module Api
       def case_note_params
         # params.require(:case_note).permit(:meeting_date, :attendee, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids])
 
-        default_params = params.require(:case_note).permit(:meetineg_date, :attendee, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids])
+        default_params = params.require(:case_note).permit(:meeting_date, :attendee, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids])
         default_params = params.require(:case_note).permit(:meeting_date, :attendee, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids, attachments: []]) if action_name == 'create'
         default_params
       end
