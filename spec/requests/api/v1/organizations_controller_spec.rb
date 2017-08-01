093ad2fa7c2633497e2113a6f7d65698e186abcd
SopharyMSH <sopharydev@gmail.com>
2017-08-01 15:27:51 +0700

    api added spec for organizations

---
+++ spec/requests/api/v1/organizations_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+RSpec.describe Api::V1::OrganizationsController, type: :request do
+
+  describe 'GET #index' do
+
+    before do
+      get '/api/v1/organizations'
+    end
+
+    it 'should be return status 200' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should be returns the organizations with the correct data' do
+      expect(json['organizations'].size).to eq 1
+    end
+  end
+end
