76d9a2f9b9eb3bd7fc44196cd24f8af027f464e4
sophal <sophal@rotati.com>
2017-08-08 16:40:16 +0700

    refactor query (#583)

--- app/classes/advanced_searches/program_exit_date_sql_builder.rb
+++ app/classes/advanced_searches/program_exit_date_sql_builder.rb
@@ -24,7 +24,7 @@ module AdvancedSearches
       when 'greater_or_equal'
         leave_program_date = leave_programs.where('exit_date >= ?', @value)
       when 'is_empty'
-        client_ids = ClientEnrollment.where(program_stream_id: @program_stream_id).group_by{|c| [c.client_id, c.program_stream_id]}.collect{|k, v| k.first if v.size == 1 && v.last.status == 'Active'}.select(&:present?)
+        client_ids = ClientEnrollment.where(program_stream_id: @program_stream_id).select("client_id").group(:client_id).having("count(*) = 1 and max(status)= 'Active'")
       when 'is_not_empty'
         leave_program_date = leave_programs.where.not(exit_date: nil)
       when 'between'
