2ad2a58fc29bf7f787a067c1d30985cc13f79963
SopharyMSH <sopharydev@gmail.com>
2017-08-09 13:44:44 +0700

    api updated custom_field_property_params

--- app/controllers/api/v1/custom_field_properties_controller.rb
+++ app/controllers/api/v1/custom_field_properties_controller.rb
@@ -40,8 +40,11 @@ module Api
       end
 
       def custom_field_property_params
-        default_params = params.require(:custom_field_property).permit({}).merge(properties: (params['custom_field_property']['properties']), custom_field_id: params[:custom_field_id])
-        default_params = default_params.merge(attachments: (params['custom_field_property']['attachments'])) if action_name == 'create'
+        params[:custom_field_property][:properties].keys.each do |k|
+          params[:custom_field_property][:properties][k].delete('') if params[:custom_field_property][:properties][k].class == Array && params[:custom_field_property][:properties][k].count > 1
+        end
+        default_params = params.require(:custom_field_property).permit({}).merge(properties: params[:custom_field_property][:properties], custom_field_id: params[:custom_field_id])
+        default_params = default_params.merge(attachments: params[:custom_field_property][:attachments]) if action_name == 'create'
         default_params
       end
 
