f0cbfac89d1f767ccd06d30821dcea72d2379e35
PirunSeng <sengpirun.rain@gmail.com>
2017-08-14 15:22:37 +0700

    Fix tracking multiple select options (#604)

--- app/controllers/client_enrollments_controller.rb
+++ app/controllers/client_enrollments_controller.rb
@@ -60,9 +60,7 @@ class ClientEnrollmentsController < AdminController
   private
 
   def client_enrollment_params
-    params[:client_enrollment][:properties].keys.each do |k|
-      params[:client_enrollment][:properties][k].delete('') if params[:client_enrollment][:properties][k].class == Array && params[:client_enrollment][:properties][k].count > 1
-    end
+    params[:client_enrollment][:properties].values.map{|v| v.delete('')}
     params.require(:client_enrollment).permit(:enrollment_date, {}).merge(properties: params[:client_enrollment][:properties], program_stream_id: params[:program_stream_id])
   end
 
