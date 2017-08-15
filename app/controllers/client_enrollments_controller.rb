06b19364149685950068f2ac36718ee931ad353c
PirunSeng <sengpirun.rain@gmail.com>
2017-08-15 14:44:02 +0700

    Remove unused method call in coffee and check presence of properties_params in controller

--- app/controllers/client_enrollments_controller.rb
+++ app/controllers/client_enrollments_controller.rb
@@ -74,7 +74,7 @@ class ClientEnrollmentsController < AdminController
     properties_params.values.map{ |v| v.delete('') if (v.is_a?Array) && v.size > 1 }
 
     default_params = params.require(:client_enrollment).permit(:enrollment_date).merge!(program_stream_id: params[:program_stream_id])
-    default_params = default_params.merge!(properties: properties_params)
+    default_params = default_params.merge!(properties: properties_params) if properties_params.present?
     default_params = default_params.merge!(form_builder_attachments_attributes: params[:client_enrollment][:form_builder_attachments_attributes]) if action_name == 'create' && attachment_params.present?
     default_params
   end
