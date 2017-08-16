e70b17dd9d174ebd22b0bf85a0896d3871b4715f
Choengteur <Choengteurchhoy@gmail.com>
2017-08-16 11:22:31 +0700

    change migration default values program stream

--- db/migrate/20170804045725_add_default_field_to_leave_program.rb
+++ db/migrate/20170804045725_add_default_field_to_leave_program.rb
@@ -1,9 +1,9 @@
 class AddDefaultFieldToLeaveProgram < ActiveRecord::Migration
   def up
-    add_column :leave_programs, :properties, :jsonb, default: {}
+    change_column :leave_programs, :properties, :jsonb, default: {}
   end
 
   def down
-    remove_column :leave_programs, :properties, :jsonb, default: {}
+    change_column :leave_programs, :properties, :jsonb
   end
 end
