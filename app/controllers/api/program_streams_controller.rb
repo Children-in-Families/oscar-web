98a5310179e2978ce5a8d71cafb8ddea574f39ae
Choengteur <choengteurchhoy@gmail.com>
2017-08-18 12:01:51 +0700

    fix form builder can not remove filed file

--- app/controllers/api/program_streams_controller.rb
+++ app/controllers/api/program_streams_controller.rb
@@ -2,7 +2,10 @@ module Api
   class ProgramStreamsController < AdminController
     def enrollment_fields
       program_stream = ProgramStream.find params[:program_stream_id]
+      client_enrollment_ids = ClientEnrollment.find_by_program_stream_id(program_stream.id).ids
+      file_uploader = FormBuilderAttachment.find_by_form_buildable(client_enrollment_ids, 'ClientEnrollment').map { |c| c.name if c.file.any? }
       properties = program_stream.client_enrollments.pluck(:properties).select(&:present?).map(&:keys).flatten.uniq
+      properties += file_uploader
       render json: properties
     end
 
@@ -10,14 +13,21 @@ module Api
       program_stream = ProgramStream.find params[:program_stream_id]
       properties = {}
       program_stream.trackings.each do |tracking|
-        properties.store(tracking.name,tracking.client_enrollment_trackings.pluck(:properties).select(&:present?).map(&:keys).flatten.uniq)
+        client_enrollment_tracking_ids = ClientEnrollmentTracking.enrollment_trackings_by(tracking.id).ids
+        file_uploader = FormBuilderAttachment.find_by_form_buildable(client_enrollment_tracking_ids, 'ClientEnrollmentTracking').map { |c| c.name if c.file.any? }
+        tracking_fields = tracking.client_enrollment_trackings.pluck(:properties).select(&:present?).map(&:keys).flatten.uniq
+        tracking_fields += file_uploader
+        properties.store(tracking.name, tracking_fields)
       end
       render json: properties.merge(field: 'tracking')
     end
 
     def exit_program_fields
       program_stream = ProgramStream.find params[:program_stream_id]
+      leave_program_ids = LeaveProgram.find_by_program_stream_id(program_stream.id).ids
+      file_uploader = FormBuilderAttachment.find_by_form_buildable(leave_program_ids, 'LeaveProgram').map { |c| c.name if c.file.any? }
       properties = program_stream.leave_programs.pluck(:properties).select(&:present?).map(&:keys).flatten.uniq
+      properties += file_uploader
       render json: properties
     end
   end
