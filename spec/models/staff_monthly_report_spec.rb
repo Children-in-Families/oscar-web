b16c69b7624a2aeea1bf4c4ebb95e3e9028d93bc
Pirun Seng <sengpirun.rain@gmail.com>
2017-08-23 10:41:21 +0700

    Refactor staff monthly report spec

--- spec/models/staff_monthly_report_spec.rb
+++ spec/models/staff_monthly_report_spec.rb
@@ -1,44 +1,48 @@
 describe 'Staff Monthly Report' do
+  before do
+    TaskHistory.destroy_all
+  end
   let!(:user_1) { create(:user) }
   let!(:user_2) { create(:user) }
   let!(:user_3) { create(:user) }
 
-  let!(:user_1_tasks) { create_list(:task, Date.today.last_month.end_of_month.day, :incomplete, completion_date: Date.today.last_month, users: [user_1]) }
-  let!(:user_2_tasks) { create_list(:task, (Date.today.last_month.end_of_month.day * 2), :incomplete, completion_date: Date.today.last_month, users: [user_2]) }
+  let!(:user_1_tasks) { create_list(:task, 2, :incomplete, completion_date: Date.today.last_month, users: [user_1]) }
+  let!(:user_2_tasks) { create_list(:task, 32, :incomplete, completion_date: Date.today.last_month, users: [user_2]) }
 
-  let!(:visit_1){ FactoryGirl.create(:visit, user: user_1, created_at: Date.today.last_month) }
-  let!(:visit_2){ FactoryGirl.create(:visit, user: user_1, created_at: Date.today.last_month) }
-  let!(:visits){ FactoryGirl.create_list(:visit, 32, user: user_1, created_at: Date.today.last_month) }
-  let!(:user_2_visits){ FactoryGirl.create_list(:visit, 10, user: user_2, created_at: Date.today.last_month) }
+  let!(:visit_1){ create(:visit, user: user_1, created_at: Date.today.last_month) }
+  let!(:visit_2){ create(:visit, user: user_1, created_at: Date.today.last_month) }
+  let!(:visits){ create_list(:visit, 32, user: user_1, created_at: Date.today.last_month) }
+  let!(:user_2_visits){ create_list(:visit, 2, user: user_2, created_at: Date.today.last_month) }
 
   let!(:client_1) { create(:client, :accepted, users: [user_1]) }
   let!(:client_3) { create(:client, :accepted, users: [user_1, user_2]) }
   let!(:client_2) { create(:client, :accepted, users: [user_3]) }
+  let!(:client_4) { create(:client, :accepted, users: [user_3]) }
 
-  let!(:domain_group_1) { FactoryGirl.create(:domain_group) }
-  let!(:domain_group_2) { FactoryGirl.create(:domain_group) }
-  let!(:domain_group_3) { FactoryGirl.create(:domain_group) }
-  let!(:domain_group_4) { FactoryGirl.create(:domain_group) }
-  let!(:domain_group_5) { FactoryGirl.create(:domain_group) }
-  let!(:domain_group_6) { FactoryGirl.create(:domain_group) }
+  let!(:domain_group_1) { create(:domain_group) }
+  let!(:domain_group_2) { create(:domain_group) }
+  let!(:domain_group_3) { create(:domain_group) }
+  let!(:domain_group_4) { create(:domain_group) }
+  let!(:domain_group_5) { create(:domain_group) }
+  let!(:domain_group_6) { create(:domain_group) }
 
-  let!(:case_note) { FactoryGirl.create(:case_note, client: client_1, created_at: Date.today.last_month) }
-  let!(:case_notes) { FactoryGirl.create_list(:case_note, 62, client: client_2, created_at: Date.today.last_month) }
+  let!(:case_note) { create(:case_note, client: client_1, created_at: Date.today.last_month) }
+  let!(:case_notes) { create_list(:case_note, 4, client: client_2, created_at: Date.today.last_month) }
 
-  let!(:case_note_domain_group_1) { FactoryGirl.create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_1, note: ('Test')) }
-  let!(:case_note_domain_group_2) { FactoryGirl.create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_2, note: ('Test')) }
-  let!(:case_note_domain_group_3) { FactoryGirl.create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_3, note: ('Test')) }
-  let!(:case_note_domain_group_4) { FactoryGirl.create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_4, note: ('Test')) }
-  let!(:case_note_domain_group_5) { FactoryGirl.create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_5, note: ('Test')) }
-  let!(:case_note_domain_group_6) { FactoryGirl.create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_6, note: ('Test')) }
+  let!(:case_note_domain_group_1) { create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_1, note: ('Test')) }
+  let!(:case_note_domain_group_2) { create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_2, note: ('Test')) }
+  let!(:case_note_domain_group_3) { create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_3, note: ('Test')) }
+  let!(:case_note_domain_group_4) { create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_4, note: ('Test')) }
+  let!(:case_note_domain_group_5) { create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_5, note: ('Test')) }
+  let!(:case_note_domain_group_6) { create(:case_note_domain_group, case_note: case_note, domain_group: domain_group_6, note: ('Test')) }
 
-  let!(:assessment_1) { FactoryGirl.create(:assessment, client: client_1, created_at: (Date.today.last_month - 8.months)) }
-  let!(:assessment_2) { FactoryGirl.create(:assessment, client: client_1, created_at: Date.today.last_month) }
+  let!(:assessment_1) { create(:assessment, client: client_1, created_at: (Date.today.last_month - 8.months)) }
+  let!(:assessment_2) { create(:assessment, client: client_1, created_at: Date.today.last_month) }
 
-  let!(:assessment_3) { FactoryGirl.create(:assessment, client: client_3, created_at: (Date.today.last_month - 6.months)) }
-  let!(:assessment_4) { FactoryGirl.create(:assessment, client: client_3, created_at: Date.today.last_month) }
+  let!(:assessment_3) { create(:assessment, client: client_3, created_at: (Date.today.last_month - 6.months)) }
+  let!(:assessment_4) { create(:assessment, client: client_3, created_at: Date.today.last_month) }
 
-  let!(:assessment_5) { FactoryGirl.create(:assessment, client: client_2, created_at: Date.today.last_month) }
+  let!(:assessment_5) { create(:assessment, client: client_2, created_at: Date.today.last_month) }
 
   feature 'report' do
     scenario 'average number of daily logins' do
@@ -54,6 +58,7 @@ describe 'Staff Monthly Report' do
     scenario 'average number of casenotes completed per client' do
       expect(StaffMonthlyReport.average_number_of_casenotes_completed_per_client(user_1)).to eq(1)
       expect(StaffMonthlyReport.average_number_of_casenotes_completed_per_client(user_2)).to eq(0)
+      expect(StaffMonthlyReport.average_number_of_casenotes_completed_per_client(user_3)).to eq(2)
     end
 
     scenario 'average length of time, as month, between completing CSI for each client' do
