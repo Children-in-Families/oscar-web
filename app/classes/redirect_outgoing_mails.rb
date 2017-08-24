0d600b9101fbf0f27e52e61905e8f7a6d0b0be46
Pirun Seng <sengpirun.rain@gmail.com>
2017-08-24 15:38:13 +0700

    Fix cc and bcc of email interceptor

--- app/classes/redirect_outgoing_mails.rb
+++ app/classes/redirect_outgoing_mails.rb
@@ -1,9 +1,11 @@
 class RedirectOutgoingMails
   class << self
- 
+
     def delivering_email(mail)
       return if mail.subject == 'Reset password instructions'
       mail.to = ENV['FORWARD_EMAIL']
+      mail.cc = ENV['FORWARD_EMAIL']
+      mail.bcc = ENV['FORWARD_EMAIL']
     end
   end
 end
