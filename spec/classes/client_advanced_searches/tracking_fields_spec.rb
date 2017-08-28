106415b92a5ecc846337511e8f233ab74356e68c
Pitou <pitou@Pitous-Mac-Book-Pro.local>
2017-08-28 15:41:09 +0700

    exit program field and tracking fields specs

---
+++ spec/classes/client_advanced_searches/tracking_fields_spec.rb
@@ -0,0 +1,35 @@
+describe AdvancedSearches::TrackingFields, 'Method' do
+  let!(:client)             { create(:client) }
+  let!(:program_stream)     { create(:program_stream) }
+  let!(:tracking)           { create(:tracking, program_stream: program_stream) }
+  let!(:client_enrollment)  { create(:client_enrollment, client: client, program_stream: program_stream) }
+
+  before do
+    @tracking_fields = AdvancedSearches::TrackingFields.new([program_stream.id]).render
+    @fields = @tracking_fields.last
+  end
+
+  context 'render' do
+    it 'return field not nil' do
+      expect(@tracking_fields).not_to be_nil
+    end
+
+    it 'return all fields' do
+      expect(@tracking_fields.size).to equal 3
+    end
+
+    it 'return field with id' do
+      tracking = program_stream.trackings.first
+      expect(@fields[:id]).to include "tracking_#{program_stream.name}_#{tracking.name}_e-mail"
+    end
+
+    it 'return field with optGroup' do
+      tracking = program_stream.trackings.first
+      expect(@fields[:optgroup]).to include "#{program_stream.name} | #{tracking.name} | Tracking"
+    end
+
+    it 'return field with label' do
+      expect(@fields[:label]).to include 'e-mail'
+    end
+  end
+end
