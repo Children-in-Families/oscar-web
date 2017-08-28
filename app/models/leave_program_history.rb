7bdbc8a6d243d67663ee0b1940b4e2d2117c4176
Pirun Seng <sengpirun.rain@gmail.com>
2017-08-28 13:42:38 +0700

    Format field properties with space and dot to underscore

--- app/models/leave_program_history.rb
+++ app/models/leave_program_history.rb
@@ -11,6 +11,15 @@ class LeaveProgramHistory
 
   def self.initial(leave_program)
     attributes = leave_program.attributes
+    attributes['properties'] = format_property(attributes)
     create(object: attributes)
+  end
+
+  def self.format_property(attributes)
+    mappings = {}
+    attributes['properties'].each do |k, v|
+      mappings[k] = k.gsub(/(\s|[.])/, '_')
+    end
+    attributes['properties'].map {|k, v| [mappings[k].downcase, v] }.to_h
   end
 end
