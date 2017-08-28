8550356665670902fb4c83e5dd25637ed590ff2f
sophal <caili.cl29@gmail.com>
2017-08-28 15:30:12 +0700

    ec manager

--- spec/features/ability_spec.rb
+++ spec/features/ability_spec.rb
@@ -93,4 +93,81 @@ describe 'Abilities' do
       should be_able_to(:read, ProgressNote)
     end
   end
+
+  context 'ec manager permissions' do
+    let!(:user){ create(:user, :ec_manager) }
+
+    it 'can manage Agency' do
+      should be_able_to(:manage, Agency)
+    end
+
+    it 'can manage ReferralSource' do
+      should be_able_to(:manage, ReferralSource)
+    end
+
+    it 'can manage QuarterlyReport' do
+      should be_able_to(:manage, QuarterlyReport)
+    end
+
+    it 'can manage ProgramStream' do
+      should be_able_to(:read, ProgramStream)
+      should be_able_to(:preview, ProgramStream)
+    end
+
+    it 'can manage Client' do
+      should be_able_to(:create, Client)
+      should be_able_to(:manage, Client, status: 'Active EC')
+      should be_able_to(:manage, Client, case_worker_clients: { user_id: user.id })
+    end
+
+    it 'can manage CaseNote' do
+      should be_able_to(:manage, CaseNote)
+    end
+
+    it 'can read ProgressNote' do
+      should be_able_to(:read, ProgressNote)
+    end
+
+    it 'can manage Family' do
+      should be_able_to(:manage, Family)
+    end
+
+    it 'can manage Partner' do
+      should be_able_to(:manage, Partner)
+    end
+
+    it 'can manage Case' do
+      should be_able_to(:manage, Case, { case_type: 'EC', exited: false })
+    end
+
+    it 'can manage Assessment' do
+      should be_able_to(:manage, Assessment)
+    end
+
+    it 'can manage Task' do
+      should be_able_to(:manage, Task)
+    end
+
+    it 'can manage CustomFieldProperty' do
+      should be_able_to(:manage, CustomFieldProperty, custom_formable_type: 'Client')
+      should be_able_to(:manage, CustomFieldProperty, custom_formable_type: 'Family')
+      should be_able_to(:manage, CustomFieldProperty, custom_formable_type: 'Partner')
+    end
+
+    it 'can manage CustomField' do
+      should be_able_to(:manage, CustomField)
+    end
+
+    it 'can manage ClientEnrollment' do
+      should be_able_to(:manage, ClientEnrollment)
+    end
+
+    it 'can manage ClientEnrollmentTracking' do
+      should be_able_to(:manage, ClientEnrollmentTracking)
+    end
+
+    it 'can manage LeaveProgram' do
+      should be_able_to(:manage, LeaveProgram)
+    end
+  end
 end
