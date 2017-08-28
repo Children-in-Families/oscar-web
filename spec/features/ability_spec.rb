c40876df70d56b36c0efbc7772bdff5910292a0d
sophal <caili.cl29@gmail.com>
2017-08-28 11:10:34 +0700

    change should be able to manage to can manage

--- spec/features/ability_spec.rb
+++ spec/features/ability_spec.rb
@@ -1,74 +1,74 @@
 describe 'Abilities' do
   subject(:ability){ Ability.new(user) }
 
-  context 'login as admin' do
+  context 'admin permissions' do
     let!(:user){ create(:user, :admin) }
 
-    it 'should be able to manage Agency' do
+    it 'can manage Agency' do
       should be_able_to(:manage, Agency.new)
     end
 
-    it 'should be able to manage ReferralSource' do
+    it 'can manage ReferralSource' do
       should be_able_to(:manage, ReferralSource.new)
     end
 
-    it 'should be able to manage QuarterlyReport' do
+    it 'can manage QuarterlyReport' do
       should be_able_to(:manage, QuarterlyReport.new)
     end
 
-    it 'should be able to manage ProgramStream' do
+    it 'can manage ProgramStream' do
       should be_able_to(:manage, ProgramStream.new)
     end
 
-    it 'should be able to manage AbleScreeningQuestion' do
+    it 'can manage AbleScreeningQuestion' do
       should be_able_to(:manage, AbleScreeningQuestion.new)
     end
 
-    it 'should be able to manage Assessment' do
+    it 'can manage Assessment' do
       should be_able_to(:manage, Assessment.new)
     end
 
-    it 'should be able to manage Attachment' do
+    it 'can manage Attachment' do
       should be_able_to(:manage, Attachment.new)
     end
 
-    it 'should be able to manage Case' do
+    it 'can manage Case' do
       should be_able_to(:manage, Case.new)
     end
 
-    it 'should be able to manage CaseNote' do
+    it 'can manage CaseNote' do
       should be_able_to(:manage, CaseNote.new)
     end
 
-    it 'should be able to manage Client' do
+    it 'can manage Client' do
       should be_able_to(:manage, Client.new)
     end
 
-    it 'should be able to manage ProgressNote' do
+    it 'can manage ProgressNote' do
       should be_able_to(:manage, ProgressNote.new)
     end
 
-    it 'should be able to manage Task' do
+    it 'can manage Task' do
       should be_able_to(:manage, Task.new)
     end
 
-    it 'should be able to manage CustomFieldProperty' do
+    it 'can manage CustomFieldProperty' do
       should be_able_to(:manage, CustomFieldProperty.new)
     end
 
-    it 'should be able to manage CustomField' do
+    it 'can manage CustomField' do
       should be_able_to(:manage, CustomField.new)
     end
 
-    it 'should be able to manage ClientEnrollment' do
+    it 'can manage ClientEnrollment' do
       should be_able_to(:manage, ClientEnrollment.new)
     end
 
-    it 'should be able to manage ClientEnrollmentTracking' do
+    it 'can manage ClientEnrollmentTracking' do
       should be_able_to(:manage, ClientEnrollmentTracking.new)
     end
 
-    it 'should be able to manage LeaveProgram' do
+    it 'can manage LeaveProgram' do
       should be_able_to(:manage, LeaveProgram.new)
     end
   end
