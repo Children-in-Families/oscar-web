dbcd4cf5b0af2a45c00c0df3489da95e3481b641
sophal <caili.cl29@gmail.com>
2017-08-28 10:34:48 +0700

    agency spec

--- spec/features/ability_spec.rb
+++ spec/features/ability_spec.rb
@@ -1,3 +1,66 @@
+describe 'Agency' do
+  subject(:ability){ Ability.new(user) }
+
+  context 'login as admin' do
+    let!(:user){ create(:user, :admin) }
+    it 'should be able to manage' do
+      should be_able_to(:manage, Agency.new)
+    end
+  end
+
+  context 'login as manager' do
+    let!(:user){ create(:user, :manager) }
+    it 'should be able to manage' do
+      should be_able_to(:manage, Agency.new)
+    end
+  end
+
+  context 'login as ec manage' do
+    let!(:user){ create(:user, :ec_manager) }
+    it 'should be able to manage' do
+      should be_able_to(:manage, Agency.new)
+    end
+  end
+
+  context 'login as fc manage' do
+    let!(:user){ create(:user, :fc_manager) }
+    it 'should be able to manage' do
+      should be_able_to(:manage, Agency.new)
+    end
+  end
+
+  context 'login as kc manage' do
+    let!(:user){ create(:user, :kc_manager) }
+    it 'should be able to manage' do
+      should be_able_to(:manage, Agency.new)
+    end
+  end
+
+  context 'login as able manager' do
+    let!(:user){ create(:user, :able_manager) }
+    it 'should be able to manage' do
+      should be_able_to(:manage, Agency.new)
+    end
+  end
+
+  context 'login as strategic overviewer' do
+    let!(:user){ create(:user, :strategic_overviewer) }
+    it 'should be able to manage' do
+     should be_able_to(:read, Agency.new)
+    end
+    it 'should be able to manage' do
+      should_not be_able_to(:manage, Agency.new)
+    end
+  end
+
+  context 'login as case worker' do
+    let!(:user){ create(:user, :case_worker) }
+    it 'should be able to manage' do
+      should be_able_to(:manage, Agency.new)
+    end
+  end
+end
+
 describe 'able screening question' do
   subject(:ability){ Ability.new(user) }
 
