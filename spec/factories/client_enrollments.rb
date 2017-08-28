d51157b2e8c077c1b36eb35d494dcde475d79e90
Pitou <pitou@Pitous-Mac-Book-Pro.local>
2017-08-28 14:01:09 +0700

    client enrollment filter with Enrollment Date field spec

--- spec/factories/client_enrollments.rb
+++ spec/factories/client_enrollments.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :client_enrollment do
-    enrollment_date FFaker::Time.date
+    enrollment_date Date.today
     properties { {"e-mail"=>"test@example.com", "age"=>"3", "description"=>"this is testing"}.to_json }
     association :client, factory: :client
     association :program_stream, factory: :program_stream
