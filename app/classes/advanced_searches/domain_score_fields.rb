ff83458b6fd057f7ded50775250a2a3a84ac1058
Choengteur <choengteurchhoy@gmail.com>
2017-09-01 15:07:07 +0700

    resolve comments review

--- app/classes/advanced_searches/domain_score_fields.rb
+++ app/classes/advanced_searches/domain_score_fields.rb
@@ -3,8 +3,8 @@ module AdvancedSearches
     extend AdvancedSearchHelper
 
     def self.render
-      domain_score_grop     = format_header('csi_domain_scores')
-      domain_options.map { |item| number_filter_type(item, domain_score_format(item), domain_score_grop) }
+      domain_score_group     = format_header('csi_domain_scores')
+      domain_options.map { |item| number_filter_type(item, domain_score_format(item), domain_score_group) }
     end
 
     private
@@ -23,7 +23,7 @@ module AdvancedSearches
         optgroup: group,
         label: label,
         type: 'integer',
-        validation: { max: 4, min: 0 },
+        validation: { max: 4, min: 1 },
         operators: ['equal', 'not_equal', 'between']
       }
     end
