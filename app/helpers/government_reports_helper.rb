487a2dd825bb5526af2a494f259d6ccbcd8b5fcd
Pirun Seng <sengpirun.rain@gmail.com>
2017-10-05 10:12:06 +0700

    Display Government Form and PDF button only on CWD instance

--- app/helpers/government_reports_helper.rb
+++ app/helpers/government_reports_helper.rb
@@ -6,12 +6,14 @@ module GovernmentReportsHelper
   def need_rank(need)
     # Need.find_by(name: need).client_needs.find_by(client_id: @client.id).try(:rank)
     need = Need.find_by(name: need)
+    return '' unless need.present?
     @client.client_needs.find_by(need_id: need.id).try(:rank)
   end
 
   def problem_rank(problem)
     # Problem.find_by(name: problem).client_problems.find_by(client_id: @client.id).try(:rank)
     problem = Problem.find_by(name: problem)
+    return '' unless problem.present?
     @client.client_problems.find_by(problem_id: problem.id).try(:rank)
   end
 end
