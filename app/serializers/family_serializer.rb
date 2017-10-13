4a569c3c52b5ed2b1583155563a8325006aa211c
Choengteur <choengteurchhoy@gmail.com>
2017-10-13 11:09:18 +0700

    update api family serializer fetch clients

--- app/serializers/family_serializer.rb
+++ app/serializers/family_serializer.rb
@@ -2,7 +2,7 @@ class FamilySerializer < ActiveModel::Serializer
   attributes :id, :name, :code, :case_history, :caregiver_information, :significant_family_member_count, :household_income, :dependable_income, :female_children_count, :male_children_count, :female_adult_count, :male_adult_count, :family_type, :contract_date, :address, :province, :clients
 
   def clients
-    object.clients.map do |client|
+    Client.where(id: object.children).map do |client|
       formatted_client = client.as_json(except: [:birth_province_id, :received_by_id, :followed_up_by_id, :province_id, :referral_source_id, :donor_id])
 
       formatted_client.merge!(birth_province:   client.birth_province.as_json(only: [:id, :name]))
