390602305fd74baab75a4349d77286c4e9adc0ad
Choengteur Chhoy <choengteurchhoy@gmail.com>
2017-11-24 09:12:00 +0700

    Request api from another site (#818)

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -2,12 +2,18 @@ module Api
   module V1
     class BaseApiController < ApplicationController
       include DeviseTokenAuth::Concerns::SetUserByToken
-      before_action :authenticate_user!
+      before_action :authenticate_user_request
 
       private
 
       def find_client
         @client = Client.accessible_by(current_ability).find(params[:client_id])
+      end
+
+      def authenticate_user_request
+        unless request.headers['access-token'] == ENV['GOV_ACCESS_TOKEN'] && request.headers['gov-domain'] == ENV['GOV_DOMAIN'] && request.headers['client'] == ENV['GOV_CLIENT'] && request.headers['uid'] == ENV['GOV_UID']
+          authenticate_user!
+        end
       end
     end
   end
