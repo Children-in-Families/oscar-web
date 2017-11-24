0a5a2a38824042f0f1e6cf87e6f1336f14b66f61
sophal <caili.cl29@gmail.com>
2017-11-24 14:41:54 +0700

    add spec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -9,11 +9,11 @@ describe 'User' do
   let!(:custom_field){ create(:custom_field) }
   let!(:program_stream){ create(:program_stream) }
 
-  before do
-    login_as(admin)
-  end
-
   feature 'Disable' do
+    before do
+      login_as(admin)
+    end
+
     let!(:disable_user){ create(:user, email: 'aa@bb.com', disable: true, password: '12345678', password_confirmation: '12345678') }
 
     scenario 'disable user from log in', js: true do
@@ -33,6 +33,7 @@ describe 'User' do
 
   feature 'Delete' do
     before do
+      login_as(admin)
       visit users_path
     end
     scenario 'success', js: true do
@@ -43,6 +44,56 @@ describe 'User' do
 
     scenario 'does not succeed' do
       expect(page).to have_css("a[href='#{user_path(used_user)}'][data-method='delete'][class='btn btn-outline btn-danger btn-xs disabled']")
+    end
+  end
+
+  feature 'Prompt User Warning' do
+    context 'Admin' do
+      before do
+        login_as(admin)
+      end
+
+      scenario 'has seen the prompt' do
+        visit program_streams_path
+        expect(page).not_to have_css('#warning-program')
+
+        visit domains_path
+        expect(page).not_to have_css('#domain-warning')
+      end
+
+      scenario 'has not seen the prompt' do
+        admin.update(domain_warning: false, program_warning: false)
+
+        visit program_streams_path
+        expect(page).to have_css('#warning-program')
+
+        visit domains_path
+        expect(page).to have_css('#domain-warning')
+      end
+    end
+
+    context 'User' do
+      before do
+        login_as(user)
+      end
+
+      scenario 'has seen the prompt' do
+        visit program_streams_path
+        expect(page).not_to have_css('#warning-program')
+
+        visit domains_path
+        expect(page).not_to have_css('#domain-warning')
+      end
+
+      scenario 'has not seen the prompt' do
+        user.update(program_warning: false)
+
+        visit program_streams_path
+        expect(page).to have_css('#warning-program')
+
+        visit domains_path
+        expect(page).not_to have_css('#domain-warning')
+      end
     end
   end
 
