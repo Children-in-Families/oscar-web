149ec3f6a09ab9afcbfcb39b7ba137cb6b22c683
Choengteur <choengteurchhoy@gmail.com>
2017-12-01 11:11:22 +0700

    add trackings in program stream api in client serializer

--- app/controllers/api/v1/program_streams_controller.rb
+++ app/controllers/api/v1/program_streams_controller.rb
@@ -2,10 +2,7 @@ module Api
   module V1
     class ProgramStreamsController < Api::V1::BaseApiController
       def index
-        program_streams = ProgramStream.all.map do |p|
-          p.as_json.merge(tracking_fields: p.trackings)
-        end
-        render json: program_streams
+        render json: ProgramStream.all
       end
     end
   end
