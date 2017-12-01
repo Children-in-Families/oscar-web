d1ae249a9383f7d27ce8a6f0e4e2fde83ff93d8d
Choengteur <choengteurchhoy@gmail.com>
2017-12-01 10:54:18 +0700

    add trackings in program stream api

--- app/controllers/api/v1/program_streams_controller.rb
+++ app/controllers/api/v1/program_streams_controller.rb
@@ -2,7 +2,10 @@ module Api
   module V1
     class ProgramStreamsController < Api::V1::BaseApiController
       def index
-        render json: ProgramStream.all
+        program_streams = ProgramStream.all.map do |p|
+          p.as_json.merge(tracking_fields: p.trackings)
+        end
+        render json: program_streams
       end
     end
   end
