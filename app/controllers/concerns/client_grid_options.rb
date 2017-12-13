ba86ae44b3470e9eabd02d6e9a4e460f4bd23b38
sophal <caili.cl29@gmail.com>
2017-12-13 17:07:56 +0700

    add case note type to most recent data (#874)

--- app/controllers/concerns/client_grid_options.rb
+++ app/controllers/concerns/client_grid_options.rb
@@ -25,6 +25,7 @@ module ClientGridOptions
     date_of_assessments
     form_title_report
     case_note_date_report
+    case_note_type_report
   end
 
   def form_title_report
@@ -90,6 +91,19 @@ module ClientGridOptions
     else
       @client_grid.column(:case_note_date, header: I18n.t('datagrid.columns.clients.case_note_date')) do |client|
         client.case_notes.most_recents.pluck(:meeting_date).select(&:present?).join(' | ') if client.case_notes.any?
+      end
+    end
+  end
+
+  def case_note_type_report
+    return unless params[:case_note_type_].present?
+    if params[:data].presence == 'recent'
+      @client_grid.column(:case_note_type, header: I18n.t('datagrid.columns.clients.case_note_type')) do |client|
+        client.case_notes.most_recents.order(meeting_date: :desc).first.try(:interaction_type)
+      end
+    else
+      @client_grid.column(:case_note_type, header: I18n.t('datagrid.columns.clients.case_note_type')) do |client|
+        client.case_notes.most_recents.pluck(:interaction_type).select(&:present?).join(' | ') if client.case_notes.any?
       end
     end
   end
