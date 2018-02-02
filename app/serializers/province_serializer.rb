35f212b006581b3e6238337e5d1338e2aad6bf2f
Choengteur <choengteurchhoy@gmail.com>
2018-02-02 10:26:11 +0700

    add districts to province api

--- app/serializers/province_serializer.rb
+++ app/serializers/province_serializer.rb
@@ -1,3 +1,7 @@
 class ProvinceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description
+  attributes :id, :name, :description, :districts
+
+  def districts
+    object.districts.order(:name)
+  end
 end
