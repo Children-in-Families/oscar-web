2243906555171cbf8e503a4e38c3db82830a30ab
Pirun Seng <sengpirun.rain@gmail.com>
2018-03-16 15:50:17 +0700

    Verify code with ROTP::TOTP

--- app/controllers/users/multi_factor_authentication_controller.rb
+++ app/controllers/users/multi_factor_authentication_controller.rb
@@ -3,20 +3,21 @@ class Users::MultiFactorAuthenticationController < ApplicationController
   before_action :set_user
 
   def verify_enable
-    if current_user == @user &&
-       current_user.validate_and_consume_otp!(params[:multi_factor_authentication][:otp_attempt])
-       current_user.otp_required_for_login = true
-       current_user.save!
+    totp = ROTP::TOTP.new(current_user.otp_secret)
 
-       redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_enabled')
+    if totp.verify_with_drift(params[:multi_factor_authentication][:otp_attempt], 600)
+      current_user.otp_required_for_login = true
+      current_user.save!
+      redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_enabled')
     else
       redirect_to edit_user_registration_path, alert: t('.two_factor_authentication_could_not_be_enabled')
     end
   end
 
   def verify_disabled
-    if current_user == @user &&
-       current_user.validate_and_consume_otp!(params[:multi_factor_authentication][:otp_attempt])
+    totp = ROTP::TOTP.new(current_user.otp_secret)
+
+    if totp.verify_with_drift(params[:multi_factor_authentication][:otp_attempt], 600)
       current_user.otp_required_for_login = false
       current_user.save!
       redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_disabled')
