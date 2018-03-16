25c2f8649e49fe05194446077f88b3685c52c1c2
Pirun Seng <sengpirun.rain@gmail.com>
2018-03-16 16:29:15 +0700

    Update otp allowed drift

--- app/controllers/users/multi_factor_authentication_controller.rb
+++ app/controllers/users/multi_factor_authentication_controller.rb
@@ -5,7 +5,7 @@ class Users::MultiFactorAuthenticationController < ApplicationController
   def verify_enable
     totp = ROTP::TOTP.new(current_user.otp_secret)
 
-    if totp.verify_with_drift(params[:multi_factor_authentication][:otp_attempt], 600)
+    if totp.verify_with_drift(params[:multi_factor_authentication][:otp_attempt], current_user.class.otp_allowed_drift)
       current_user.otp_required_for_login = true
       current_user.save!
       redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_enabled')
@@ -17,7 +17,7 @@ class Users::MultiFactorAuthenticationController < ApplicationController
   def verify_disabled
     totp = ROTP::TOTP.new(current_user.otp_secret)
 
-    if totp.verify_with_drift(params[:multi_factor_authentication][:otp_attempt], 600)
+    if totp.verify_with_drift(params[:multi_factor_authentication][:otp_attempt], current_user.class.otp_allowed_drift)
       current_user.otp_required_for_login = false
       current_user.save!
       redirect_to edit_user_registration_path, notice: t('.two_factor_authentication_disabled')
