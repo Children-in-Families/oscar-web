53ad9cccd5f05070fa53008080563b4e11e69244
Choengteur <choengteurchhoy@gmail.com>
2018-04-19 10:14:35 +0700

    add callback update permission user

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,13 +71,34 @@ class User < ActiveRecord::Base
   before_save  :set_manager_ids, if: 'manager_id_changed?'
   after_save :reset_manager, if: 'roles_changed?'
   after_create :build_permission
+  after_update :update_permission, if: 'roles_changed?'
+
+  def update_permission
+    unless self.admin? || self.strategic_overviewer?
+      self.create_permission
+
+      unless self.case_worker? || self.custom_field_permissions.empty?
+        CustomField.all.each do |cf|
+          self.custom_field_permissions.create(custom_field_id: cf.id)
+        end
+      end
+
+      unless self.program_stream_permissions.empty?
+        ProgramStream.all.each do |ps|
+          self.program_stream_permissions.create(program_stream_id: ps.id)
+        end
+      end
+    end
+  end
 
   def build_permission
     unless self.admin? || self.strategic_overviewer?
       self.create_permission
 
-      CustomField.all.each do |cf|
-        self.custom_field_permissions.create(custom_field_id: cf.id)
+      unless self.case_worker?
+        CustomField.all.each do |cf|
+          self.custom_field_permissions.create(custom_field_id: cf.id)
+        end
       end
 
       ProgramStream.all.each do |ps|
