6fdc3a803b38696a2e11c8e89d02a2c132ee2a50
sophal <caili.cl29@gmail.com>
2018-04-30 14:40:25 +0700

    remove country setting

--- spec/features/settings_spec.rb
+++ spec/features/settings_spec.rb
@@ -6,21 +6,6 @@ describe 'Settings' do
     login_as(admin)
   end
 
-  feature 'Country', js: true do
-    scenario 'default to Cambodia' do
-      visit root_path
-      expect(current_url).to include('country=cambodia')
-    end
-
-    scenario 'switch to Thailand' do
-      visit country_settings_path
-      find('.thumbnail#thailand').click
-
-      expect(current_url).to include('country=thailand')
-      expect(current_url).not_to include('country=cambodia')
-    end
-  end
-
   feature 'Assessment', js: true do
     scenario 'update assessment setting' do
       visit settings_path
