c193cf2280f854709bea41afa3555af6e1b45dea
Choengteur <choengteurchhoy@gmail.com>
2018-05-08 14:37:31 +0700

    update duration of overdue tasks, forms and assessments in remind manager mailer

--- app/mailers/remind_manager_mailer.rb
+++ app/mailers/remind_manager_mailer.rb
@@ -15,13 +15,13 @@ class RemindManagerMailer < ApplicationMailer
       overdue_assessments = []
       user.clients.active_accepted_status.each do |client|
         break if overdue_forms.present? || overdue_tasks.present? || overdue_assessments.present?
-        client_overdue_tasks = client.tasks.incomplete.exclude_exited_ngo_clients.of_user(user).incomplete.where('completion_date < ?', 7.days.ago)
+        client_overdue_tasks = client.tasks.incomplete.exclude_exited_ngo_clients.of_user(user).incomplete.where('completion_date <= ?', 7.days.ago)
         overdue_tasks << client_overdue_tasks if client_overdue_tasks.present?
-        overdue_assessments << client.next_assessment_date if client.next_assessment_date < 7.days.ago
+        overdue_assessments << client.next_assessment_date if client.next_assessment_date <= 7.days.ago
 
         custom_fields = client.custom_fields.where.not(frequency: '')
         custom_fields.each do |custom_field|
-          overdue_forms << custom_field.form_title if client.next_custom_field_date(client, custom_field) < 7.days.ago
+          overdue_forms << custom_field.form_title if client.next_custom_field_date(client, custom_field) <= 7.days.ago
         end
 
         client_active_enrollments = client.client_enrollments.active
@@ -29,7 +29,7 @@ class RemindManagerMailer < ApplicationMailer
           trackings = client_active_enrollment.trackings.where.not(frequency: '')
           trackings.each do |tracking|
             last_client_enrollment_tracking = client_active_enrollment.client_enrollment_trackings.last
-            overdue_forms << tracking.name if client.next_client_enrollment_tracking_date(tracking, last_client_enrollment_tracking) < 7.days.ago
+            overdue_forms << tracking.name if client.next_client_enrollment_tracking_date(tracking, last_client_enrollment_tracking) <= 7.days.ago
           end
         end
       end
