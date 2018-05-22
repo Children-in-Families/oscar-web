206b915cd785dbcae3a6fd24147258d72ba1cbd7
Pirun Seng <sengpirun.rain@gmail.com>
2018-05-22 14:27:28 +0700

    Fix active program stream filter spec

--- spec/classes/client_advanced_searches/client_association_filter_spec.rb
+++ spec/classes/client_advanced_searches/client_association_filter_spec.rb
@@ -41,7 +41,7 @@ describe AdvancedSearches::ClientAssociationFilter, 'Method' do
     end
 
     it 'return clients that filter with program_stream' do
-      client_filter = AdvancedSearches::ClientAssociationFilter.new(Client.all, 'program_stream', 'equal', program_stream.id).get_sql
+      client_filter = AdvancedSearches::ClientAssociationFilter.new(Client.all, 'active_program_stream', 'equal', program_stream.id).get_sql
 
       expect(client_filter[:id]).to include 'clients.id IN (?)'
       expect(client_filter[:values]).to include client.id
