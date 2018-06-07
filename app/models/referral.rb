573d686853db283e23f352f580117134da15d214
Pirun Seng <sengpirun.rain@gmail.com>
2018-06-07 14:16:59 +0700

    Refactor Referral info with policy and add more model specs

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -17,12 +17,30 @@ class Referral < ActiveRecord::Base
   after_save :make_a_copy_to_target_ngo
 
   scope :received, -> { where(referred_to: Organization.current.short_name) }
+  scope :delivered, -> { where(referred_from: Organization.current.short_name) }
   scope :unsaved, -> { where(saved: false) }
+  scope :saved, -> { where(saved: true) }
+  scope :received_and_saved, -> { received.saved }
+  scope :most_recents, -> { order(created_at: :desc) }
+
+  def non_oscar_ngo?
+    referred_to == 'external referral'
+  end
+
+  def referred_to_ngo
+    referred_to = Organization.find_by(short_name: self.referred_to).try(:full_name)
+    referred_to.present? ? referred_to : self.referred_to.titleize
+  end
+
+  def referred_from_ngo
+    Organization.find_by(short_name: self.referred_from).try(:full_name)
+  end
 
   private
 
   def check_saved_referral_in_target_ngo
     org = Organization.current
+    return if self.non_oscar_ngo?
     Organization.switch_to referred_to
     is_saved = Referral.find_by(slug: slug, date_of_referral: date_of_referral).try(:saved)
     Organization.switch_to org.short_name
@@ -31,7 +49,7 @@ class Referral < ActiveRecord::Base
 
   def set_referred_from
     current_org = Organization.current
-    return if current_org.short_name == referred_to || referred_to == "external referral"
+    return if current_org.short_name == referred_to
 
     referred_from = Organization.find_by(full_name: self.referred_from).try(:short_name)
     self.referred_from = referred_from
