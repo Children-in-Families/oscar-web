5f4137071e38c5721b98d8a89861380e3fa6d095
Pirun Seng <sengpirun.rain@gmail.com>
2018-06-19 19:02:23 +0700

    Try remove thump from uploader

--- app/uploaders/consent_form_uploader.rb
+++ app/uploaders/consent_form_uploader.rb
@@ -9,19 +9,7 @@ class ConsentFormUploader < CarrierWave::Uploader::Base
     'image-placeholder.png'
   end
 
-  version :thumb, if: :image? do
-    process resize_to_fill: [100, 100]
-  end
-
   def extension_white_list
     %w(jpg jpeg png doc docx xls xlsx pdf)
-  end
-
-  protected
-
-  def image?(new_file)
-    if new_file.content_type.present?
-      new_file.content_type.start_with? 'image'
-    end
   end
 end
