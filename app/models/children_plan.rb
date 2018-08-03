0f1d41060276061e7c0eb4f414e280dae3c1c56b
Pirun Seng <sengpirun.rain@gmail.com>
2018-08-03 14:32:35 +0700

    Fix seeds and layout

--- app/models/children_plan.rb
+++ app/models/children_plan.rb
@@ -5,4 +5,6 @@ class ChildrenPlan < ActiveRecord::Base
   has_many :government_forms, through: :government_form_children_plans
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  default_scope { order(:created_at) }
 end
--- app/models/family_plan.rb
+++ app/models/family_plan.rb
@@ -5,4 +5,6 @@ class FamilyPlan < ActiveRecord::Base
   has_many :government_forms, through: :government_form_family_plans
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  default_scope { order(:created_at) }
 end
