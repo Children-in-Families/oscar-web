e59e73b787987abe0bfddc88b998d5d1ef980c7e
Kanitha <kanitha@Kanitha.local>
2018-08-07 09:54:08 +0700

    fix migration

--- db/migrate/20180731014745_change_government_form_fields.rb
+++ db/migrate/20180731014745_change_government_form_fields.rb
@@ -1,10 +1,10 @@
 class ChangeGovernmentFormFields < ActiveRecord::Migration
   def change
-    remove_column :government_forms, :interview_commune
-    remove_column :government_forms, :interview_village
-    remove_column :government_forms, :assessment_commune
-    remove_column :government_forms, :primary_carer_commune
-    remove_column :government_forms, :primary_carer_village
+    remove_column :government_forms, :interview_commune, :integer
+    remove_column :government_forms, :interview_village, :integer
+    remove_column :government_forms, :assessment_commune, :integer
+    remove_column :government_forms, :primary_carer_commune, :integer
+    remove_column :government_forms, :primary_carer_village, :integer
 
     add_column :government_forms, :interview_commune_id, :integer
     add_column :government_forms, :interview_village_id, :integer
