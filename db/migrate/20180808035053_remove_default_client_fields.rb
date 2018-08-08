0db142eb67c95c4ce5482ea598e508067ea28346
Choengteur <choengteurchhoy@gmail.com>
2018-08-08 14:53:00 +0700

    update migration remove default client fields

--- db/migrate/20180808035053_remove_default_client_fields.rb
+++ db/migrate/20180808035053_remove_default_client_fields.rb
@@ -1,6 +1,11 @@
 class RemoveDefaultClientFields < ActiveRecord::Migration
-  def change
+  def up
     change_column_default :clients, :has_been_in_orphanage, nil
     change_column_default :clients, :has_been_in_government_care, nil
+  end
+
+  def down
+    change_column_default :clients, :has_been_in_orphanage, false
+    change_column_default :clients, :has_been_in_government_care, false
   end
 end
