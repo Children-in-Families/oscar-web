47594b3766474b9773762348d94e60cc1600c93e
Choengteur <choengteurchhoy@gmail.com>
2018-08-10 09:42:31 +0700

    add validation age CSI

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -7,6 +7,7 @@ class Setting < ActiveRecord::Base
   validates_numericality_of :max_assessment, only_integer: true, greater_than: 3, if: -> { max_assessment.present? && assessment_frequency == 'month' }
   validates_numericality_of :max_assessment, only_integer: true, greater_than: 0, if: -> { max_assessment.present? && assessment_frequency == 'year' }
   validates_numericality_of :max_case_note, only_integer: true, greater_than: 0, if: -> { max_case_note.present? }
+  validates_numericality_of :age, only_integer: true, greater_than: 0, less_than_or_equal_to: 100 , if: -> { age.present? }
   # validates_numericality_of :min_assessment, less_than: :max_assessment, if: -> { max_assessment.present? && min_assessment.present?}
   # validates_numericality_of :max_assessment, greater_than: :min_assessment, if: -> { max_assessment.present? && min_assessment.present?}
   validates :max_case_note, presence: true, if: -> { case_note_frequency.present? }
