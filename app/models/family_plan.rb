889507a2c45a637c6582d2bfb887b187678f1e21
kirykr <kirymeas@gmail.com>
2018-08-22 16:38:28 +0700

    updated depend destroy to government_form_family_plans in family plan

--- app/models/family_plan.rb
+++ app/models/family_plan.rb
@@ -1,7 +1,7 @@
 class FamilyPlan < ActiveRecord::Base
   has_paper_trail
 
-  has_many :government_form_family_plans, dependent: :restrict_with_error
+  has_many :government_form_family_plans, dependent: :destroy
   has_many :government_forms, through: :government_form_family_plans
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
