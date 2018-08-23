3418e01b4c8729865af57fba35639c9041b68343
Pirun Seng <sengpirun.rain@gmail.com>
2018-08-23 14:11:49 +0700

    Add jobs for importing

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,3 +13,21 @@ every :month, at: '00:00 am' do
   rake 'ngo_usage_report:generate', output: 'log/whenever.log'
   # rake 'staff_monthly_report:generate', output: 'log/whenever.log'
 end
+
+every :day, at: '07:30 am' do
+  rake 'import_villages_to_cif_until_cct:start', output: 'log/whenever.log'
+end
+
+every :day, at: '08:00 am' do
+  rake 'import_villages_to_mtp_until_wmo:start', output: 'log/whenever.log'
+end
+
+every :day, at: '08:30 am' do
+  rake 'import_villages_to_agh_until_voice:start', output: 'log/whenever.log'
+end
+
+every :day, at: '09:00 am' do
+  rake 'import_villages_to_mho_until_kmr:start', output: 'log/whenever.log'
+end
+
+# TODO, remove tasks and update crontab when done.
