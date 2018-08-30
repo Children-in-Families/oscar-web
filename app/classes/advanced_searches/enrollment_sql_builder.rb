dc77cfe7ec699b5f6b53620d9f8dfd37d86506df
Choengteur Chhoy <choengteurchhoy@gmail.com>
2018-08-30 14:11:10 +0700

    Radio Group selects one as default (#1162)

--- app/classes/advanced_searches/enrollment_sql_builder.rb
+++ app/classes/advanced_searches/enrollment_sql_builder.rb
@@ -46,9 +46,17 @@ module AdvancedSearches
       when 'not_contains'
         properties_result = client_enrollments.where("properties ->> '#{@field}' NOT ILIKE '%#{@value.squish}%' ")
       when 'is_empty'
-        properties_result = client_enrollments.where("properties -> '#{@field}' ? '' ")
+        if @type == 'checkbox'
+          properties_result = client_enrollments.where("properties -> '#{@field}' ? ''")
+        else
+          properties_result = client_enrollments.where("properties -> '#{@field}' ? '' OR properties -> '#{@field}' IS NULL")
+        end
       when 'is_not_empty'
-        properties_result = client_enrollments.where.not("properties -> '#{@field}' ? '' ")
+        if @type == 'checkbox'
+          properties_result = client_enrollments.where.not("properties -> '#{@field}' ? ''")
+        else
+          properties_result = client_enrollments.where.not("properties -> '#{@field}' ? '' OR properties -> '#{@field}' IS NULL")
+        end
       when 'between'
         properties_result = client_enrollments.where("(properties ->> '#{@field}')#{ '::numeric' if integer? } BETWEEN '#{@value.first}' AND '#{@value.last}' AND properties ->> '#{@field}' != ''")
       end
