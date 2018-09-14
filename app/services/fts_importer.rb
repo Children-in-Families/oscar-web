3c760914c640930050bdaa0540b02b945472ab52
Choengteur <choengteurchhoy@gmail.com>
2018-09-14 13:27:36 +0700

    update client of user Kongkea and remove duplicate clients

--- app/services/fts_importer.rb
+++ app/services/fts_importer.rb
@@ -21,6 +21,8 @@ module FtsImporter
       ((workbook.first_row + 1)..workbook.last_row).each do |row|
         family_name           = workbook.row(row)[headers['Family Name (English)']].squish
         given_name            = workbook.row(row)[headers['Given Name (English)']].squish
+        dob                   = workbook.row(row)[headers['Date of Birth']]
+        case_work_name        = workbook.row(row)[headers['* Case Worker / Staff']]
         current_province        = Province.find_by("name ilike ?", "%#{workbook.row(row)[headers['Current Province']].squish}%") if workbook.row(row)[headers['Current Province']].present?
         district                = current_province.districts.find_by("name ilike ?", "%#{workbook.row(row)[headers['Address - District/Khan']].squish}%") if workbook.row(row)[headers['Address - District/Khan']].present? && current_province.present?
         commune                 = district.communes.find_by(name_en: workbook.row(row)[headers['Address - Commune/Sangkat']].squish) if district.present? && workbook.row(row)[headers['Address - Commune/Sangkat']].present?
@@ -28,21 +30,27 @@ module FtsImporter
         code                    = workbook.row(row)[headers['Family ID']]
         if (given_name == 'Makara' && family_name == 'Meurn') || (given_name == 'Norng' && family_name == 'Poy') || (given_name == 'Pok' && family_name == 'Panha') || (given_name == 'Buk' && family_name == 'Pa') || (given_name == 'Heng' && family_name == 'Kan')
           clients = Client.given_name_like(given_name).family_name_like(family_name)
-          clients.each do |client|
-            client.code = code if code.present?
-            client.province = current_province if current_province.present?
-            client.district = district if district.present? && current_province.present?
-            client.commune = commune if commune.present? && district.present? && current_province.present?
-            client.village = village if village.present? && commune.present? && district.present? && current_province.present?
-            client.save(validate: false)
-          end
-        else
-          client = Client.given_name_like(given_name).family_name_like(family_name).first
+          clients.last.destroy if clients.count > 1
+          client = clients.first
           client.code = code if code.present?
           client.province = current_province if current_province.present?
           client.district = district if district.present? && current_province.present?
           client.commune = commune if commune.present? && district.present? && current_province.present?
           client.village = village if village.present? && commune.present? && district.present? && current_province.present?
+          client.save(validate: false)
+        else
+          client = Client.given_name_like(given_name).family_name_like(family_name).find_by(date_of_birth: dob)
+          client.code = code if code.present?
+          client.province = current_province if current_province.present?
+          client.district = district if district.present? && current_province.present?
+          client.commune = commune if commune.present? && district.present? && current_province.present?
+          client.village = village if village.present? && commune.present? && district.present? && current_province.present?
+          client.save(validate: false)
+        end
+        if case_work_name == 'Kongkea '
+          client = Client.given_name_like(given_name).family_name_like(family_name).find_by(date_of_birth: dob)
+          user = User.find_by(first_name: 'Kongkea')
+          client.user_ids = [user.id]
           client.save(validate: false)
         end
       end
@@ -56,7 +64,7 @@ module FtsImporter
         client_family_name  = workbook.row(row)[headers['Last Name']].squish
         client_given_name   = workbook.row(row)[headers['*Name']].squish
         donor = Donor.find_or_create_by(name: name, code: name)
-        client = Client.given_name_like(client_given_name).family_name_like(client_family_name).first
+        client = Client.find_by(given_name: client_given_name, family_name: client_family_name)
         if client.donors.empty?
           donor_ids = [donor.id]
           client.donor_ids = donor_ids
@@ -73,7 +81,7 @@ module FtsImporter
         family_name           = workbook.row(row)[headers['FAMILY NAME']].squish
         given_name            = workbook.row(row)[headers['FIRST NAME']].squish
         user                  = User.find_by(first_name: 'Vichheka', last_name: 'Kim')
-        client = Client.given_name_like(given_name).family_name_like(family_name).first
+        client = Client.find_by(given_name: given_name, family_name: family_name)
         client.user_ids = [user.id]
         client.save(validate: false)
       end
