a183e3182cc3d7a802b424619ebf1572f85be33a
Pirun Seng <sengpirun.rain@gmail.com>
2018-09-19 14:10:31 +0700

    Remove FamilyPlan default scope

--- app/models/government_form.rb
+++ app/models/government_form.rb
@@ -79,6 +79,7 @@ class GovernmentForm < ActiveRecord::Base
 
   def populate_family_plans
     skipped_plans = ['កម្រិតសិក្សាអប់រំ', 'កូនៗផ្សេងទៀតដែលអាចជួយបាន', 'ទំនាក់ទំនងសង្គម']
+
     FamilyPlan.order(:priority).each do |plan|
       next if skipped_plans.include?(plan.name)
       government_form_family_plans.build(family_plan: plan)
@@ -89,6 +90,7 @@ class GovernmentForm < ActiveRecord::Base
     form_two = ['ចំណេះដឹងទូទៅក្នុងសង្គម', 'កូនៗផ្សេងទៀតដែលអាចជួយគ្រួសារបាន']
     form_six = [*form_two, 'សុខភាពផ្លូវចិត្ត', 'កម្រិតសិក្សាអប់រំ']
     skipped_plans = form == 'two' ? form_two : form_six
+
     FamilyPlan.order(:priority).each do |status|
       next if skipped_plans.include?(status.name)
       government_form_family_plans.build(family_status: status)
