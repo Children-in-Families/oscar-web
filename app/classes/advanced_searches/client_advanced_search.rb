954985ffde6759d99534518b444202a95f49978e
kirykr <kirymeas@gmail.com>
2018-10-04 11:43:16 +0700

    clean up commented lines

--- app/classes/advanced_searches/client_advanced_search.rb
+++ app/classes/advanced_searches/client_advanced_search.rb
@@ -12,15 +12,12 @@ module AdvancedSearches
       client_base_sql     = AdvancedSearches::ClientBaseSqlBuilder.new(@clients, @basic_rules).generate
 
       query_array << client_base_sql[:sql_string]
-      # client_ids = overdue_assessment_clients if @overdue_assessment == 'true'
       client_base_sql[:values].each{ |v| query_array << v }
 
       if client_base_sql[:sql_string].first.present?
         rules = @basic_rules["rules"].reject {|hash_value| hash_value["id"] != "active_program_stream" }
         operators = rules.map{|value| value["operator"] }.uniq
         if @basic_rules["condition"] == "AND" && rules.count > 1 && operators.sort == ["not_equal", "equal"].sort
-          # client_ids = @clients.joins(:client_enrollments).where(query_array).group(:id).having('count(client_enrollments) = 1').ids
-
           excluded_client_ids = rules.map{|rule| rule['value'] if rule['operator'] == 'not_equal'}
           clients = @clients.joins(:client_enrollments).where(client_enrollments: { status: 'Active' }).where(query_array).reject do |client|
             client_enrollment_ids = client.client_enrollments.map(&:program_stream_id)
