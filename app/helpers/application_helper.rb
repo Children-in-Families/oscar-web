4b99020f8d81183fb8e2448474d99085c6b29c21
Pirun Seng <sengpirun.rain@gmail.com>
2018-12-07 13:47:26 +0700

    Fix csi setting helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -291,6 +291,10 @@ module ApplicationHelper
   end
 
   def enable_all_csi_tools?
+    Setting.first.enable_default_assessment && Setting.first.enable_custom_assessment
+  end
+
+  def enable_any_csi_tools?
     Setting.first.enable_default_assessment || Setting.first.enable_custom_assessment
   end
 end
