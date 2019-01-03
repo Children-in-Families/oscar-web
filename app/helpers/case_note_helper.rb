51c5796f49253e1fee1277753adc28598e5a4c5f
JANE LY <chimkanitha@gmail.com>
2019-01-03 10:03:43 +0700

    handle button disable when custom domain empty

--- app/helpers/case_note_helper.rb
+++ app/helpers/case_note_helper.rb
@@ -28,7 +28,7 @@ module CaseNoteHelper
   end
 
   def new_custom_link
-    if case_notes_editable? && policy(@client).create?
+    if case_notes_editable? && policy(@client).create? && Domain.custom_csi_domains.present?
       link_to new_client_case_note_path(@client, custom: true) do
         @current_setting.custom_assessment
       end
