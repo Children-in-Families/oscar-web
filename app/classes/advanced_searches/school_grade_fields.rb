896255548d5287e170abfadf77c60caac0b03bdd
Choengteur <choengteurchhoy@gmail.com>
2019-01-21 10:05:38 +0700

    fix show description Birth Province, Exit Circumstance, Gender, Has the client lived in orphanage/government care, Is the client rated for id poor, School grade and domain score

--- app/classes/advanced_searches/school_grade_fields.rb
+++ app/classes/advanced_searches/school_grade_fields.rb
@@ -3,7 +3,7 @@ module AdvancedSearches
     extend AdvancedSearchHelper
 
     def self.render
-      school_grade  = ['School Grade'].map { |item| drop_list_options('school_grade', format_header('school_grade'), Client::GRADES, format_header('basic_fields')) }
+      school_grade  = ['School Grade'].map { |item| drop_list_options('school_grade', format_header('school_grade'), school_grade_options, format_header('basic_fields')) }
       school_grade
     end
 
@@ -21,6 +21,11 @@ module AdvancedSearches
         data: { values: values },
         operators: ['equal', 'not_equal', 'is_empty', 'is_not_empty', 'between']
       }
+    end
+
+
+    def self.school_grade_options
+      Client::GRADES.map{|s| { s => s }  }
     end
   end
 end
