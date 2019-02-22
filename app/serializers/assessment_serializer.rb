be9f395e35f2249b4e3e09e96185c89604ed1b82
sophal <caili.cl29@gmail.com>
2019-02-22 14:35:32 +0700

    add default field to assessments api

--- app/serializers/assessment_serializer.rb
+++ app/serializers/assessment_serializer.rb
@@ -1,9 +1,13 @@
 class AssessmentSerializer < ActiveModel::Serializer
-  attributes :id, :client_id, :created_at, :updated_at, :case_notes, :completed
+  attributes :id, :client_id, :created_at, :updated_at, :case_notes, :completed, :assessment_domain
 
-  has_many :assessment_domains
+  # has_many :assessment_domains
 
   def case_notes
     ActiveModel::ArraySerializer.new(object.case_notes, each_serializer: CaseNoteSerializer)
+  end
+
+  def assessment_domain
+    object.assessment_domains
   end
 end
