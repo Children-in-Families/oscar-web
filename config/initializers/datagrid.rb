78ffc423d40f730670f4803492d05bb0a453dd71
kanitha <kanitha@Sila.local>
2019-02-25 15:56:38 +0700

    custom excel cell date format

--- config/initializers/datagrid.rb
+++ config/initializers/datagrid.rb
@@ -5,9 +5,16 @@ Datagrid.module_eval do
     book = Spreadsheet::Workbook.new
     book.create_worksheet
     book.worksheet(0).insert_row(0, self.header(*column_names))
+    date_format = Spreadsheet::Format.new :number_format => 'DD/MM/YYYY'
 
     each_with_batches.with_index do |asset, index|
       book.worksheet(0).insert_row (index + 1), row_for(asset, *column_names).map(&:to_s)
+    end
+
+    header.each_with_index do |value, index|
+      if value.downcase.include?('date') || value.downcase.include?('កាលបរិច្ឆេទ') || value.downcase.include?('ថ្ងៃ') || value.downcase.include?('ေမြးဖြာသည့္') || value.downcase.include?('နေ့စှဲ')
+        book.worksheet(0).column(index).default_format = date_format
+      end
     end
 
     buffer = StringIO.new
