1fb9484c55731de14e45e985df741a9770a38961
Pirun Seng <sengpirun.rain@gmail.com>
2019-02-25 16:44:26 +0700

    Update pin number to be exactly 5 characters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,7 @@ describe User, 'validations' do
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_inclusion_of(:roles).in_array(User::ROLES) }
   it { is_expected.to validate_presence_of(:gender) }
-  it { is_expected.to validate_length_of(:pin_code).is_at_most(5) }
+  it { is_expected.to validate_length_of(:pin_code).is_equal_to(5) }
   it { is_expected.to validate_numericality_of(:pin_code).is_greater_than_or_equal_to(0) }
   it { is_expected.to allow_value('').for(:pin_code) }
 end
