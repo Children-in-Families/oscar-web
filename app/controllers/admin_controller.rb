415b11350b590c08650564b606532f34244dd625
Pirun Seng <sengpirun.rain@gmail.com>
2019-02-26 14:04:49 +0700

    Fix login that requires user to do it twice (#1342)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,10 @@
 class AdminController < ApplicationController
   protect_from_forgery with: :exception
 
-  before_action :remove_remember_me, :authenticate_user!
+  before_action :authenticate_user!
   before_action :notify_user, :set_sidebar_basic_info
 
   protected
-
-  def remove_remember_me
-    if user_signed_in? && current_user.last_sign_in_at < 1.week.ago
-      current_user.forget_me!
-      sign_out current_user
-    end
-  end
 
   def notify_user
     clients = Client.accessible_by(current_ability).non_exited_ngo
