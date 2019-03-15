b328b35a92b1caa751f3e49e1f3ec66b5ce2e56d
sophal <caili.cl29@gmail.com>
2019-03-15 15:52:44 +0700

    rework on birth province api

--- app/controllers/api/v1/birth_provinces_controller.rb
+++ app/controllers/api/v1/birth_provinces_controller.rb
@@ -2,20 +2,13 @@ module Api
   module V1
     class BirthProvincesController < Api::V1::BaseApiController
       def index
-        birth_province_ids = Client.pluck(:birth_province_id)
-        birth_provinces = Province.where(id: birth_province_ids)
-        
-        render json: order_by_countries(birth_provinces)
-      end
+        current_org = Organization.current.short_name
+        Organization.switch_to 'shared'
+        countries = ['Cambodia', 'Thailand', 'Myanmar', 'Lesotho', 'Uganda']
+        provinces = countries.map{ |country| { country: country, provinces: Province.country_is(country.downcase) } }
+        Organization.switch_to current_org
 
-      def order_by_countries(provinces)
-        {
-          'Cambodia': provinces.country_is('cambodia'),
-          'Thailand': provinces.country_is('thailand'),
-          'Myanmar': provinces.country_is('myanmar'),
-          'Lesotho': provinces.country_is('lesotho'),
-          'Uganda': provinces.country_is('uganda'),
-        }
+        render json: provinces
       end
     end
   end
