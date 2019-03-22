e616c2401abd8f3bc4bce98524b3c27050d4d93a
JANE LY <chimkanitha@gmail.com>
2019-03-22 11:12:30 +0700

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -1583,12 +1583,8 @@ ActiveRecord::Schema.define(version: 20190322023721) do
     t.string   "gender",                         default: ""
     t.boolean  "enable_gov_log_in",              default: false
     t.boolean  "enable_research_log_in",         default: false
-    t.datetime "deleted_at"
-    t.datetime "activated_at"
-    t.datetime "deactivated_at"
   end
 
-  add_index "users", ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["organization_id"], name: "index_users_on_organization_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
