6b8d3136788f8275f3c0c45853038a417dc8a57d
JANE LY <chimkanitha@gmail.com>
2019-03-22 10:38:24 +0700

    add relation between task and case note

--- app/controllers/concerns/create_bulk_task.rb
+++ app/controllers/concerns/create_bulk_task.rb
@@ -1,10 +1,10 @@
 module CreateBulkTask
 
-  def create_bulk_task(task_in_params)
+  def create_bulk_task(task_in_params, parent_id=nil)
     task_attr = task_in_params.map do |task|
       task_attr = JSON.parse(task)
       task_attr['name'] = task_attr['name'].gsub('qout', '"').gsub('apos', "'")
-      task_attr.merge(user_id: current_user.id)
+      task_attr.merge('case_note_id'=> "#{parent_id}", user_id: current_user.id)
     end
 
     tasks = @client.tasks.create(task_attr)
