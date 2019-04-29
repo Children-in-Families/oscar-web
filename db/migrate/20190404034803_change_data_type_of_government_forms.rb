5e49ae0da9444aef32e3f293426ee83c396859a5
measbona <measbona3@gmail.com>
2019-04-29 13:47:53 +0700

    change datatype and custom field char

--- db/migrate/20190404034803_change_data_type_of_government_forms.rb
+++ db/migrate/20190404034803_change_data_type_of_government_forms.rb
@@ -1,6 +1,8 @@
 class ChangeDataTypeOfGovernmentForms < ActiveRecord::Migration
   def up
     change_column :government_forms, :source_info, :text, :default => ''
+    change_column :government_forms, :summary_info_of_referral, :text, :default => ''
+    change_column :government_forms, :guardian_comment, :text, :default => ''
     change_column :government_forms, :case_worker_comment, :text, :default => ''
     change_column :government_form_children_plans, :goal, :text, :default => ''
     change_column :government_form_children_plans, :action, :text, :default => ''
@@ -12,6 +14,8 @@ class ChangeDataTypeOfGovernmentForms < ActiveRecord::Migration
 
   def down
     change_column :government_forms, :source_info, :string, :default => ''
+    change_column :government_forms, :summary_info_of_referral, :string, :default => ''
+    change_column :government_forms, :guardian_comment, :string, :default => ''
     change_column :government_forms, :case_worker_comment, :string, :default => ''
     change_column :government_form_children_plans, :goal, :string, :default => ''
     change_column :government_form_children_plans, :action, :string, :default => ''
