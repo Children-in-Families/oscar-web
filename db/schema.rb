76be49fe1d841ad06680a92fba0b3bbf5502e45f
JANE LY <chimkanitha@gmail.com>
2019-05-09 10:19:06 +0700

    add index to duplicate checker column 165844833

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190508022540) do
+ActiveRecord::Schema.define(version: 20190509031724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1235,6 +1235,7 @@ ActiveRecord::Schema.define(version: 20190508022540) do
     t.string   "duplicate_checker"
   end
 
+  add_index "shared_clients", ["duplicate_checker"], name: "index_shared_clients_on_duplicate_checker", using: :btree
   add_index "shared_clients", ["slug"], name: "index_shared_clients_on_slug", unique: true, using: :btree
 
   create_table "sponsors", force: :cascade do |t|
