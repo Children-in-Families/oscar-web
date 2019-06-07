fa4cd9e72172c57f2e8efdbbf4202d2488ee0d13
JANE LY <chimkanitha@gmail.com>
2019-06-07 14:09:48 +0700

    api translation to only clients families and users

--- app/controllers/api/v1/translations_controller.rb
+++ app/controllers/api/v1/translations_controller.rb
@@ -7,10 +7,22 @@ module Api
       def translation
         if ['en', 'km', 'my'].include?(params[:lang])
           yml = File.open(Rails.root.join("config/locales/#{params[:lang]}.yml"), 'r').read
-          render json: JSON.dump(YAML::load(yml))
+          json = JSON.parse(JSON.dump(YAML::load(yml)))
+          translates = {
+            "clients" => json["#{params[:lang]}"]["clients"],
+            "families" => json["#{params[:lang]}"]["families"],
+            "users" => json["#{params[:lang]}"]["users"]
+          }
+          render json: translates
         else
           yml = File.open(Rails.root.join("config/locales/en.yml"), 'r').read
-          render json: JSON.dump(YAML::load(yml))
+          json = JSON.parse(JSON.dump(YAML::load(yml)))
+          translates = {
+            "clients" => json["en"]["clients"],
+            "families" => json["en"]["families"],
+            "users" => json["en"]["users"]
+          }
+          render json: translates
         end
       end
     end
