92d7ce2c9ee81b787b7ca1749e0bf9ec3537d267
kirykr <kirymeas@gmail.com>
2019-06-20 22:23:10 +0700

    will refactore the table header

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -10,27 +10,8 @@ module Api
     end
 
     def assessments
-      assessment_domain_hash = {}
-      client_data = []
-      # clients = Client.where(id: params[:client_ids].split('/'))
-      domains = params[:default] == 'true' ? Domain.csi_domains.ids : Domain.custom_csi_domains.ids
-      assessments = Assessment.includes(:assessment_domains, :client).where(assessments: { default: params[:default] }, client_id: params[:client_ids].split('/')).where.not(client_id: nil)
-      assessments_count = assessments.count
-
-      assessment_data = params[:length] != '-1' ? assessments.page(page).per(per_page) : assessments
-
-      assessment_data.each do |assessment|
-        assessment_domain_hash = assessment.assessment_domains.pluck(:domain_id, :score).to_h if assessment.assessment_domains.present?
-        domain_scores = domains.map { |domain_id| assessment_domain_hash.present? ? ["domain_#{domain_id}", assessment_domain_hash[domain_id]] : ["domain_#{domain_id}", ''] }
-        client_hash = { slug: assessment.client.slug,
-          name: assessment.client.en_and_local_name,
-          'assessment-number': assessment.client.assessments.count, date: assessment.created_at.strftime('%d %B %Y')
-        }
-        client_hash.merge!(domain_scores.to_h)
-        client_data << client_hash
-      end
-
-      render json: { recordsTotal:  assessments_count, recordsFiltered: assessments_count, data: client_data }
+      @assessments_count ||= Assessment.joins(:client).where(default: params[:default]).count
+      render json: { recordsTotal:  @assessments_count, recordsFiltered: @assessments_count, data: data }
     end
 
     private
@@ -47,12 +28,52 @@ module Api
       { text: "#{@csi_statistics} & #{@enrollments_statistics}" }
     end
 
+    def data
+      assessment_domain_hash = {}
+      client_data = []
+      assessment_data.each do |assessment|
+        assessment_domain_hash = assessment.assessment_domains.pluck(:domain_id, :score).to_h if assessment.assessment_domains.present?
+        domain_scores = domains.ids.map { |domain_id| assessment_domain_hash.present? ? ["domain_#{domain_id}", assessment_domain_hash[domain_id]] : ["domain_#{domain_id}", ''] }
+        client_hash = { slug: assessment.client.slug,
+          name: assessment.client.en_and_local_name,
+          'assessment-number': assessment.client.assessments.count, date: assessment.created_at.strftime('%d %B %Y')
+        }
+        client_hash.merge!(domain_scores.to_h)
+        client_data << client_hash
+      end
+      client_data
+    end
+
+    def assessment_data
+      @assessments ||= fetch_assessments
+    end
+
+    def fetch_assessments
+      assessments = Assessment.includes(:assessment_domains, :client).where(assessments: { default: params[:default] }, client_id: params[:client_ids].split('/'))
+      assessments = assessments.joins(:client).order("#{sort_column} #{sort_direction}")
+      assessment_data = params[:length] != '-1' ? assessments.page(page).per(per_page) : assessments
+    end
+
+    def domains
+      @domains ||= params[:default] == 'true' ? Domain.csi_domains : Domain.custom_csi_domains
+    end
+
     def page
       params[:start].to_i/per_page + 1
     end
 
     def per_page
       params[:length].to_i > 0 ? params[:length].to_i : 10
+    end
+
+    def sort_column
+      domains_fields = domains.map { |domain|  "assessment_domains.score" }
+      columns = ["regexp_replace(clients.slug, '\\D*', '', 'g')::int", "clients.given_name", "clients.assessments_count", "assessments.created_at", *domains_fields]
+      columns[params[:order]['0']['column'].to_i]
+    end
+
+    def sort_direction
+      params[:order]['0']['dir'] == "desc" ? "desc" : "asc"
     end
   end
 end
