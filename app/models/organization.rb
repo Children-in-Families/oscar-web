cbde177be88da57c1717a96ed89580ded414dd5c
JANE LY <chimkanitha@gmail.com>
2019-07-01 09:24:20 +0700

    hide demo org temporary

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -8,7 +8,7 @@ class Organization < ActiveRecord::Base
   scope :without_shared, -> { where.not(short_name: 'shared') }
   scope :exclude_current, -> { where.not(short_name: Organization.current.short_name) }
   scope :oscar, -> { visible.where.not(short_name: 'demo') }
-  scope :visible, -> { where.not(short_name: ['cwd', 'myan', 'rok', 'shared', 'my', 'tutorials']) }
+  scope :visible, -> { where.not(short_name: ['cwd', 'myan', 'rok', 'shared', 'my', 'tutorials', 'demo']) }
   scope :cambodian, -> { where(country: 'cambodia') }
   scope :skip_dup_checking_orgs, -> { where(short_name: ['demo', 'cwd', 'myan', 'rok', 'my']) }
 
