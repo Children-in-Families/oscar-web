1f8a6f37d888693bdc6763c28c6ea78bf092cc42
Kiry Meas <kirymeas@gmail.com>
2019-07-03 15:10:46 +0700

    Add mulit client to casework 166230221 (#1422)

--- app/models/client.rb
+++ app/models/client.rb
@@ -228,6 +228,10 @@ class Client < ActiveRecord::Base
     "#{local_family_name} #{local_given_name}"
   end
 
+  def latin_name_with_id
+    "#{given_name} #{family_name} (#{id})"
+  end
+
   def next_assessment_date(user_activated_date = nil)
     return Date.today if assessments.defaults.empty?
     return nil if user_activated_date.present? && assessments.defaults.latest_record.created_at < user_activated_date
