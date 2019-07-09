a82e3d32dc82348adf4adaf176be9b33311d98fc
JANE LY <chimkanitha@gmail.com>
2019-07-09 11:30:22 +0700

    add program stream service to tutorial ngo

--- app/services/import_static_service.rb
+++ app/services/import_static_service.rb
@@ -10,18 +10,18 @@ module ImportStaticService
     def import
       column_letters = ('A'..'Z').to_a
       header_letters = column_letters[0..12]
-      Organization.all.pluck(:short_name).each do |short_name|
-        Organization.switch_to short_name
-        header_letters.each do |letter|
-          fields = workbook.column(letter)
-          values = fields.compact
-          value  = values.shift
-          service = Service.find_or_create_by(name: value)
-          values.each do |name|
-            Service.find_or_create_by(name: name, parent_id: service.id)
-          end
+      # Organization.all.pluck(:short_name).each do |short_name|
+      Organization.switch_to 'tutorials'
+      header_letters.each do |letter|
+        fields = workbook.column(letter)
+        values = fields.compact
+        value  = values.shift
+        service = Service.find_or_create_by(name: value)
+        values.each do |name|
+          Service.find_or_create_by(name: name, parent_id: service.id)
         end
       end
+      # end
     end
   end
 end
