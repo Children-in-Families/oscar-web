72879d90fc55a2f59d2885ba7509402b8847d3c0
kirykr <kirymeas@gmail.com>
2019-07-18 10:14:26 +0700

    remove pry

--- app/helpers/case_note_helper.rb
+++ app/helpers/case_note_helper.rb
@@ -28,7 +28,6 @@ module CaseNoteHelper
   end
 
   def new_custom_link
-    binding.pry
     if case_notes_editable? && policy(@client).create?
       link_to new_client_case_note_path(@client, custom: true) do
         @current_setting.custom_assessment
