9758276dcc02e02d19f0c527dedbc74f1c4c5c80
JANE LY <chimkanitha@gmail.com>
2019-07-23 08:51:52 +0700

    update initial referral date 166432524

---
+++ app/services/cfi_initial_referral_date.rb
@@ -0,0 +1,32 @@
+module CfiInitialReferralDate
+  class Import
+    attr_accessor :path, :headers, :workbook
+
+    def initialize(sheet_name, path = 'vendor/data/cfi_initial_referral_date.xlsx')
+      @path     = path
+      @workbook = Roo::Excelx.new(path)
+
+      sheet_index = workbook.sheets.index(sheet_name)
+      workbook.default_sheet = workbook.sheets[sheet_index]
+      sheet_header
+    end
+
+    def sheet_header
+      @headers = Hash.new
+      workbook.row(1).each_with_index { |header, i| headers[header] = i }
+    end
+
+    def initial_referral_date
+      ((workbook.first_row + 1)..workbook.last_row).each do |row|
+        id = workbook.row(row)[headers['ID']]
+        date = workbook.row(row)[headers['Initial Referral Date']]
+        client = Client.find_by(slug: id.squish)
+        client.update(initial_referral_date: date)
+
+        puts "#{client.id} - #{client.initial_referral_date}"
+      end
+
+      puts 'Done!'
+    end
+  end
+end
