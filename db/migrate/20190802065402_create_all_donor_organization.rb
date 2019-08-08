f19d2fe6189c93b6800dbf4faa0d60ffd9d0bfbb
kirykr <kirymeas@gmail.com>
2019-08-08 15:24:20 +0700

    remove unused method

--- db/migrate/20190802065402_create_all_donor_organization.rb
+++ db/migrate/20190802065402_create_all_donor_organization.rb
@@ -8,7 +8,6 @@ class CreateAllDonorOrganization < ActiveRecord::Migration
     reversible do |dir|
       dir.up do
         if schema_search_path == "\"public\""
-          # create_all_donors
           execute <<-SQL.squish
             DO
             $do$
@@ -82,36 +81,6 @@ class CreateAllDonorOrganization < ActiveRecord::Migration
             DROP USER IF EXISTS "#{ENV['READ_ONLY_DATABASE_USER']}";
           SQL
         end
-      end
-    end
-  end
-
-  def create_all_donors
-    donors = Organization.pluck(:id, :short_name).map do |org_id, short_name|
-      Organization.switch_to short_name
-      Donor.all.map do |donor|
-        [[org_id, short_name], [donor.id, donor.name]]
-      end
-    end
-
-    donor_names = donors.reject(&:blank?).map do |group|
-      group.flatten(1)[1..-1].map(&:last)
-    end
-
-    donor_values = donor_names.flatten.uniq.map{ |donor_name| { name: donor_name.strip } }
-
-    Organization.switch_to 'public'
-
-    org_donors = donors.reject(&:blank?).map do |group|
-      org_donor = group.flatten(1).to_h.to_a
-      org, donors = org_donor[0], org_donor[1..-1]
-
-      donors.to_h.values.uniq.map{ |donor_name| { name: donor_name.strip } }.each do |donor|
-        AllDonor.find_or_create_by(donor)
-      end
-
-      donors.to_h.values.uniq.map{ |donor_name| { organization_id: org[0], all_donor_id: AllDonor.find_by(name: donor_name.strip).id } }.each do |donor_org|
-        AllDonorOrganization.find_or_create_by(donor_org)
       end
     end
   end
