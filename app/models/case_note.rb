56afc6873d0fd716d640fb95fdbeb48a560330e9
sorphorn <so.sorphorn@gmail.com>
2019-08-09 10:38:54 +0700

    change name of scope in case note

--- app/models/case_note.rb
+++ app/models/case_note.rb
@@ -14,7 +14,7 @@ class CaseNote < ActiveRecord::Base
   accepts_nested_attributes_for :case_note_domain_groups
 
   scope :most_recents, -> { order(created_at: :desc) }
-  scope :most_recents_sort_case_note , -> {order(meeting_date: :desc)}
+  scope :recent_meeting_dates , -> {order(meeting_date: :desc)}
   
   scope :no_case_note_in, ->(value) { where('meeting_date <= ? AND id = (SELECT MAX(cn.id) FROM CASE_NOTES cn where CASE_NOTES.client_id = cn.client_id)', value) }
 
