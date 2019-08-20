63eb1e5b94043990fe169447ee961f65094e56e1
kirykr <kirymeas@gmail.com>
2019-08-20 16:37:22 +0700

    added program_streams and services

---
+++ db/migrate/20190820085203_fn_oscar_dashboard_program_streams.rb
@@ -0,0 +1,58 @@
+class FnOscarDashboardProgramStreams < ActiveRecord::Migration
+  def change
+    reversible do |dir|
+      dir.up do
+        if schema_search_path == "\"public\""
+          execute <<-SQL.squish
+            CREATE OR REPLACE FUNCTION "public"."fn_oscar_dashboard_program_streams"()
+              RETURNS TABLE("id" int4, "organization_name" varchar, "name" varchar, "ngo_name" varchar, "completed" bool, enrollment jsonb, "description" text) AS $BODY$
+              DECLARE
+                sql TEXT := '';
+                sch record;
+                ps_r record;
+              BEGIN
+                FOR sch IN
+                  SELECT organizations.full_name, organizations.short_name FROM "public"."donors"
+                  INNER JOIN "public"."donor_organizations" ON "public"."donor_organizations"."donor_id" = "public"."donors"."id"
+                  INNER JOIN "public"."organizations" ON "public"."organizations"."id" = "public"."donor_organizations"."organization_id"
+                  WHERE "public"."donors"."name" = 'Save the Children'
+                LOOP
+                  sql := sql || format(
+                                  'SELECT %2$s.id, %1$L organization_name, %2$s.name, %2$s.ngo_name,
+                                   %2$s.completed, %2$s.enrollment, %2$s.description FROM %1$I.%2$s UNION ',
+                                  sch.short_name, 'program_streams');
+                END LOOP;
+
+                FOR ps_r IN EXECUTE left(sql, -7)
+                LOOP
+                  id := ps_r.id;
+                  organization_name := ps_r.organization_name;
+                  name := ps_r.name;
+                  ngo_name := ps_r.ngo_name;
+                  completed := ps_r.completed;
+                  enrollment := ps_r.enrollment;
+                  description := ps_r.description;
+                  RETURN NEXT;
+                END LOOP;
+              END
+            $BODY$
+              LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+              COST 100
+              ROWS 1000;
+
+            GRANT EXECUTE ON FUNCTION "public"."fn_oscar_dashboard_program_streams"() TO "#{ENV['POWER_BI_GROUP']}";
+          SQL
+        end
+      end
+
+      dir.down do
+        if schema_search_path == "\"public\""
+          execute <<-SQL.squish
+            REVOKE EXECUTE ON FUNCTION "public"."fn_oscar_dashboard_program_streams"() FROM "#{ENV['POWER_BI_GROUP']}";
+            DROP FUNCTION IF EXISTS "public"."fn_oscar_dashboard_program_streams"() CASCADE;
+          SQL
+        end
+      end
+    end
+  end
+end
