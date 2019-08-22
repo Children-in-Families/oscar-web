ea9b9259f451d39ab30ba361d5a466e27cb8a135
DuchSamai <duchsamai77@gmail.com>
2019-08-22 16:19:05 +0700

    Finish programs were deleted but app still insist user updates 167216568

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,7 @@ class DashboardsController < AdminController
 
   def index
     @setting = Setting.first
-    @program_streams = ProgramStream.includes(:program_stream_services, :services).where(program_stream_services: { service_id: nil })
+    @program_streams = ProgramStream.without_deleted.includes(:program_stream_services, :services).where(program_stream_services: { service_id: nil })
     @dashboard = Dashboard.new(Client.accessible_by(current_ability))
     @referral_sources = ReferralSource.child_referrals.where(ancestry: nil)
     @clients = Client.accessible_by(current_ability).active_accepted_status
