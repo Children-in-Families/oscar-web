42ee7c4d8b098566eb71340be190f59fb14ea19e
kirykr <kirymeas@gmail.com>
2019-10-14 10:59:54 +0700

    update pg function to use global_id parameter

--- db/migrate/20191014021122_fn_oscar_dashboard_domain_groups.rb
+++ db/migrate/20191014021122_fn_oscar_dashboard_domain_groups.rb
@@ -15,7 +15,7 @@ class FnOscarDashboardDomainGroups < ActiveRecord::Migration
                   SELECT organizations.full_name, organizations.short_name FROM "public"."donors"
                   INNER JOIN "public"."donor_organizations" ON "public"."donor_organizations"."donor_id" = "public"."donors"."id"
                   INNER JOIN "public"."organizations" ON "public"."organizations"."id" = "public"."donor_organizations"."organization_id"
-                  WHERE "public"."donors"."name" = donor_name
+                  WHERE "public"."donors"."global_id" = donor_global_id
                 LOOP
                   sql := sql || format(
                                   'SELECT %2$s.id, %1$L organization_name, %2$s.description, %2$s.name,
