7d17e8c927ebb3151d1fa5a0fcf2a515e08e7a86
measbona <measbona3@gmail.com>
2019-11-13 16:19:26 +0700

    set value to field of client

--- app/javascript/components/NewClientForm/AdministrativeInfo.js
+++ app/javascript/components/NewClientForm/AdministrativeInfo.js
@@ -5,7 +5,7 @@ import {
 } from '../Commons/inputs'
 
 export default props => {
-  const { onChangeDate, data: { users, client } } = props
+  const { onChangeDate, onChangeSelect, data: { users, client } } = props
   const userLists = users.map(user => ({label: user.first_name + ' ' + user.last_name, value: user.id}))
 
   return (
@@ -20,7 +20,7 @@ export default props => {
 
       <div className='row'>
         <div className='col-xs-8'>
-          <SelectInput label='Receiving Staff Member' collections={userLists} />
+          <SelectInput label='Receiving Staff Member' collections={userLists} onChangeSelect={onChangeSelect} name='received_by_id' />
         </div>
       </div>
 
@@ -32,19 +32,19 @@ export default props => {
 
       <div className='row'>
         <div className='col-xs-8'>
-          <SelectInput label='Case Worker / Assigned Staff Memger' isMulti collections={userLists} />
+          <SelectInput label='Case Worker / Assigned Staff Memger' isMulti collections={userLists} onChangeSelect={onChangeSelect} name='users' />
         </div>
       </div>
 
       <div className='row'>
         <div className='col-xs-8'>
-          <SelectInput label='First Follow Up by' collections={userLists} />
+          <SelectInput label='First Follow Up by' collections={userLists} onChangeSelect={onChangeSelect} name='followed_up_by_id' />
         </div>
       </div>
 
       <div className='row'>
         <div className='col-xs-8'>
-          <DateInput label='Date of First Follow Up' collections={userLists} />
+          <DateInput label='Date of First Follow Up' collections={userLists} onChangeDate={onChangeDate} name='follow_up_date' />
         </div>
       </div>
     </>
