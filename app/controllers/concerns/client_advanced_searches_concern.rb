fe6b0f29ba348ee0a7d1d6e4b5e4fbeb2eb05fac
sorphorn <so.sorphorn@gmail.com>
2019-11-14 13:49:35 +0700

    add popup to prevent broken rule load save search

--- app/controllers/concerns/client_advanced_searches_concern.rb
+++ app/controllers/concerns/client_advanced_searches_concern.rb
@@ -19,13 +19,12 @@ module ClientAdvancedSearchesConcern
     respond_to do |f|
       f.html do
         # begin
-          @csi_statistics         = CsiStatistic.new(@client_grid.scope.where(id: @clients_by_user.ids).accessible_by(current_ability)).assessment_domain_score.to_json
-          @enrollments_statistics = ActiveEnrollmentStatistic.new(@client_grid.scope.where(id: @clients_by_user.ids).accessible_by(current_ability)).statistic_data.to_json
-          clients                 = @client_grid.scope { |scope| scope.where(id: @clients_by_user.ids).accessible_by(current_ability) }.assets
-          @clients                = clients
-          @results                = clients.size
-          @client_grid.scope { |scope| scope.where(id: @clients_by_user.ids).accessible_by(current_ability).page(params[:page]).per(20) }
-
+        @csi_statistics         = CsiStatistic.new(@client_grid.scope.where(id: @clients_by_user.ids).accessible_by(current_ability)).assessment_domain_score.to_json
+        @enrollments_statistics = ActiveEnrollmentStatistic.new(@client_grid.scope.where(id: @clients_by_user.ids).accessible_by(current_ability)).statistic_data.to_json
+        clients                 = @client_grid.scope { |scope| scope.where(id: @clients_by_user.ids).accessible_by(current_ability) }.assets
+        @clients                = clients
+        @results                = clients.size
+        @client_grid.scope { |scope| scope.where(id: @clients_by_user.ids).accessible_by(current_ability).page(params[:page]).per(20) }
       end
       f.xls do
         @client_grid.scope { |scope| scope.where(id: @clients_by_user.ids).accessible_by(current_ability) }
--- app/helpers/saved_search_helper.rb
+++ app/helpers/saved_search_helper.rb
@@ -1,65 +1,88 @@
 module SavedSearchHelper
-  def prevent_load_saved_searches(advanced_search)
-    if advanced_search.program_streams.present?
-      if !(@program_streams.ids & class_eval(advanced_search.program_streams)).empty?
-        link_to clients_path(save_search_params(advanced_search.search_params).merge(advanced_search_id: advanced_search.id)), class: 'btn btn-xs btn-success btn-outline dany', data: { "save-search-#{advanced_search.id}": advanced_search.queries.to_json } do
+  private
+    def prevent_load_saved_searches(advanced_search)
+      if Organization.current.short_name == "mtp" && AdvancedSearch::BROKEN_RULE_MTP.include?(advanced_search.id)
+        link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-toggle" => "popover","data-content" => "Program stream, rule or custom form has been deleted.", "data-placement" => "top","data-trigger" => "hover", :type => "button"} do
           fa_icon 'clipboard'
         end
-      else
-        link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-target" => "#modal-prevent_load_saved_searches", "data-toggle" => "modal", :type => "button"} do
-          fa_icon 'clipboard'
-        end
-      end
-    elsif advanced_search.custom_forms.present?
-      if !(@custom_fields.ids & class_eval(advanced_search.custom_forms)).empty?
-        link_to clients_path(save_search_params(advanced_search.search_params).merge(advanced_search_id: advanced_search.id)), class: 'btn btn-xs btn-success btn-outline dany', data: { "save-search-#{advanced_search.id}": advanced_search.queries.to_json } do
-          fa_icon 'clipboard'
-        end
-      else
-        link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-target" => "#modal-prevent_load_saved_searches", "data-toggle" => "modal", :type => "button"} do
-          fa_icon 'clipboard'
-        end
-      end
-    else
-      AdvancedSearch::BROKEN_SAVE_SEARCH.each do |broken_save_search|
-        if broken_save_search.first == Organization.current.short_name && broken_save_search.last == advanced_search.id
-          link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-target" => "#modal-prevent_load_saved_searches", "data-toggle" => "modal", :type => "button"} do
-            fa_icon 'clipboard'
-          end
-        else
+      elsif advanced_search.program_streams.present?
+        if !(@program_streams.ids & class_eval(advanced_search.program_streams)).empty?
           link_to clients_path(save_search_params(advanced_search.search_params).merge(advanced_search_id: advanced_search.id)), class: 'btn btn-xs btn-success btn-outline dany', data: { "save-search-#{advanced_search.id}": advanced_search.queries.to_json } do
             fa_icon 'clipboard'
           end
+        else
+          link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-toggle" => "popover","data-content" => "Program stream,rule or custom form has been deleted.", "data-placement" => "top","data-trigger" => "hover", :type => "button"} do
+            fa_icon 'clipboard'
+          end
         end
-      end
-    end
-  end
-
-  def prevent_edit_load_saved_searches(advanced_search)
-    if advanced_search.program_streams.present?
-      if !(@program_streams.ids & class_eval(advanced_search.program_streams)).empty?
-        link_to edit_advanced_search_save_query_path(advanced_search), class: 'btn btn-outline btn-success btn-xs' do
-          fa_icon 'pencil'
+      elsif advanced_search.custom_forms.present?
+        if !(@custom_fields.ids & class_eval(advanced_search.custom_forms)).empty?
+          link_to clients_path(save_search_params(advanced_search.search_params).merge(advanced_search_id: advanced_search.id)), class: 'btn btn-xs btn-success btn-outline dany', data: { "save-search-#{advanced_search.id}": advanced_search.queries.to_json } do
+            fa_icon 'clipboard'
+          end
+        else
+          link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-toggle" => "popover","data-content" => "Program stream,rule or custom form has been deleted.", "data-placement" => "top","data-trigger" => "hover", :type => "button"} do
+            fa_icon 'clipboard'
+          end
         end
       else
-        link_to '#', {class: 'btn btn-outline btn-success btn-xs', "data-target" => "#modal-prevent_load_saved_searches", "data-toggle" => "modal", :type => "button"} do
-          fa_icon 'pencil'
-        end
+        blank_save_search(advanced_search)
       end
-    elsif advanced_search.custom_forms.present?
-      if !(@custom_fields.ids & class_eval(advanced_search.custom_forms)).empty?
+    end
+
+    def blank_save_search(advanced_search)
+      current_org = Organization.current.short_name
+      advanced_search_id = advanced_search.id
+      custom_advanced_search = [current_org, advanced_search_id]
+      if AdvancedSearch::BROKEN_SAVE_SEARCH.include?(custom_advanced_search)
+        link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-toggle" => "popover","data-content" => "Program stream,rule or custom form has been deleted.", "data-placement" => "top","data-trigger" => "hover",:type => "button"} do
+          fa_icon 'clipboard'
+        end
+      else
         link_to clients_path(save_search_params(advanced_search.search_params).merge(advanced_search_id: advanced_search.id)), class: 'btn btn-xs btn-success btn-outline dany', data: { "save-search-#{advanced_search.id}": advanced_search.queries.to_json } do
           fa_icon 'clipboard'
         end
-      else
-        link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-target" => "#modal-prevent_load_saved_searches", "data-toggle" => "modal", :type => "button"} do
-          fa_icon 'clipboard'
-        end
-      end
-    else
-      link_to edit_advanced_search_save_query_path(advanced_search), class: 'btn btn-outline btn-success btn-xs' do
-        fa_icon 'pencil'
       end
     end
-  end
+
+    def prevent_edit_load_saved_searches(advanced_search)
+      if advanced_search.program_streams.present?
+        if !(@program_streams.ids & class_eval(advanced_search.program_streams)).empty?
+          link_to edit_advanced_search_save_query_path(advanced_search), class: 'btn btn-outline btn-success btn-xs' do
+            fa_icon 'pencil'
+          end
+        else
+          link_to '#', {class: 'btn btn-outline btn-success btn-xs', "data-toggle" => "popover","data-content" => "Program stream,rule or custom form has been deleted.", "data-placement" => "top","data-trigger" => "hover", :type => "button"} do
+            fa_icon 'pencil'
+          end
+        end
+      elsif advanced_search.custom_forms.present?
+        if !(@custom_fields.ids & class_eval(advanced_search.custom_forms)).empty?
+          link_to edit_advanced_search_save_query_path(advanced_search), class: 'btn btn-outline btn-success btn-xs' do
+            fa_icon 'pencil'
+          end
+        else
+          link_to '#', {class: 'btn btn-xs btn-success btn-outline', "data-toggle" => "popover","data-content" => "Program stream,rule or custom form has been deleted.", "data-placement" => "top","data-trigger" => "hover", :type => "button"} do
+            fa_icon 'pencil'
+          end
+        end
+      else
+        blank_edit_save_search(advanced_search)
+      end
+    end
+
+    def blank_edit_save_search(advanced_search)
+      current_org = Organization.current.short_name
+      advanced_search_id = advanced_search.id
+      custom_advanced_search = [current_org, advanced_search_id]
+      if AdvancedSearch::BROKEN_SAVE_SEARCH.include?(custom_advanced_search)
+        link_to '#', {class: 'btn btn-xs btn-success btn-outline',"data-toggle" => "popover","data-content" => "Program stream,rule or custom form has been deleted.", "data-placement" => "top","data-trigger" => "hover", :type => "button"} do
+          fa_icon 'pencil'
+        end
+      else
+        link_to edit_advanced_search_save_query_path(advanced_search), class: 'btn btn-outline btn-success btn-xs' do
+          fa_icon 'pencil'
+        end
+      end
+    end
 end
