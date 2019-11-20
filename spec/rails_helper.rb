2a1a78601804cea60f6e4c23d8d9215df7cc6271
kirykr <kirymeas@gmail.com>
2019-11-20 16:33:55 +0700

    remove capibara_screenshot gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'capybara-screenshot/rspec'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -25,9 +25,6 @@ require 'capybara-screenshot/rspec'
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
-Capybara::Screenshot.webkit_options = { width: 1024, height: 768 }
-Capybara::Screenshot.prune_strategy = :keep_last_run
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
