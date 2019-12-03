be89923b3913ac12511ab4d57137dc72afc0178a
kirykr <kirymeas@gmail.com>
2019-12-03 17:51:47 +0700

    fixed family client not correct

--- db/migrate/20191107074516_migrate_current_family_id_to_client.rb
+++ db/migrate/20191107074516_migrate_current_family_id_to_client.rb
@@ -1,23 +1,17 @@
 class MigrateCurrentFamilyIdToClient < ActiveRecord::Migration
   def up
-    # Client.joins(:families).each do |client|
-    #   next if client.current_family_id.present?
-    #   if client.current_family_id.nil?
-    #     if client.family_ids.last.present?
-    #       client.current_family_id = client.family_ids.last
-    #       client.save(validate: false)
-    #     else
-    #       client.current_family_id = client.family.id
-    #       client.save(validate: false)
-    #     end
-    #   elsif client.family
-    #     client.current_family_id = client.family.id
-    #     client.save(validate: false)
-    #   else
-    #     next
-    #   end
-    #   puts "Client: #{client.slug}"
-    # end
+    Client.joins(:families).each do |client|
+      next if client.current_family_id.present?
+      if client.current_family_id.nil?
+        if client.family_ids.last.present?
+          client.current_family_id = client.family_ids.last
+          client.save(validate: false)
+        end
+      else
+        next
+      end
+      puts "Client: #{client.slug}"
+    end
   end
 
   def down
