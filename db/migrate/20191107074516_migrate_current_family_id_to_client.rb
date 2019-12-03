e2f763236d0d600f77e3da3408e989f8a440c8b3
kirykr <kirymeas@gmail.com>
2019-12-03 13:18:12 +0700

    revert family back

--- db/migrate/20191107074516_migrate_current_family_id_to_client.rb
+++ db/migrate/20191107074516_migrate_current_family_id_to_client.rb
@@ -1,24 +1,28 @@
 class MigrateCurrentFamilyIdToClient < ActiveRecord::Migration
   def up
-    Client.joins(:families).each do |client|
-      next if client.current_family_id.present?
-      if client.current_family_id.nil?
-        if client.family_ids.last.present?
-          client.current_family_id = client.family_ids.last
-          client.save(validate: false)
-        else
-          client.current_family_id = client.family.id
-          client.save(validate: false)
-        end
-      elsif client.family
-        client.current_family_id = client.family.id
-        client.save(validate: false)
-      else
-        next
-      end
-      puts "Client: #{client.slug}"
-    end
+    # Client.joins(:families).each do |client|
+    #   next if client.current_family_id.present?
+    #   if client.current_family_id.nil?
+    #     if client.family_ids.last.present?
+    #       client.current_family_id = client.family_ids.last
+    #       client.save(validate: false)
+    #     else
+    #       client.current_family_id = client.family.id
+    #       client.save(validate: false)
+    #     end
+    #   elsif client.family
+    #     client.current_family_id = client.family.id
+    #     client.save(validate: false)
+    #   else
+    #     next
+    #   end
+    #   puts "Client: #{client.slug}"
+    # end
   end
 
-  def down; end
+  def down
+    execute <<-SQL.squish
+      UPDATE clients SET current_family_id = NULL WHERE current_family_id IS NOT NULL;
+    SQL
+  end
 end
