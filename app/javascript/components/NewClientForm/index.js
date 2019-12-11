218eb2b16e71b4804e3563fdc256e08ea4454207
sophal <caili.cl29@gmail.com>
2019-12-11 15:58:34 +0700

    duplicate checker finished

--- app/javascript/components/NewClientForm/index.js
+++ app/javascript/components/NewClientForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Modal from '../Commons/Modal'
 import AdministrativeInfo from './admin'
 import RefereeInfo from './refereeInfo'
 import ReferralInfo from './referralInfo'
@@ -15,11 +16,17 @@ const Forms = props => {
     }
   } = props
 
-  const [errorFields, seterrorFields] = useState([])
-  const [step, setStep] = useState(1)
-  const [clientData, setClientData] = useState({ user_ids, quantitative_case_ids, agency_ids, donor_ids, family_ids, ...client })
-  const [refereeData, setrefereeData] = useState({})
-  const [carerData, setcarerData] = useState({})
+  const [step, setStep]               = useState(1)
+  const [modalOpen, setModalOpen]     = useState(false)
+  const [onSave, setOnSave]           = useState(false)
+
+  const [dupFields, setDupFields]     = useState([])
+  const [errorSteps, setErrorSteps]   = useState([])
+  const [errorFields, setErrorFields] = useState([])
+
+  const [clientData, setClientData]   = useState({ user_ids, quantitative_case_ids, agency_ids, donor_ids, family_ids, ...client })
+  const [refereeData, setRefereeData] = useState({})
+  const [carerData, setCarerData]     = useState({})
 
   const address = { currentDistricts: districts, currentCommunes: communes, currentVillages: villages, currentProvinces  }
   const adminTabData = { users, client: clientData, errorFields }
@@ -35,20 +42,19 @@ const Forms = props => {
     {text: 'Client / Referral - Vulnerability Information and Referral Note', step: 4}
   ]
 
-  const activeClass = value => step === value ? 'active' : ''
+  const classStyle = value => errorSteps.includes(value) ? 'errorTab' : step === value ? 'activeTab' : 'normalTab'
 
   const renderTab = (data, index) => {
     return (
       <span
         key={index}
         onClick={() => handleTab(data.step)}
-        className={`tabButton ${activeClass(data.step)}`}
+        className={`tabButton ${classStyle(data.step)}`}
       >
         {data.text}
       </span>
     )
   }
-
 
   const onChange = (obj, field) => event => {
     const inputType = ['date', 'select', 'checkbox', 'radio']
@@ -62,15 +68,15 @@ const Forms = props => {
         setClientData({...clientData, ...field})
         break;
       case 'referee':
-        setrefereeData({...refereeData, ...field })
+        setRefereeData({...refereeData, ...field })
         break;
       case 'carer':
-        setcarerData({...carerData, ...field })
+        setCarerData({...carerData, ...field })
         break;
     }
   }
 
-  const handleValidation = () => {
+  const handleValidation = (stepTobeCheck = 0) => {
     const components = [
       // { step: 1, data: refereeData, fields: ['referee_name', 'referee_referral_source_catgeory_id'] },
       { step: 1, data: clientData, fields: ['name_of_referee', 'referral_source_category_id'] },
@@ -80,37 +86,119 @@ const Forms = props => {
     ]
 
     const errors = []
+    const errorSteps = []
 
     components.forEach(component => {
-      if (step === component.step) {
+      if (step === component.step || (stepTobeCheck !== 0 && component.step === stepTobeCheck)) {
         component.fields.forEach(field => {
-          (component.data[field] === '' || (Array.isArray(component.data[field]) && !component.data[field].length) || component.data[field] === null) && errors.push(field)
+          if (component.data[field] === '' || (Array.isArray(component.data[field]) && !component.data[field].length) || component.data[field] === null) {
+            errors.push(field)
+            errorSteps.push(component.step)
+          }
         })
       }
     })
 
     if (errors.length > 0) {
-      seterrorFields(errors)
+      setErrorFields(errors)
+      setErrorSteps([ ...new Set(errorSteps)])
       return false
     } else {
-      seterrorFields([])
+      setErrorFields([])
+      setErrorSteps([])
       return true
     }
   }
 
   const handleTab = goingToStep => {
-    if(goingToStep < step || handleValidation())
-      setStep(goingToStep)
-    if(goingToStep == 3 && step == 1 || goingToStep == 4 && step == 1 && handleValidation())
-      setStep(2)
+    const goBack    = goingToStep < step
+    const goForward = goingToStep === step + 1
+    const goOver    = goingToStep >= step + 2 || goingToStep >= step + 3
+    console.log('goBack', goBack)
+    console.log('goForward', goForward)
+    console.log('goOver', goOver)
+    if((goForward && handleValidation()) || (goOver && handleValidation(1) && handleValidation(2)) || goBack)
+      if(step === 2)
+        checkClientExist()(() => setStep(goingToStep))
+      else
+        setStep(goingToStep)
   }
 
   const buttonNext = () => {
-    if (handleValidation())
-      setStep(step + 1)
+    if (handleValidation()) {
+      if (step === 2 )
+        checkClientExist()(() => setStep(step + 1))
+      else
+        setStep(step + 1)
+    }
   }
 
-  const handleSave = () => {
+  const checkClientExist = () => callback => {
+    const data =  {
+      given_name: clientData.given_name ,
+      family_name: clientData.family_name,
+      local_given_name: clientData.local_given_name,
+      local_family_name: clientData.local_family_name,
+      date_of_birth: clientData.date_of_birth || '',
+      birth_province_id: clientData.birth_province_id || '',
+      current_province_id: clientData.province_id || '',
+      district_id: clientData.district_id || '',
+      village_id: clientData.village_id || '',
+      commune_id: clientData.commune_id || ''
+    }
+
+    if(!clientData.id) {
+      if(data.given_name !== '' || data.family_name !== '' || data.local_given_name !== '' || data.local_family_name !== '' || data.date_of_birth !== '' || data.birth_province_id !== '' || data.current_province_id !== '' || data.district_id !== '' || data.village_id !== '' || data.commune_id !== '') {
+        $.ajax({
+          type: 'GET',
+          url: '/api/clients/compare',
+          data: data,
+        }).success(response => {
+          if(response.similar_fields.length > 0) {
+            setDupFields(response.similar_fields)
+            setModalOpen(true)
+          } else
+            callback()
+        })
+      } else
+        callback()
+    } else
+      callback()
+  }
+
+  const renderModalContent = data => {
+    return (
+      <>
+        <p>The client record you are saving has similarities to other records in OSCaR in the following fields:</p>
+        <ul>
+          {
+            data.map((fields,index) => {
+              let newFields = fields.split('_')
+              newFields.splice(0, 1)
+              return <li key={index} style={{textTransform: 'capitalize'}}>{newFields.join(' ')}</li>
+            })
+          }
+        </ul>
+        <p>Please check with the client whether they have ever worked with another organisation that may have put their details into OSCaR.</p>
+      </>
+    )
+  }
+
+  const renderModalFooter = () => {
+    return (
+      <>
+        <p>Duplicate Checker feature is currently in Beta testing, please Email: info@oscarhq.com if you have any issues with excessive false positive/negative results.</p>
+        <div style={{display:'flex', justifyContent: 'flex-end'}}>
+          <button style={{margin: 5}} className='btn btn-primary' onClick={() => (setModalOpen(false), setStep(step + 1))}>Continue</button>
+          <button style={{margin: 5}} className='btn btn-default' onClick={() => setModalOpen(false)}>Cancel</button>
+        </div>
+      </>
+    )
+  }
+
+  const handleSave = event => {
+    setOnSave(true)
+
     if (handleValidation()) {
       const action = clientData.id ? 'PUT' : 'POST'
       const url = clientData.id ? `/api/clients/${clientData.id}` : '/api/clients'
@@ -132,6 +220,15 @@ const Forms = props => {
 
   return (
     <div className='containerClass'>
+      <Modal
+        title='Warning'
+        isOpen={modalOpen}
+        type='warning'
+        closeAction={() => setModalOpen(false)}
+        content={ renderModalContent(dupFields) }
+        footer={ renderModalFooter() }
+      />
+
       <div className='tabHead'>
         {tabs.map((tab, index) => renderTab(tab, index))}
       </div>
@@ -169,7 +266,7 @@ const Forms = props => {
           <span className={step === 1 && 'clientButton preventButton' || 'clientButton allowButton'} onClick={buttonPrevious}>Previous</span>
           { step !== 4 && <span className={'clientButton allowButton'} onClick={buttonNext}>Next</span> }
           { step === 4 && errorFields.length > 0 && <span className='clientButton saveButton' data-target='#myModal' data-toggle='modal'>Save</span> }
-          { step === 4 && errorFields.length === 0 && <span className='clientButton saveButton' onClick={handleSave}>Save</span> }
+          { step === 4 && errorFields.length === 0 && <span className={onSave ? 'clientButton preventButton': 'clientButton saveButton' } onClick={handleSave}>Save</span> }
           <CreateFamilyModal id="myModal" data={{ families, clientData, refereeData }} onChange={onChange} />
         </div>
       </div>
