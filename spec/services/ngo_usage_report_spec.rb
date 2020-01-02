1578b23a209af9615056539c6bc9d15946cfaace
kirykr <kirymeas@gmail.com>
2020-01-02 11:57:50 +0700

    work on spec

--- spec/services/ngo_usage_report_spec.rb
+++ spec/services/ngo_usage_report_spec.rb
@@ -14,12 +14,22 @@ describe NgoUsageReport, 'generator' do
 
     it 'returns NGO info' do
       Organization.current.update(created_at: Date.today)
-
       expect(@report.ngo_info(Organization.current)).to eq({
         ngo_name: 'Organization Testing',
         ngo_on_board: date_format(Date.today),
         fcf: 'No',
         ngo_country: 'Cambodia'
+      })
+    end
+
+    it 'returns NGO info with country name blank.' do
+      Organization.current.update(created_at: Date.today)
+      # Setting.delete_all
+      expect(@report.ngo_info(Organization.current)).to eq({
+        ngo_name: 'Organization Testing',
+        ngo_on_board: date_format(Date.today),
+        fcf: 'No',
+        ngo_country: ''
       })
     end
 
