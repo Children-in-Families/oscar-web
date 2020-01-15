1dda11ef5816b12767185289db11e5289e871e5d
kirykr <kirymeas@gmail.com>
2020-01-15 09:48:34 +0700

    worked on mapping field value

--- app/classes/advanced_searches/advanced_search_fields.rb
+++ app/classes/advanced_searches/advanced_search_fields.rb
@@ -1,13 +1,13 @@
-module AdvancedSearch
+module AdvancedSearches
   class AdvancedSearchFields
-    attr_reader :header_group, :number_fields, :text_fields, :date_picker_fields, :drop_list_options
+    attr_reader :translations, :number_type_list, :text_type_list, :date_type_list, :drop_down_type_list
 
-    def initialize(header_group=[], number_fields=[], text_fields=[], date_picker_fields=[], drop_list_fields=[])
-      @header_group = header_group
-      @number_fields = number_fields
-      @text_fields = text_fields
-      @date_picker_fields = date_picker_fields
-      @drop_list_options = drop_list_options
+    def initialize(args)
+      @translations        = args.fetch(:translation)
+      @text_type_list      = args.fetch(:text_field)
+      @date_type_list      = args.fetch(:date_picker_field)
+      @number_type_list    = args.fetch(:number_field)
+      @drop_down_type_list = args.fetch(:drop_list_option)
     end
 
     def render
@@ -24,33 +24,8 @@ module AdvancedSearch
 
     private
 
-    def number_type_list
-      ['significant_family_member_count', 'household_income', 'female_children_count', 'male_children_count', 'female_adult_count', 'male_adult_count', 'id']
-    end
-
-    def text_type_list
-      ['code', 'name', 'caregiver_information', 'case_history', 'street', 'house']
-    end
-
-    def date_type_list
-      ['contract_date']
-    end
-
-    def drop_down_type_list
-      [
-        ['family_type', family_type_options],
-        ['status', status_options],
-        ['province_id', provinces],
-        ['district_id', districts],
-        ['dependable_income', { yes: 'Yes', no: 'No' }],
-        ['client_id', clients],
-        ['commune_id', communes],
-        ['village_id', villages]
-      ]
-    end
-
-    def header_translation(group)
-
-    end
+      def header_translation(key)
+        translations[key.to_sym] || ''
+      end
   end
 end
--- app/helpers/call_helper.rb
+++ app/helpers/call_helper.rb
@@ -1,5 +1,28 @@
 module CallHelper
   def call_report_builder_fields
-    call_builder_fields = AdvancedSearch::AdvancedSearchFields.new()
+    args = group_call_field_types
+    call_builder_fields = AdvancedSearches::AdvancedSearchFields.new(args)
+  end
+
+  def group_call_field_types
+    translations = @calls_grid.datagrid_attributes[2..-1].map do |header|
+      [header, I18n.t("datagrid.columns.partners.#{header.to_s}")]
+    end.to_h
+
+    number_fields = []; text_fields = []; date_picker_fields = []; drop_list_options = []
+
+    @calls_grid.filters.each do |filter|
+      case filter.class.name
+      when /integerfilter/i
+        number_fields << filter.name
+      when /defaultfilter/i
+        text_fields << filter.name
+      when /datefilter/i
+        date_picker_fields << filter.name
+      when /enumfilter/i
+        drop_list_options << filter.name
+      end
+    end
+    { translation: translations, number_field: number_fields, text_field: text_fields, date_picker_field: date_picker_fields, drop_list_option: drop_list_options }
   end
 end
