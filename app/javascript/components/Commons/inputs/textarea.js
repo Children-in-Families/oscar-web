e7f032e0a6e19f5f00b5bc0b2348123cb86f5261
PirunSeng <sengpirun.rain@gmail.com>
2020-02-06 14:01:29 +0700

    20200206 fixes location description on multiple clients

--- app/javascript/components/Commons/inputs/textarea.js
+++ app/javascript/components/Commons/inputs/textarea.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export default props => {
-  const { isError, hidden, label, required, onChange, T, ...others } = props
+  const { isError, hidden, label, required, onChange, T, value, ...others } = props
   return (
     <div className='form-group'>
       <label className={ hidden ? 'hidden' : '' } style={isError && styles.errorText || {}}>
         {required && <abbr title='required'>* </abbr>}
         {label}
       </label>
-      <textarea className="form-control col-xs-12" style={styles.heightBox} onChange={onChange} {...others} />
+      <textarea className="form-control col-xs-12" style={styles.heightBox} onChange={onChange} {...others} value={value || ''} />
       {isError && <span style={styles.errorText}>{T.translate("validation.cannot_blank")}</span>}
     </div>
   )
