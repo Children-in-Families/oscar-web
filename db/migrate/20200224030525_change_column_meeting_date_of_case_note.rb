e08752697a1ed57d226fe1f98cf7a11013466a5f
kirykr <kirymeas@gmail.com>
2020-02-24 16:02:16 +0700

    changed meeting_date datetime

--- db/migrate/20200224030525_change_column_meeting_date_of_case_note.rb
+++ db/migrate/20200224030525_change_column_meeting_date_of_case_note.rb
@@ -4,13 +4,14 @@ class ChangeColumnMeetingDateOfCaseNote < ActiveRecord::Migration
 
     minute_added = 0
     values = CaseNote.all.map do |case_note|
+      minute_added = 0 if minute_added == 59
       case_note_meeting_date = case_note.meeting_date + (minute_added += 1).minute
-      "(#{case_note.id}, '#{case_note_meeting_date.to_s.gsub('UTC', '').squish.split(' ').join(', ')}')"
+      "(#{case_note.id}, '#{case_note_meeting_date.to_s}')"
     end.join(", ")
 
     if values.present?
       execute <<-SQL.squish
-        UPDATE case_notes SET meeting_date = DATE(mapping_values.meeting_date) FROM (VALUES #{values}) AS mapping_values (case_note_id, meeting_date) WHERE case_notes.id = mapping_values.case_note_id;
+        UPDATE case_notes SET meeting_date = mapping_values.meeting_date::timestamptz FROM (VALUES #{values}) AS mapping_values (case_note_id, meeting_date) WHERE case_notes.id = mapping_values.case_note_id;
       SQL
     end
   end
