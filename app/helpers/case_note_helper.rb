b1c72c58e4f44dca18b93d40567b1279ce74dcd3
kirykr <kirymeas@gmail.com>
2020-02-27 13:18:49 +0700

    fixed case note helper case_notes_readable

--- app/helpers/case_note_helper.rb
+++ app/helpers/case_note_helper.rb
@@ -73,13 +73,13 @@ module CaseNoteHelper
 
   def case_notes_readable?
     return true if current_user.admin? || current_user.strategic_overviewer?
-    current_user.permission.case_notes_readable
+    current_user.permission&.case_notes_readable
   end
 
   def case_notes_editable?
     return true if current_user.admin?
     return false if current_user.strategic_overviewer?
-    current_user.permission.case_notes_editable
+    current_user.permission&.case_notes_editable
   end
 
   def case_notes_deleted?
