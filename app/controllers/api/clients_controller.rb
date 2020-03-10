66625e73b5ce256ba14a665008c02c3adbda23d6
kirykr <kirymeas@gmail.com>
2020-03-10 16:00:35 +0700

    worked on created new family from client

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -53,7 +53,7 @@ module Api
       carer.update_attributes(carer_params)
       client.carer_id = carer.id
       new_params = client.current_family_id ? client_params : client_params.except(:family_ids)
-      if client.update_attributes(client_params.except(:referee_id, :carer_id, :family_ids))
+      if client.update_attributes(client_params.except(:referee_id, :carer_id))
         if params[:client][:assessment_id]
           assessment = Assessment.find(params[:client][:assessment_id])
           # redirect_to client_assessment_path(client, assessment), notice: t('.assessment_successfully_created')
@@ -72,7 +72,7 @@ module Api
             :slug, :archived_slug, :code, :name_of_referee, :main_school_contact, :rated_for_id_poor, :what3words, :status, :country_origin,
             :kid_id, :assessment_id, :given_name, :family_name, :local_given_name, :local_family_name, :gender, :date_of_birth,
             :birth_province_id, :initial_referral_date, :referral_source_id, :telephone_number,
-            :referral_phone, :received_by_id, :followed_up_by_id,
+            :referral_phone, :received_by_id, :followed_up_by_id, :current_family_id,
             :follow_up_date, :school_grade, :school_name, :current_address,
             :house_number, :street_number, :suburb, :description_house_landmark, :directions, :street_line1, :street_line2, :plot, :road, :postal_code, :district_id, :subdistrict_id,
             :has_been_in_orphanage, :has_been_in_government_care,
