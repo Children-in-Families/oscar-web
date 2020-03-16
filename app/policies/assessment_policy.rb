75b2d2decfbd228bc1b1f498a7741274e2bf864d
kirykr <kirymeas@gmail.com>
2020-03-16 16:48:10 +0700

    fixed custom assessment create new

--- app/policies/assessment_policy.rb
+++ app/policies/assessment_policy.rb
@@ -12,9 +12,15 @@ class AssessmentPolicy < ApplicationPolicy
   def new?(value='', custom_assessment=nil)
     return false if user.strategic_overviewer?
     setting = Setting.first
-    enable_assessment = record.default? ? setting.enable_default_assessment? && record.client.eligible_default_csi? : setting.enable_custom_assessment? && record.client.eligible_custom_csi?(custom_assessment)
-    editable_user     = user.admin? ? true : user.permission&.assessments_editable
-    enable_assessment && editable_user && !record.client.exit_ngo? && record.client.can_create_assessment?(record.default, value)
+    if custom_assessment
+      enable_assessment = record.default? ? setting.enable_default_assessment? && record.client.eligible_default_csi? : setting.enable_custom_assessment? && record.client.eligible_custom_csi?(custom_assessment)
+      editable_user     = user.admin? ? true : user.permission&.assessments_editable
+      enable_assessment && editable_user && !record.client.exit_ngo? && record.client.can_create_assessment?(record.default, value)
+    else
+      enable_assessment = record.default? ? setting.enable_default_assessment? && record.client.eligible_default_csi? : setting.enable_custom_assessment?
+      editable_user     = user.admin? ? true : user.permission&.assessments_editable
+      enable_assessment && editable_user && !record.client.exit_ngo? && record.client.can_create_assessment?(record.default, value)
+    end
   end
 
   def edit?
