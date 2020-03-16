be19d4bc6f9fd11ff279009910ee038c6462f9ed
Vibol T <vibolteav@gmail.com>
2020-03-16 08:51:11 +0700

    fixed old migration

--- db/migrate/20170927071506_add_clients_field_to_family.rb
+++ db/migrate/20170927071506_add_clients_field_to_family.rb
@@ -1,8 +1,8 @@
 class AddClientsFieldToFamily < ActiveRecord::Migration
   def up
     add_column :families, :children, :integer, array: true, default: []
-
-    Family.all.each do |family|
+    # Ignore scope, because this migration added before deleted_at field added
+    Family.with_deleted.each do |family|
       family.update(children: family.client_ids.uniq) if family.client_ids.any?
     end
   end
