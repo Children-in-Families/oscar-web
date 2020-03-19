b488267b45235a0f9552263066142e68f147cc83
Vibol T <vibolteav@gmail.com>
2020-03-19 12:41:46 +0700

    updated client columns

--- app/grids/client_grid.rb
+++ app/grids/client_grid.rb
@@ -44,6 +44,36 @@ class ClientGrid < BaseGrid
     scope.where(slug: slugs)
   end
 
+  %w(
+      presented_id
+      id_number
+      client_phone
+      whatsapp
+      other_phone_number
+      local_given_name
+      local_family_name
+      v_score
+      brsc_branch
+      current_island
+      current_street
+      current_po_box
+      current_city
+      current_settlement
+      current_resident_own_or_rent
+      current_household_type
+      island2
+      street2
+      po_box2
+      city2
+      settlement2
+      resident_own_or_rent2
+      household_type2
+    ).each do |field_name|
+    filter(field_name, :string, header: -> { I18n.t("datagrid.columns.clients.#{field_name}") }) do |value, scope|
+      filter_shared_fileds(field_name, value, scope)
+    end
+  end
+
   %w(difficulties household_members interview_locations hosting_number bic_others).each do |field_name|
     options = {
       difficulties: Client::DIFFICULTIES,
@@ -740,6 +770,36 @@ class ClientGrid < BaseGrid
 
   column(:age, header: -> { I18n.t('datagrid.columns.clients.age') }, order: 'clients.date_of_birth desc') do |object|
     pluralize(object.age_as_years, 'year') + ' ' + pluralize(object.age_extra_months, 'month') if object.date_of_birth.present?
+  end
+
+  %w(
+      presented_id
+      id_number
+      client_phone
+      whatsapp
+      other_phone_number
+      local_given_name
+      local_family_name
+      v_score
+      brsc_branch
+      current_island
+      current_street
+      current_po_box
+      current_city
+      current_settlement
+      current_resident_own_or_rent
+      current_household_type
+      island2
+      street2
+      po_box2
+      city2
+      settlement2
+      resident_own_or_rent2
+      household_type2
+    ).each do |field_name|
+    column(field_name, header: -> { I18n.t("datagrid.columns.clients.#{field_name}") }) do |object|
+      object.public_send(field_name.to_sym)
+    end
   end
 
   %w(difficulties household_members interview_locations hosting_number bic_others).each do |field_name|
