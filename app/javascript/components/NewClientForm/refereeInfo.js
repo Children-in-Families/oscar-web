49ec55a5b9daef453981b43476b434f36b9fa5e5
vibol.t <vibolteav@gmail.com>
2020-03-30 13:52:37 +0700

    client form: hide/show referee address

--- app/javascript/components/NewClientForm/refereeInfo.js
+++ app/javascript/components/NewClientForm/refereeInfo.js
@@ -107,21 +107,27 @@ export default props => {
 
       </div>
 
-      <legend>
-        <div className="row">
-          <div className="col-xs-12 col-md-6 col-lg-3">
-            <p>{T.translate("refereeInfo.address")}</p>
-          </div>
-          {
-            !referee.anonymous &&
-            <div className="col-xs-12 col-md-6 col-lg-3">
-              <Checkbox label={T.translate("refereeInfo.outside_cam")} checked={referee.outside || false} onChange={onChange('referee', 'outside')} />
-            </div>
-          }
-        </div>
-      </legend>
+      {
+        fieldsVisibility.referee_address == true &&
 
-      <Address disabled={referee.anonymous} outside={referee.outside || false} onChange={onChange} data={{currentDistricts: refereeDistricts, currentCommunes: refereeCommunes, currentVillages: refereeVillages, currentProvinces, addressTypes, objectKey: 'referee', objectData: referee, T}} />
+        <div>
+          <legend>
+            <div className="row">
+              <div className="col-xs-12 col-md-6 col-lg-3">
+                <p>{T.translate("refereeInfo.address")}</p>
+              </div>
+              {
+                !referee.anonymous &&
+                <div className="col-xs-12 col-md-6 col-lg-3">
+                  <Checkbox label={T.translate("refereeInfo.outside_cam")} checked={referee.outside || false} onChange={onChange('referee', 'outside')} />
+                </div>
+              }
+            </div>
+          </legend>
+
+          <Address disabled={referee.anonymous} outside={referee.outside || false} onChange={onChange} data={{currentDistricts: refereeDistricts, currentCommunes: refereeCommunes, currentVillages: refereeVillages, currentProvinces, addressTypes, objectKey: 'referee', objectData: referee, T}} />
+        </div>
+      }
     </div>
   )
 }
