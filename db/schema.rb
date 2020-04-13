43b6356e8de4a32f164097b58265245a1a3c4e6c
Vibol T <vibolteav@gmail.com>
2020-04-13 10:12:08 +0700

    added family member note

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200412101201) do
+ActiveRecord::Schema.define(version: 20200413025327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -842,6 +842,7 @@ ActiveRecord::Schema.define(version: 20200412101201) do
     t.datetime "updated_at",                    null: false
     t.boolean  "guardian",      default: false
     t.string   "gender"
+    t.text     "note"
   end
 
   add_index "family_members", ["family_id"], name: "index_family_members_on_family_id", using: :btree
