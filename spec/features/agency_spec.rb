3f898009484237d9bd7a1d08fcc08c61b24c0b1d
Darren Jensen <darren.jensen@gmail.com>
2020-04-16 16:37:57 +0700

    attempt to fix js feature specs

--- spec/features/agency_spec.rb
+++ spec/features/agency_spec.rb
@@ -25,7 +25,7 @@ describe 'Agency' do
     end
   end
 
-  feature 'Create', js: true do
+  feature 'Create' do
     before do
       visit agencies_path
     end
@@ -38,8 +38,14 @@ describe 'Agency' do
       expect(page).to have_content('Test Agency')
     end
 
-    scenario 'invalid' do
+    scenario 'invalid', js: true, focus: true do
+      puts "********"
+      org_short_name = Organization.current.short_name
+      puts org_short_name
+      puts page.html
+      puts "********"
       click_link 'Add New Agency'
+
       within('#new_agency') do
         click_button 'Save'
       end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,7 +31,6 @@ Dir[Rails.root.join('spec/supports/**/*.rb')].each { |f| require f }
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-Capybara.javascript_driver = :poltergeist
 
 Capybara.register_server :thin do |app, port, host|
   require 'rack/handler/thin'
@@ -43,11 +42,16 @@ Capybara.server = :thin
 Capybara.register_driver :poltergeist do |app|
   options = {
     js_errors: false,
-    phantomjs_options: ['--load-images=true', '--ignore-ssl-errors=yes', '--ssl-protocol=any'],
-    timeout: 60
+    phantomjs_options: ['--debug=false', '--web-security=false', '--ignore-ssl-errors=yes', '--ssl-protocol=any', '--webdriver-logfile=/var/log/phantomjs.log'],
+    timeout: 60,
+    phantomjs: File.absolute_path(Phantomjs.path)
   }
   Capybara::Poltergeist::Driver.new(app, options)
 end
+
+Capybara.javascript_driver = :poltergeist
+
+Capybara.server_port = 3001
 
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = [:should , :expect] }
@@ -99,6 +103,14 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
     Apartment::Tenant.drop('app') rescue nil
     Organization.create_and_build_tanent(full_name: 'Organization Testing', short_name: 'app')
@@ -107,11 +119,10 @@ RSpec.configure do |config|
   end
 
   config.before(:each, js: true) do
-    page.driver.browser.url_blacklist = %w(http://use.typekit.net https://fonts.gstatic.com https://fonts.googleapis.com http://cdn.rawgit.com)
-    page.driver.browser.url_whitelist = %w(http://app.lvh.me http://lvh.me 127.0.0.1)
+    DatabaseCleaner.strategy = :truncation
     Capybara.default_max_wait_time = 10
     Capybara.always_include_port = true
-    Capybara.app_host = "http://app.oscar.test"
+    Capybara.app_host = "http://localhost"
   end
 
   config.around(:each) do |example|
@@ -119,10 +130,6 @@ RSpec.configure do |config|
       Setting.create(country_name: 'cambodia', max_case_note: 30, case_note_frequency: 'day', max_assessment: 6, age: 18, enable_default_assessment: true, enable_custom_assessment: true)
       example.run
     end
-  end
-
-  config.before(:each, :js) do
-    DatabaseCleaner.strategy = :truncation
   end
 
   config.after(:each) do
