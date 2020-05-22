e63ecba1b476935dc2798cf8710d09aaa1a98268
Vibol Teav <vibolteav@gmail.com>
2020-05-22 22:12:08 +0700

    fixed migration

--- db/migrate/20200522142638_add_government_form_to_field_setting.rb
+++ db/migrate/20200522142638_add_government_form_to_field_setting.rb
@@ -1,29 +1,19 @@
 class AddGovernmentFormToFieldSetting < ActiveRecord::Migration
   def up
-    Organization.find_each do |organisation|
-      Organization.switch_to(organisation.short_name)
+    return if Apartment::Tenant.current_tenant == 'shared'
 
-      next if Organization.shared?
-
-      FieldSetting.create(
-        name: :government_forms,
-        label: 'Government Forms',
-        current_label: 'Government Forms',
-        klass_name: :client,
-        required: false,
-        visible: %w(brc ratanak).exclude?(organisation.short_name),
-        group: :client
-      )
-    end
+    FieldSetting.create(
+      name: :government_forms,
+      label: 'Government Forms',
+      current_label: 'Government Forms',
+      klass_name: :client,
+      required: false,
+      visible: %w(brc ratanak).exclude?(Apartment::Tenant.current_tenant),
+      group: :client
+    )
   end
 
   def down
-    Organization.find_each do |organisation|
-      Organization.switch_to(organisation.short_name)
-
-      next if Organization.shared?
-
-      FieldSetting.where(name: :government_forms).delete_all
-    end
+    FieldSetting.where(name: :government_forms).delete_all
   end
 end
