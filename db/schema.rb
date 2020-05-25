29289b6d83e7b9b9a37520863cfea78081a8f4cd
Vibol Teav <vibolteav@gmail.com>
2020-05-25 10:24:43 +0700

    customize label on assessment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200522142638) do
+ActiveRecord::Schema.define(version: 20200525030557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
