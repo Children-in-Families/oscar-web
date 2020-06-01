a2e339aa073a49736a2396834eb1359676279f37
kirykr <kirymeas@gmail.com>
2020-06-01 14:05:39 +0700

    fixed since date parsing in api/v1/organization

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -9,12 +9,13 @@ module Api
 
       def clients
         bulk_clients = []
+        date_time_param = Time.parse("#{params[:since_date]}") if params[:since_date].present?
         Organization.only_integrated.pluck(:short_name).each do |short_name|
           Organization.switch_to short_name
           if params.dig(:since_date).present? && params.dig(:referred_external).present?
-            clients = JSON.parse ActiveModel::ArraySerializer.new(Client.where("created_at >= ? OR updated_at >= ? AND status = ?", "#{params[:since_date]}", "#{params[:since_date]}", 'Referred').limit(10).to_a, each_serializer: OrganizationClientSerializer).to_json
+            clients = JSON.parse ActiveModel::ArraySerializer.new(Client.where("created_at >= ? OR updated_at >= ? AND status = ?", date_time_param, date_time_param, 'Referred').limit(10).to_a, each_serializer: OrganizationClientSerializer).to_json
           elsif params.dig(:since_date).present?
-            clients = JSON.parse ActiveModel::ArraySerializer.new(Client.where("created_at >= ? OR updated_at >= ?", "#{params[:since_date]}", "#{params[:since_date]}").limit(10).to_a, each_serializer: OrganizationClientSerializer).to_json
+            clients = JSON.parse ActiveModel::ArraySerializer.new(Client.where("created_at >= ? OR updated_at >= ?", date_time_param, date_time_param).limit(10).to_a, each_serializer: OrganizationClientSerializer).to_json
           elsif params.dig(:referred_external).present?
             clients = JSON.parse ActiveModel::ArraySerializer.new(Client.where(status: 'Referred').limit(10).to_a, each_serializer: OrganizationClientSerializer).to_json
           else
