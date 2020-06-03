23e14475dec10774a122a42b0c55a7b5febefefa
vibol.t <vibolteav@gmail.com>
2020-06-03 10:41:19 +0700

    updated show page

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1278,4 +1278,8 @@ module ClientsHelper
       QuantitativeType.unscoped.order("substring(quantitative_types.name, '^[0-9]+')::int, substring(quantitative_types.name, '[^0-9]*$')")
     end
   end
+
+  def get_address(address_name)
+    @client.public_send("#{address_name}") ? [@client.public_send("#{address_name}").slice('id', 'name')] : []
+  end
 end
