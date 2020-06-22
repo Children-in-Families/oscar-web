8c5f2c701c6dd74408f7973170ae4e828da14f56
kirykr <kirymeas@gmail.com>
2020-06-22 20:12:09 +0700

    changed 20200408083757_create_global_identities.rb DO NOT delete global_identities table

--- db/migrate/20200408083757_create_global_identities.rb
+++ db/migrate/20200408083757_create_global_identities.rb
@@ -1,6 +1,6 @@
 class CreateGlobalIdentities < ActiveRecord::Migration
   def up
-    drop_table :global_identities, force: :cascade if table_exists? :global_identities
+    # drop_table :global_identities, force: :cascade if table_exists? :global_identities
 
     if !table_exists? :global_identities
       create_table  :global_identities,
@@ -12,25 +12,25 @@ class CreateGlobalIdentities < ActiveRecord::Migration
         end
     end
 
-    if schema_search_path == "\"public\""
-      execute <<-SQL.squish
-        insert into public.global_identities select encode(public.global_identity_tmp.ulid, 'escape') ulid from public.global_identity_tmp;
-      SQL
-    end
+    # if schema_search_path == "\"public\""
+    #   execute <<-SQL.squish
+    #     insert into public.global_identities select encode(public.global_identity_tmp.ulid, 'escape') ulid from public.global_identity_tmp;
+    #   SQL
+    # end
   end
 
   def down
-    if !table_exists? :global_identity_tmp
-      create_table :global_identity_tmp, force: :cascade do |t|
-        t.binary :ulid, limit: 16
-        t.string :ngo_name
-        t.integer :client_id
-      end
+    # if !table_exists? :global_identity_tmp
+    #   create_table :global_identity_tmp, force: :cascade do |t|
+    #     t.binary :ulid, limit: 16
+    #     t.string :ngo_name
+    #     t.integer :client_id
+    #   end
 
-      execute <<-SQL.squish
-        insert into public.global_identity_tmp (SELECT public.global_identities.id, public.global_identities.ulid, CAST(#{schema_search_path.gsub('"', '\'')} as varchar) ngo_name, clients.id client_id from public.global_identities INNER JOIN clients ON clients.global_id = public.global_identities.id WHERE NOT EXISTS (SELECT public.global_identity_tmp.id FROM public.global_identity_tmp WHERE public.global_identity_tmp.id = public.global_identities.id));
-      SQL
-    end
-    drop_table :global_identities, force: :cascade
+    #   execute <<-SQL.squish
+    #     insert into public.global_identity_tmp (SELECT public.global_identities.id, public.global_identities.ulid, CAST(#{schema_search_path.gsub('"', '\'')} as varchar) ngo_name, clients.id client_id from public.global_identities INNER JOIN clients ON clients.global_id = public.global_identities.id WHERE NOT EXISTS (SELECT public.global_identity_tmp.id FROM public.global_identity_tmp WHERE public.global_identity_tmp.id = public.global_identities.id));
+    #   SQL
+    # end
+    # drop_table :global_identities, force: :cascade
   end
 end
