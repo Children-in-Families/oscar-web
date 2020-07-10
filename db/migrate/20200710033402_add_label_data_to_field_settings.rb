303f1bb35129293dd1f459626c62c51b772cb60e
Vibol T <vibolteav@gmail.com>
2020-07-10 18:21:19 +0700

    Revert "remove migration file to run on next deploy"
    This reverts commit a5d59e24cb8ee5f2269ad46ac1cb2a7677f2fe8b.

---
+++ db/migrate/20200710033402_add_label_data_to_field_settings.rb
@@ -0,0 +1,49 @@
+class AddLabelDataToFieldSettings < ActiveRecord::Migration
+  FIELDS = {
+    referee_address: {
+      group: :referee,
+      klass_name: :referee,
+      current_label: 'Address',
+      label: 'Current Address'
+    },
+    referral_info: {
+      group: :client,
+      klass_name: :client,
+      current_label: 'Client / Referral Information',
+      label: 'Client Information'
+    },
+    carer_information: {
+      klass_name: :carer,
+      group: :carer,
+      current_label: 'Carer Information?',
+      label: 'Caregiver Information'
+    },
+    case_note: {
+      klass_name: :case_note,
+      group: :client,
+      current_label: 'Case Notes',
+      label: 'Implementation Log'
+    }
+  }
+
+  def up
+    return if Apartment::Tenant.current_tenant == 'shared'
+
+    FIELDS.each do |name, data|
+      field_setting = FieldSetting.create!(
+        name: name,
+        current_label: data[:current_label],
+        label: (Apartment::Tenant.current_tenant == 'ratanak' ? data[:label] : nil),
+        klass_name: data[:klass_name],
+        required: true,
+        label_only: true,
+        visible: true,
+        group: data[:group] || :client
+      )
+    end
+  end
+
+  def down
+    FieldSetting.where(name: FIELDS.keys).delete_all
+  end
+end
