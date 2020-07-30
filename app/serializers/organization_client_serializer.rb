07f6e9eeb2721a8c75971098c6f6064546b04982
kirykr <kirymeas@gmail.com>
2020-07-30 14:45:51 +0700

    address organization address code to organization_client_serializer

--- app/serializers/organization_client_serializer.rb
+++ app/serializers/organization_client_serializer.rb
@@ -4,7 +4,7 @@ class OrganizationClientSerializer < ActiveModel::Serializer
   attributes :slug, :global_id, :external_id, :external_id_display, :mosvy_number, :mosvy_number, :given_name, :family_name,
              :gender, :date_of_birth, :location_current_village_code, :address_current_village_code,
              :organization_id, :organization_name, :external_case_worker_name, :external_case_worker_id, :reason_for_referral,
-             :services, :status, :case_worker_name, :case_worker_mobile, :is_referred, :referral_consent_form
+             :services, :status, :case_worker_name, :case_worker_mobile, :is_referred, :referral_consent_form, :organization_address_code
 
   def organization_id
     Organization.current.id
@@ -48,6 +48,18 @@ class OrganizationClientSerializer < ActiveModel::Serializer
 
   def case_worker_mobile
     object.users.map(&:mobile).reject(&:blank?).join(", ")
+  end
+
+  def organization_address_code
+    setting = Setting.first
+    return '' unless setting.province
+    if setting.commune
+      setting.commune.code
+    elsif setting.district
+      setting.district.code
+    else
+      setting.province.district.first.code
+    end
   end
 
   def location_current_village_code
