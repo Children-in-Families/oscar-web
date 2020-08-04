859c9bb3bd16b0d0e223d3806b8b376024a4c90b
Vibol Teav <vibolteav@gmail.com>
2020-08-04 14:52:57 +0700

    fixed loading dev data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200723075102) do
+ActiveRecord::Schema.define(version: 20200803082706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1664,6 +1664,7 @@ ActiveRecord::Schema.define(version: 20200723075102) do
     t.string   "delete_incomplete_after_period_unit",  default: "days"
     t.boolean  "use_screening_assessment",             default: false
     t.integer  "screening_assessment_form_id"
+    t.boolean  "show_prev_assessment",                 default: false
   end
 
   add_index "settings", ["commune_id"], name: "index_settings_on_commune_id", using: :btree
