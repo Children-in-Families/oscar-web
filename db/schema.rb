86b6e34dd249cf3faa3d4db77d311dfc1de49a37
Vibol Teav <vibolteav@gmail.com>
2020-08-24 14:27:01 +0700

    Revert "temp update db structure for postgres 9.4"
    This reverts commit e49ff93cd455bffbb2105ec35eef5f11cacc8c5b.

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20200810070640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-  enable_extension "pgcrypto"
   enable_extension "uuid-ossp"
 
   create_table "able_screening_questions", force: :cascade do |t|
@@ -1009,7 +1008,7 @@ ActiveRecord::Schema.define(version: 20200810070640) do
   add_index "global_identity_organizations", ["organization_id"], name: "index_global_identity_organizations_on_organization_id", using: :btree
 
   create_table "global_services", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: "gen_random_uuid()"
+    t.uuid "uuid", default: "uuid_generate_v4()"
   end
 
   add_index "global_services", ["uuid"], name: "index_global_services_on_uuid", unique: true, using: :btree
