350032691b61783691fc537077bce8f872cbc260
kirykr <kirymeas@gmail.com>
2020-09-10 20:42:01 +0700

    added slug to ClientShareExternalSerializer

--- app/serializers/client_share_external_serializer.rb
+++ app/serializers/client_share_external_serializer.rb
@@ -1,5 +1,5 @@
 class ClientShareExternalSerializer < ActiveModel::Serializer
-  attributes  :given_name, :family_name, :local_given_name, :local_family_name, :gender, :date_of_birth, :global_id, :external_id, :external_id_display, :mosvy_number, :location_current_village_code
+  attributes  :given_name, :family_name, :local_given_name, :local_family_name, :gender, :date_of_birth, :global_id, :slug, :external_id, :external_id_display, :mosvy_number, :location_current_village_code
 
 
   def location_current_village_code
