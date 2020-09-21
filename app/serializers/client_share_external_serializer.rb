4a8a8a9d44c4eb296716d2688e0659c90e7b260b
kirykr <kirymeas@gmail.com>
2020-09-21 08:58:25 +0700

    update return string if value is null

--- app/serializers/client_share_external_serializer.rb
+++ app/serializers/client_share_external_serializer.rb
@@ -3,6 +3,18 @@ class ClientShareExternalSerializer < ActiveModel::Serializer
 
 
   def location_current_village_code
-    object.village_code || object.commune_code || object.district_code
+    object.village_code || object.commune_code || object.district_code || ""
+  end
+
+  def external_id
+    object.external_id || ""
+  end
+
+  def external_id_display
+    object.external_id_display || ""
+  end
+
+  def mosvy_number
+    object.mosvy_number || ""
   end
 end
