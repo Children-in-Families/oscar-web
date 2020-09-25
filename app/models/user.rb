4cc5416666f4634aa2577c0c8bf5f97f50d9c0e0
kirykr <kirymeas@gmail.com>
2020-09-25 09:01:08 +0700

    added gender to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   MANAGERS = ROLES.select { |role| role if role.include?('manager') }
   LANGUAGES = { en: :english, km: :khmer, my: :burmese }.freeze
 
-  GENDER_OPTIONS = ['female', 'male', 'other', 'prefer not to say']
+  GENDER_OPTIONS = ['female', 'lgbt', 'male', 'non_specified', 'other', 'unknown', 'prefer_not_to_say']
 
   devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :trackable, :validatable
