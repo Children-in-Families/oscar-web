a09e0bfc7187d554dfcdafa0115e05e398d99dc2
kirykr <kirymeas@gmail.com>
2020-11-11 14:00:18 +0700

    removed touch method callback in client enrollment

--- app/models/client_enrollment.rb
+++ app/models/client_enrollment.rb
@@ -28,7 +28,6 @@ class ClientEnrollment < ActiveRecord::Base
   after_create :set_client_status
   after_save :create_client_enrollment_history
   after_destroy :reset_client_status
-  after_commit :touch
 
   validate do |obj|
     CustomFormPresentValidator.new(obj, 'program_stream', 'enrollment').validate
