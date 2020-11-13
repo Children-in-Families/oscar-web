753ae392e445875492069648a203771c08fc5102
Darren Jensen <darren.jensen@gmail.com>
2020-11-13 17:06:46 +0700

    some updates and new config for AWS Fargate

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,22 @@ rm -f /app/tmp/pids/server.pid
 
 echo "Waiting PostgreSQL to start on 5432..."
 
-while ! pg_isready -h db; do
+while ! pg_isready -h $DATABASE_HOST; do
   sleep 0.5
   echo "Database not ready, please wait!"
 done
 
 echo "PostgreSQL started. Setting up databases..."
 
-# Create the development database
-rake db:exists || (rake db:create && rake db:schema:load && rake db:seed)
+if [ "$RAILS_ENV" = "development" ]; then
+  # Create the development database
+  rake db:exists || (rake db:create && rake db:schema:load && rake db:seed)
 
-# Import development data
-echo "Preparing to import dev data. Please wait..."
-rake import:dev_env_data
-echo "Dev data imported! Startup up Rails!"
+  # Import development data
+  echo "Preparing to import dev data. Please wait..."
+  rake import:dev_env_data
+  echo "Dev data imported! Startup up Rails!"
+fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
