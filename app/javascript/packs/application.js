5e899599a2bab926fc658689923fb7d21ab3ee4e
kirykr <kirymeas@gmail.com>
2020-11-23 15:06:43 +0700

    move frontend catching error to a saparate file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,29 +18,7 @@
 import "primereact/resources/themes/nova-light/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-const Appsignal = require("@appsignal/javascript").default;
-
-const appsignal = new Appsignal({
-  key: window.appsignalFrontendKey,
-});
-window.appsignal = appsignal;
 
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
-
-window.onerror = function (msg, url, line, col, error) {
-  console.log("onerror triggered");
-  if (error instanceof Error) {
-    appsignal.sendError(error);
-    return false;
-  }
-};
-
-window.onunhandledrejection = function (msg, url, line, col, error) {
-  console.log("onunhandledrejection triggered");
-  if (error instanceof Error) {
-    appsignal.sendError(error);
-    return false;
-  }
-};
