b0291a068df79c84c633c362f1a4f5843e1938d9
Pirun Seng <sengpirun.rain@gmail.com>
2020-11-27 14:07:44 +0700

    reactives some enrollment tracking methods compared to client_enrollment_tracking methods

--- app/models/enrollment_tracking.rb
+++ app/models/enrollment_tracking.rb
@@ -7,15 +7,16 @@ class EnrollmentTracking < ActiveRecord::Base
 
   has_paper_trail
 
-  # Todo: check if need commented code back
-  # scope :ordered, -> { order(:created_at) }
+  scope :ordered, -> { order(:created_at) }
   scope :enrollment_trackings_by, -> (tracking) { where(tracking_id: tracking) }
 
-  # delegate :program_stream, to: :client_enrollment
+  # may be used later in family book
+  # delegate :program_stream, to: :enrollment
   # delegate :name, to: :program_stream, prefix: true
 
-  # after_save :create_client_enrollment_tracking_history
+  after_save :create_entity_enrollment_tracking_history
 
+  # may be used later in family grid
   # def self.properties_by(value, object=nil)
   #   value = value.gsub(/\'+/, "''")
   #   field_properties = select("client_enrollment_trackings.id, client_enrollment_trackings.properties ->  '#{value}' as field_properties").collect(&:field_properties)
@@ -26,9 +27,9 @@ class EnrollmentTracking < ActiveRecord::Base
     form_builder_attachments.find_by(name: value)
   end
 
-  # private
+  private
 
-  # def create_client_enrollment_tracking_history
-  #   ClientEnrollmentTrackingHistory.initial(self)
-  # end
+  def create_entity_enrollment_tracking_history
+    EntityEnrollmentTrackingHistory.initial(self)
+  end
 end
--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -7,9 +7,6 @@ class Tracking < ActiveRecord::Base
   has_many :client_enrollment_trackings, dependent: :restrict_with_error
   has_many :client_enrollments, through: :client_enrollment_trackings
 
-  # has_many :enrollment_trackings, dependent: :restrict_with_error
-  # has_many :enrollments, through: :enrollment_trackings
-
   has_paper_trail
 
 
