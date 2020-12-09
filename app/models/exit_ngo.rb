45a30cebe368cd1ef631dc97a85c97cfac9d8be1
Pirun Seng <sengpirun.rain@gmail.com>
2020-12-09 19:48:56 +0700

    WIP exit family when status is Accepted

--- app/models/exit_ngo.rb
+++ app/models/exit_ngo.rb
@@ -8,6 +8,7 @@ class ExitNgo < ActiveRecord::Base
   alias_attribute :new_date, :exit_date
 
   EXIT_REASONS = ['Client is/moved outside NGO target area (within Cambodia)', 'Client is/moved outside NGO target area (International)', 'Client refused service', 'Client does not meet / no longer meets service criteria', 'Client died', 'Client does not require / no longer requires support', 'Agency lacks sufficient resources', 'Other']
+  FAMILY_EXIT_REASONS = ['Family is/moved outside NGO target area (within Cambodia)', 'Family is/moved outside NGO target area (International)', 'Family refused service', 'Family does not meet / no longer meets service criteria', 'Family died', 'Family does not require / no longer requires support', 'Agency lacks sufficient resources', 'Other']
 
   scope :most_recents, -> { order(created_at: :desc) }
 
