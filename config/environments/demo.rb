be74db936fffdc79127968263cf06198b38f51fb
kirykr <kirymeas@gmail.com>
2021-01-04 13:58:43 +0700

    update log size in demo.rb

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 50 * 1024 * 1024)
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   # Do not dump schema after migrations.
