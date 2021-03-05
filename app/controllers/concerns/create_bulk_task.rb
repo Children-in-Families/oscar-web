fc24c68650d41971c62724fce33c95496b2565b0
kirykr <kirymeas@gmail.com>
2021-03-05 15:20:11 +0700

    fixed wrong expected date in created task

--- app/controllers/concerns/create_bulk_task.rb
+++ app/controllers/concerns/create_bulk_task.rb
@@ -6,7 +6,7 @@ module CreateBulkTask
       task_attr['name'] = task_attr['name'].gsub('qout', '"').gsub('apos', "'")
       task_attr['taskable_id'] = parent.id
       task_attr['taskable_type'] = case_note.class.to_s
-      task_attr['expected_date'] = case_note.meeting_date.to_s
+      task_attr['expected_date'] = task_attr['expected_date']
 
       task_attr.merge('case_note_id' => parent.id.to_s, user_id: current_user.id, domain_group_identity: task_attr['domain_group_identity'].to_s)
     end
