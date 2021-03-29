1aa7ace6592c2f5a1e60dbaf28be78869c03ebad
kirykr <kirymeas@gmail.com>
2021-03-29 17:28:05 +0700

    export user and fixed people SessionOverFlow

--- app/helpers/form_builder_attachments_helper.rb
+++ app/helpers/form_builder_attachments_helper.rb
@@ -1,7 +1,7 @@
 module FormBuilderAttachmentsHelper
   def form_buildable_path(resource, index, label, params_program_stream)
     if controller_name == 'leave_programs' || controller_name == 'leave_enrolled_programs'
-      link_to polymorphic_path([@client, resource.client_enrollment, resource], program_stream_id: @program_stream.id, file_index: index, file_name: label, program_streams: params_program_stream), method: :delete, data: { confirm: t('.are_you_sure') }, class: "delete btn btn-outline btn-danger #{disabled?}" do
+      link_to polymorphic_path([@client || resource.client_enrollment&.client, resource.client_enrollment, resource], program_stream_id: @program_stream.id, file_index: index, file_name: label, program_streams: params_program_stream), method: :delete, data: { confirm: t('.are_you_sure') }, class: "delete btn btn-outline btn-danger #{disabled?}" do
         fa_icon('trash')
       end
     elsif controller_name == 'client_enrollment_trackings' || controller_name == 'client_enrolled_program_trackings'
