dffb378a500c1fd0c76498adb763c238a007adfc
kirykr <kirymeas@gmail.com>
2021-06-16 23:26:12 +0700

    updated sent to NGO usage report

--- app/mailers/ngo_usage_report_mailer.rb
+++ app/mailers/ngo_usage_report_mailer.rb
@@ -1,7 +1,7 @@
 class NgoUsageReportMailer < ApplicationMailer
   def send_report(date_time, previous_month)
     @previous_month = previous_month
-    emails    = [ENV['CIF1_EMAIL'], ENV['CIF2_EMAIL']]
+    emails    = [ENV['CIF1_EMAIL'], ENV['CIF2_EMAIL'], ENV['CIF3_EMAIL'], ENV['CIF4_EMAIL']]
     dev_email = ENV['DEV_EMAIL']
     attachments["OSCaR-usage-report-#{date_time}.xls"] = File.read(Rails.root.join("tmp/OSCaR-usage-report-#{date_time}.xls"))
     mail(to: emails, subject: "OSCaR Usage Report of #{@previous_month}", bcc: dev_email)
