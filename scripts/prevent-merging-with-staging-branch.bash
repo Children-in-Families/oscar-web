c2ee762192ab37e549953afada3139c3245cbe71
kirykr <kirymeas@gmail.com>
2021-06-22 09:33:33 +0700

    prevent code merged with staging

---
+++ scripts/prevent-merging-with-staging-branch.bash
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+getBranchName()
+{
+    echo $(git rev-parse --abbrev-ref HEAD)
+}
+
+getMergedBranches()
+{
+    echo $(git branch --merged)
+}
+
+if [ "$(getBranchName)" != "staging" ]; then
+    if [[ $(getMergedBranches) == *"staging"* ]]; then
+        echo "Don't create branches from the staging branch!"
+        exit 1
+    fi
+fi
