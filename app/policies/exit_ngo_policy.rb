4fa19c04a38f80aa2b7145f47c59da2c47b94af0
kirykr <kirymeas@gmail.com>
2021-06-25 15:15:31 +0700

    allow admin to destroy last exit ngo

--- app/policies/exit_ngo_policy.rb
+++ app/policies/exit_ngo_policy.rb
@@ -1,11 +1,20 @@
 class ExitNgoPolicy < ApplicationPolicy
   def edit?
-    if record.attached_to_family?
-      entity = Family.find(record.rejectable_id)
-    else
-      entity = Client.find(record.client_id)
-    end
+    entity = find_entity
     (entity.exit_ngo? && user.admin?) || (!entity.exit_ngo? && !user.strategic_overviewer?)
+  end
+
+  def destroy?
+    entity = find_entity
+    (entity.exit_ngo? && user.admin?) && entity.exit_ngos.last == record
+  end
+
+  def find_entity
+    if record.attached_to_family?
+      Family.find(record.rejectable_id)
+    else
+      Client.find(record.client_id)
+    end
   end
 
   alias update? edit?
