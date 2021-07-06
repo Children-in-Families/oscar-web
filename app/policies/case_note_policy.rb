511cc48b50b3cb72d1d51664bf453ee8e943af29
kirykr <kirymeas@gmail.com>
2021-07-06 21:43:47 +0700

    set case note policy limit edit duration only Ratanak

--- app/policies/case_note_policy.rb
+++ app/policies/case_note_policy.rb
@@ -1,6 +1,10 @@
 class CaseNotePolicy < ApplicationPolicy
   def edit?
-    return true if user.admin? || record.is_editable?
+    if Organization.ratanak?
+      return true if record.is_editable?
+    else
+      return true if user.admin?
+    end
     DateTime.now.in_time_zone(Time.zone) <= record.created_at + 24.hours
   end
 
