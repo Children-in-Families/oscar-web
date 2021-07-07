68b71e60e4a84ab27892c913e0c274bb9d586af1
kirykr <kirymeas@gmail.com>
2021-07-07 22:06:22 +0700

    reverted change in case note policy

--- app/policies/case_note_policy.rb
+++ app/policies/case_note_policy.rb
@@ -1,13 +1,14 @@
 class CaseNotePolicy < ApplicationPolicy
   def edit?
     if Organization.ratanak?
-      return true
+      return true if record.is_editable?
     else
       return true if user.admin?
     end
     DateTime.now.in_time_zone(Time.zone) <= record.created_at + 24.hours
   end
 
+  alias create? edit?
   alias update? edit?
 
   class Scope < Scope
