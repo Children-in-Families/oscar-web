a74575155070ef59b301aeac6471dbd2ca8520ee
kirykr <kirymeas@gmail.com>
2021-07-14 21:29:52 +0700

    fixed edit limit case note

--- app/policies/case_note_policy.rb
+++ app/policies/case_note_policy.rb
@@ -2,7 +2,7 @@ class CaseNotePolicy < ApplicationPolicy
   def edit?
     return true if user.admin?
     if Organization.ratanak?
-      return false if !record.is_editable?
+      record.is_editable? ? true : false
     else
       DateTime.now.in_time_zone(Time.zone) <= record.created_at + 24.hours
     end
