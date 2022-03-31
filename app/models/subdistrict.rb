9e019a6e177f61711aa349054b4eda65eae78d89
kirykr <kirymeas@gmail.com>
2022-03-31 11:18:31 +0700

    update callback in subdistrict

--- app/models/subdistrict.rb
+++ app/models/subdistrict.rb
@@ -1,12 +1,13 @@
 class Subdistrict < ActiveRecord::Base
   include AddressConcern
 
-  after_commit :flush_cache
   belongs_to :district, touch: true
   has_many :clients, dependent: :restrict_with_error
 
   validates :district, presence: true
   validates :name, presence: true, uniqueness: { case_sensitive: false, scope: [:district_id] }
+
+  after_commit :flush_cache
 
   def self.cached_find(id)
     Rails.cache.fetch([Apartment::Tenant.current, self.name, id]) { find(id) }
