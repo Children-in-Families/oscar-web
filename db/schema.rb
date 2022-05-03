e539200ef9761e4b520ca7739097e651524ae1f9
= <vibolteav@gmail.com>
2022-05-03 10:56:05 +0700

    fixed schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220427082949) do
+ActiveRecord::Schema.define(version: 20220503013717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
