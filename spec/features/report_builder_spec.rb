5ae5c0f915b75770f629c5ee47b3ea90f31acf3b
lysa <lysa@web-essentials.co>
2022-05-11 14:52:46 +0700

    [IMP] Update test
    refs OSC-18

--- spec/features/report_builder_spec.rb
+++ spec/features/report_builder_spec.rb
@@ -3,6 +3,7 @@ describe 'Report Builder' do
   let!(:client) { create(:client, :accepted, given_name: 'Adam', family_name: 'Eve', local_given_name: 'Juliet', local_family_name: 'Romeo', date_of_birth: 10.years.ago, users: [user]) }
 
   before do
+    Rails.cache.clear
     login_as(user)
     Rails.cache.clear
   end
@@ -13,8 +14,18 @@ describe 'Report Builder' do
       find('.client-search').click
       expect(page).to have_content('Search')
       find('#client-grid-search-btn').click
-      wait_for_ajax()
+      sleep 10
       expect(page).to have_content("Adam")
+    end
+  end
+
+  feature 'Search family', js: true do
+    let!(:family){ create(:family, children: [client.id] ) }
+    scenario 'for family' do
+      visit families_path
+      find('#client-grid-search-btn').click
+      sleep 10
+      expect(page).to have_content(family.id)
     end
   end
 
