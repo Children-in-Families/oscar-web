5d71ca21e2f14a34dc5e8a5452a5d533c8cee54e
lysa <lysa@web-essentials.co>
2022-05-12 08:47:18 +0700

    [IMP] Add clear cache test
    refs OSC-18

--- spec/features/report_builder_spec.rb
+++ spec/features/report_builder_spec.rb
@@ -9,12 +9,11 @@ describe 'Report Builder' do
 
   feature 'Search client', js: true do
     scenario 'for client' do
-      Rails.cache.clear
       visit clients_path
       find('.client-search').click
       expect(page).to have_content('Search')
       find('#client-grid-search-btn').click
-      sleep 10
+      wait_for_ajax()
       expect(page).to have_content("Adam")
     end
   end
@@ -22,11 +21,10 @@ describe 'Report Builder' do
   feature 'Search family', js: true do
     let!(:family){ create(:family, children: [client.id] ) }
     scenario 'for family' do
-      Rails.cache.clear
       visit families_path
       fill_in 'Family ID', with: family.id
       find('#client-grid-search-btn').click
-      sleep 10
+      wait_for_ajax()
       expect(page).to have_content(family.id)
     end
   end
