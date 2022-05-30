639e6f6059d9a5acc6f1fd9c21bb09ad73edbefe
kirykr <kirymeas@gmail.com>
2022-05-30 13:34:22 +0700

    created demo branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :stage, 'production'
 set :rails_env, :production
 set :appsignal_env, :production
-set :branch, proc { `git rev-parse --abbrev-ref stable`.chomp }
+set :branch, proc { `git rev-parse --abbrev-ref demo-production`.chomp }
 # TODO: Deploy one after another by commentting out one
 
 # OSCaR
-server '52.221.46.112', user: 'deployer', roles: %w{app web db}
+server '13.215.194.167', user: 'deployer', roles: %w{app web db}
