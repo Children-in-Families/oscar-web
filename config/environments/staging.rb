fcd5ba6d454d9ea3da73d7d7f3e860f1f9b92663
kirykr <kirymeas@gmail.com>
2022-08-22 14:29:53 +0700

    Added Gem terser and update js_compressor in staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = false
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = :terser
   # config.assets.css_compressor = :sass
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
