250dc2cf48d4aaf7711c59e2d44c92719da20687
kirykr <kirymeas@gmail.com>
2022-10-05 12:25:33 +0700

    Fixed NoMethod error. in app/helpers/custom_field_properties_helper.rb

--- app/helpers/custom_field_properties_helper.rb
+++ app/helpers/custom_field_properties_helper.rb
@@ -58,6 +58,8 @@ module CustomFieldPropertiesHelper
   end
 
   def remove_local_field_prop_unicode(field_props)
+    return field_props if field_props["local_label"].nil?
+
     field = field_props["local_label"].gsub(/\&gt\;|\&lt\;|\&amp\;|\"/, '&lt;' => '<', '&gt;' => '>', '&amp;' => '&', '"' => '%22')
   end
 
