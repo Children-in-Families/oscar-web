9146cc815f1b0f3e9c75aff615a72cbb4f07979e
kirykr <kirymeas@gmail.com>
2022-10-05 09:57:37 +0700

    updated deploy ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,4 @@ set :branch, proc { `git rev-parse --abbrev-ref demo-production`.chomp }
 # TODO: Deploy one after another by commentting out one
 
 # OSCaR
-server '52.221.240.90', user: 'deployer', roles: %w{app web db}
+server '54.255.207.134', user: 'deployer', roles: %w{app web db}
