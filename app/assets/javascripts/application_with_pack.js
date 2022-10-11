b3324d7d4ded4444c3dbac16a593d488b40f3fb9
kirykr <kirymeas@gmail.com>
2022-10-11 11:44:35 +0700

    Update react-datetime-picker AM/PM not triggered action

---
+++ app/assets/javascripts/application_with_pack.js
@@ -0,0 +1,62 @@
+// FRAMEWORK
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require bootstrap-sprockets
+//= require jquery.steps.min
+//= require jquery.validate
+//= require jquery.validate.additional-methods
+//= require jquery.nicescroll.min
+
+//= require editable/bootstrap-editable
+//= require editable/rails
+//= require jQuery.print
+
+//= require image_upload_previewer/image_upload_previewer
+//= require image_upload
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.en-GB.js
+//= require datepicker
+//= require metisMenu/jquery.metisMenu.js
+//= require lodash/lodash.min.js
+// require query_builder/query-builder.standalone.min.js
+//= require jquery.extendext
+//= require doT
+//= require query-builder
+
+//remove query_builder/utils, query_builder/model and query_builder/plugin bcuz conflict with form-builder select option not show
+
+//= require moment
+//= require fullcalendar
+//= require bootstrap_file_input/purify.min.js
+//= require bootstrap_file_input/fileinput.js
+//= require bootstrap_file_input/fa/theme.min.js
+//= require bootstrap_file_input/explorer/theme.min.js
+
+// WRAPBOOTSTRAP
+//= require iCheck/icheck.min.js
+//= require wrapbootstrap/inspinia.js
+//= require slimscroll/jquery.slimscroll.min.js
+//= require toastr/toastr.min.js
+//= require bootstrap-tour/bootstrap-tour.js
+
+// LADDA
+//= require ladda/spin.min
+//= require ladda/ladda.min
+//= require ladda/ladda.jquery.min
+
+//LOAD MODULE
+//= require namespace
+//= require util
+//= require initializer
+//= require common
+//= require jquery.infinitescroll.min
+//= require footable.all.min
+
+//APPLICATION JS
+//= require calendars/index
+//= require clients/index
+//= require report_creator
+//= require clients/show
+//= require clients/form
+//= require clients/book
