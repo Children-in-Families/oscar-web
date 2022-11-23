6a043cbe60c05b9eb2e98fc7f343148883c05dd0
kirykr <kirymeas@gmail.com>
2022-11-23 09:49:40 +0700

    Fixed referral to external system error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -444,10 +444,14 @@ module ApplicationHelper
       ngos << ["MoSVY External System", "MoSVY External System"]
       ngos << ["I don't see the NGO I'm looking for...", "external referral", disabled: @referral&.referred_to != 'external referral']
     else
-      ngos << ["MoSVY External System", "MoSVY External System", disabled: @referral&.referred_to != 'external referral']
+      ngos << ["MoSVY External System", "MoSVY External System", disabled: @referral&.referred_to != 'MoSVY External System'] if is_ngo_share_to_external?
       ngos << ["I don't see the NGO I'm looking for...", "external referral", disabled: @referral&.referred_to != 'external referral']
     end
     ngos
+  end
+
+  def is_ngo_share_to_external?
+    current_organization.integrated?
   end
 
   def initial_referral_date_picker_format(entity)
