2123153f3588f2a9ba04667d070815a3f605c76b
kirykr <kirymeas@gmail.com>
2023-04-13 16:43:43 +0700

    Fixed error in custom field property new form

--- app/models/custom_field_property.rb
+++ app/models/custom_field_property.rb
@@ -8,7 +8,7 @@ class CustomFieldProperty < ActiveRecord::Base
   belongs_to :custom_field
   belongs_to :user
 
-  scope :by_custom_field, -> (value) { where(custom_field:  value) }
+  scope :by_custom_field, ->(value) { where(custom_field:  value) }
   scope :most_recents,    ->         { order(created_at: :desc) }
 
   has_paper_trail
