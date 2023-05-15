59b924779288cc8bc0542cad0844d8d2189a2627
Vibol Teav <vibolteav@gmail.com>
2023-05-15 19:14:20 +0700

    fix missing notification object

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,11 +7,20 @@ class AdminController < ApplicationController
   protected
 
   def notify_user
-    if params[:user_id] || controller_name == 'notifications'
+    if preload_notifications?
       clients = Client.none.accessible_by(current_ability).non_exited_ngo
       @notification = UserNotification.new(current_user, clients)
     else
       @lazy_load_notification = true
     end
+  end
+
+  def preload_notifications?
+    controller_name == 'notifications' ||
+    (dashboard_request? && (params[:user_id] || current_user.case_worker? || current_user.manager?))
+  end
+
+  def dashboard_request?
+    controller_name == 'dashboards' && action_name == 'index'
   end
 end
