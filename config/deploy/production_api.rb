5b02c9fcd7e7c19faa814288b7b361291b64f431
kirykr <kirymeas@gmail.com>
2023-06-14 14:34:34 +0700

    removed gem and updae capfile

--- config/deploy/production_api.rb
+++ config/deploy/production_api.rb
@@ -1,7 +1,7 @@
 set :stage, 'production_api'
 set :rails_env, :production_api
 set :appsignal_env, :production_api
-set :branch, proc { `git rev-parse --abbrev-ref stable`.chomp }
+set :branch, proc { `git rev-parse --abbrev-ref 2023060603-create-instant-for-primero-api`.chomp }
 # TODO: Deploy one after another by commentting out one
 
 # OSCaR
