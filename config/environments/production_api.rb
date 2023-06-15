ac50dc1c0e32739e65cf2f737935c0466c2e0c03
kirykr <kirymeas@gmail.com>
2023-06-15 13:32:45 +0700

    set config.assets.compile = false

--- config/environments/production_api.rb
+++ config/environments/production_api.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.compile = false
   config.assets.debug = false
 
 
