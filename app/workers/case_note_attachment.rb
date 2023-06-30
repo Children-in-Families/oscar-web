2b642eacc1f22e4aea2f7eed917685fc336e7628
Vibol Teav <vibolteav@gmail.com>
2023-06-30 14:35:14 +0700

    refactor rake task to sidekiq

---
+++ app/workers/case_note_attachment.rb
@@ -0,0 +1,14 @@
+class CaseNoteAttachment
+  include Sidekiq::Worker
+
+  def perform(case_note_id, short_name)
+    Organization.switch_to short_name
+
+    case_note = CaseNote.find_by(id: case_note_id)
+    return if case_note.blank?
+    
+    attachments = case_note.case_note_domain_groups.map(&:attachments).flatten
+    case_note.attachments = attachments
+    case_note.save(validate: false)
+  end
+end
