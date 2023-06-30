d61b0510275db1e0228a2e0c8659f42407efd1c3
Vibol Teav <vibolteav@gmail.com>
2023-06-30 20:16:31 +0700

    fix rake task for staging & production

--- app/workers/case_note_attachment.rb
+++ app/workers/case_note_attachment.rb
@@ -8,7 +8,13 @@ class CaseNoteAttachment
     return if case_note.blank?
     
     attachments = case_note.case_note_domain_groups.map(&:attachments).flatten
-    case_note.attachments = attachments.map{ |attachment| Pathname.new(attachment.path).open }
+
+    if Rails.env.staging? || Rails.env.production? || Rails.env.demo? || Rails.env.ratanak_staging?
+      case_note.remote_attachments_urls = attachments.map(&:url)
+    else
+      case_note.attachments = attachments.map{ |attachment| Pathname.new(attachment.path).open }
+    end
+
     case_note.save(validate: false)
   rescue => e
     pp e.message
