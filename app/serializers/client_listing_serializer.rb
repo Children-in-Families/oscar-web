561839c83cdfabcf37d16a5ca492ca7461c2adac
kirykr <kirymeas@gmail.com>
2023-07-03 17:15:06 +0700

    Update API 1
    - Update Client Listing Serializer

--- app/serializers/client_listing_serializer.rb
+++ app/serializers/client_listing_serializer.rb
@@ -1,5 +1,9 @@
 class ClientListingSerializer < ActiveModel::Serializer
-  attributes  :id, :name, :assessments_count, :case_note_count, :task_count
+  attributes  :id, :name, :gender, :profile, :assessments_count, :case_notes_count, :tasks_count
+
+  def profile
+    object.profile.present? ? { uri: object.profile.url } : {}
+  end
 
   def case_notes_count
     object.case_notes.count
