7f0719df20fdcc3a0d051c4d98bf9003b1cb4400
kirykr <kirymeas@gmail.com>
2023-07-05 16:30:06 +0700

    Update API for Mobile
    - Refactored Risk Assessment
    - Update Client payload
    - Update Assessment payload

--- app/controllers/api/v1/program_streams_controller.rb
+++ app/controllers/api/v1/program_streams_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class ProgramStreamsController < Api::V1::BaseApiController
-
       def index
         render json: ProgramStream.complete.ordered
       end
--- app/controllers/concerns/assessment_concern.rb
+++ app/controllers/concerns/assessment_concern.rb
@@ -2,4 +2,14 @@ module AssessmentConcern
   def find_custom_assessment_setting
     @custom_assessment_setting = CustomAssessmentSetting.find_by(custom_assessment_name: params[:custom_name])
   end
+
+  def add_more_attachments(new_file, assessment_domain_id)
+    return unless new_file.present?
+
+    assessment_domain = AssessmentDomain.find(assessment_domain_id)
+    files = assessment_domain.attachments
+    files += new_file
+    assessment_domain.attachments = files
+    assessment_domain.save
+  end
 end
