00c3242cb2de7e715bf0d03ef97e33874bb3ef7f
kirykr <kirymeas@gmail.com>
2023-07-07 14:01:56 +0700

    Update API V1
    - Update client serializer: quantitative_cases
    - Update Task Serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :name, :expected_date, :completion_date, :domain, :completed, :user_id
+  attributes :id, :name, :expected_date, :completion_date, :domain, :completed, :user_id, :taskable_id, :taskable_type
 
   def domain
     object.domain.as_json(only: [:id, :name, :identity])
