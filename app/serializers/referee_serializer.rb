bd13267d3d58566655cd04e8c940df6ed73ecd96
kirykr <kirymeas@gmail.com>
2023-07-07 15:46:09 +0700

    Update referee_serializer added missing field

--- app/serializers/referee_serializer.rb
+++ app/serializers/referee_serializer.rb
@@ -1,8 +1,8 @@
 class RefereeSerializer < ActiveModel::Serializer
-  attributes  :id, :name, :address_type, :current_address, :email,
+  attributes  :id, :name, :address_type, :outside, :current_address, :email,
               :gender, :house_number, :outside_address, :street_number,
               :phone, :adult, :suburb, :description_house_landmark,
-              :directions, :street_line1, :street_line2, :plot,
+              :directions, :street_line1, :street_line2, :plot, :anonymous,
               :road, :postal_code, :state_id, :township_id, :subdistrict_id, :locality,
               :province_id, :district_id, :commune_id, :village_id
 end
