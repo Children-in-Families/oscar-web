3eba3a3e23d22a9fa3d640636f0d20ecb33aa906
kirykr <kirymeas@gmail.com>
2023-07-16 16:13:41 +0700

    Update API V1
    - Added field current_family_id to client_list_serializer

--- app/serializers/client_listing_serializer.rb
+++ app/serializers/client_listing_serializer.rb
@@ -1,5 +1,5 @@
 class ClientListingSerializer < ActiveModel::Serializer
-  attributes :id, :name, :gender, :profile, :date_of_birth, :assessments_count, :case_notes_count, :tasks_count
+  attributes :id, :name, :gender, :profile, :date_of_birth, :current_family_id, :assessments_count, :case_notes_count, :tasks_count
 
   def profile
     object.profile.present? ? { uri: object.profile.url } : {}
