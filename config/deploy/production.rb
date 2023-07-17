b5780552e512ee64f76d64168bf42c6b1410277d
kirykr <kirymeas@gmail.com>
2023-07-17 13:05:44 +0700

    updated production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,7 @@ set :branch, proc { `git rev-parse --abbrev-ref stable`.chomp }
 # TODO: Deploy one after another by commentting out one
 
 # OSCaR
-server '52.221.46.112', user: 'deployer', roles: %w{app web db}
+server '52.221.46.112', user: 'deployer', roles: %w{app web db}, ssh_options: {
+  :keepalive => true,
+  :keepalive_interval => 60 #seconds
+}
