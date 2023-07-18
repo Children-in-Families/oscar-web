6a5a97eb354b51b7976dc5a85aceb63e2d2fd0d5
Vibol Teav <vibolteav@gmail.com>
2023-07-19 05:33:05 +0700

    update CMS tools

--- config/routes.rb
+++ config/routes.rb
@@ -465,6 +465,7 @@ Rails.application.routes.draw do
 
   resources :settings, except: [:destroy] do
     collection do
+      get :screening_forms
       get 'default_columns' => 'settings#default_columns'
       get 'research_module' => 'settings#research_module'
       get 'custom_labels' => 'settings#custom_labels'
