28b0574e1b988145b428a3c5249bb348a8c974a5
kirykr <kirymeas@gmail.com>
2023-07-20 10:52:19 +0700

    Added client_id, note fields to case_note_serializer

--- app/serializers/case_note_serializer.rb
+++ app/serializers/case_note_serializer.rb
@@ -1,5 +1,5 @@
 class CaseNoteSerializer < ActiveModel::Serializer
-  attributes :attendee, :meeting_date, :interaction_type, :assessment_id, :id, :case_note_domain_groups, :selected_domain_group_ids, :attachments, :created_at, :updated_at
+  attributes :id, :attendee, :meeting_date, :interaction_type, :note, :client_id, :assessment_id, :case_note_domain_groups, :selected_domain_group_ids, :attachments, :created_at, :updated_at
 
   def case_note_domain_groups
     ActiveModel::ArraySerializer.new(object.case_note_domain_groups, each_serializer: CaseNoteDomainGroupSerializer)
