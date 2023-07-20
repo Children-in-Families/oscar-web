c9e083e00c4c6ab6a96d3d02d57c9a8aac06d00d
kirykr <kirymeas@gmail.com>
2023-07-20 09:53:14 +0700

    Fixed case note assessment attachment update

--- app/controllers/api/v1/case_notes_controller.rb
+++ app/controllers/api/v1/case_notes_controller.rb
@@ -12,7 +12,6 @@ module Api
         case_note.assessment = @client.assessments.custom_latest_record
         case_note.meeting_date = "#{case_note.meeting_date.strftime("%Y-%m-%d")}, #{Time.now.strftime("%H:%M:%S")}"
         if case_note.save
-          add_more_attachments(params[:case_note][:attachments]) if params.dig(:case_note, :attachments)
           case_note.complete_tasks(params[:case_note][:case_note_domain_groups_attributes], current_user.id) if params.dig(:case_note, :case_note_domain_groups_attributes)
           create_bulk_task(params[:task], case_note) if params.key?(:task)
           case_note.complete_screening_tasks(params) if params[:case_note].key?(:tasks_attributes)
@@ -36,7 +35,6 @@ module Api
 
         if case_note.update_attributes(case_note_params)
           if params.dig(:case_note, :case_note_domain_groups_attributes)
-            add_more_attachments(params[:case_note][:attachments]) if params.dig(:case_note, :attachments)
             case_note.complete_tasks(params[:case_note][:case_note_domain_groups_attributes], current_user.id)
           end
           create_bulk_task(params[:task], case_note) if params.key?(:task)
@@ -54,27 +52,6 @@ module Api
         if params[:file_index].present?
           remove_attachment_at_index(params[:file_index].to_i)
           head 204
-        end
-      end
-
-      private
-
-      def case_note_params
-        default_params = permit_case_note_params
-        default_params = params.require(:case_note).permit(:meeting_date, :attendee, :interaction_type, :custom, :note, :custom_assessment_setting_id, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids, attachments: []]) if action_name == 'create'
-        default_params = assign_params_to_case_note_domain_groups_params(default_params) if default_params.dig(:case_note, :domain_group_ids)
-        default_params = default_params.merge(selected_domain_group_ids: params.dig(:case_note, :domain_group_ids).reject(&:blank?))
-        meeting_date   = "#{default_params[:meeting_date]} #{Time.now.strftime("%T %z")}"
-        default_params.merge(meeting_date: meeting_date)
-      end
-
-      def add_more_attachments(new_file, case_note_domain_group_id)
-        if new_file.present?
-          case_note_domain_group = CaseNoteDomainGroup.find(case_note_domain_group_id)
-          files = case_note_domain_group.attachments
-          files += new_file
-          case_note_domain_group.attachments = files
-          case_note_domain_group.save
         end
       end
     end
--- app/serializers/case_note_serializer.rb
+++ app/serializers/case_note_serializer.rb
@@ -1,11 +1,7 @@
 class CaseNoteSerializer < ActiveModel::Serializer
-  attributes :attendee, :meeting_date, :interaction_type, :assessment_id, :id, :case_note_domain_groups, :case_note_domain_group, :selected_domain_group_ids, :created_at, :updated_at
+  attributes :attendee, :meeting_date, :interaction_type, :assessment_id, :id, :case_note_domain_groups, :selected_domain_group_ids, :attachments, :created_at, :updated_at
 
   def case_note_domain_groups
-    ActiveModel::ArraySerializer.new(object.case_note_domain_groups, each_serializer: CaseNoteDomainGroupSerializer)
-  end
-
-  def case_note_domain_group
     ActiveModel::ArraySerializer.new(object.case_note_domain_groups, each_serializer: CaseNoteDomainGroupSerializer)
   end
 end
