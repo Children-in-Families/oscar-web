55f20a75dcef0a606c4aa6319141203bcffd73c4
Vibol Teav <vibolteav@gmail.com>
2023-07-26 15:10:17 +0700

    fix populate_notes for draft assessment

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -83,6 +83,7 @@ class AssessmentsController < AdminController
   def edit
     if @assessment.draft?
       params[:default] ||= "true" if params[:id] != "draft" && @assessment.default?
+      params[:custom_name] ||= @assessment.custom_assessment_setting.custom_assessment_name if @assessment.custom_assessment_setting.present?
 
       @prev_assessment = @client.assessments.last
       @assessment.populate_notes(params[:default], params[:custom_name])
