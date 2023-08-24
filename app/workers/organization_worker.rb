1fb829775891310a59f9d0a5f5010aced9ca8041
Vibol Teav <vibolteav@gmail.com>
2023-08-24 13:31:28 +0700

    remove double worker

--- app/workers/organization_worker.rb
+++ app/workers/organization_worker.rb
@@ -3,7 +3,6 @@ class OrganizationWorker
   sidekiq_options queue: 'priority'
 
   def perform(org_id, referral_source_category_name)
-    # Do something
-    Organization.delay(queue: :priority).seed_generic_data(org_id, referral_source_category_name)
+    Organization.seed_generic_data(org_id, referral_source_category_name)
   end
 end
