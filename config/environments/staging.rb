89f5d913f0f885abcdd7f8e4d7f63681070bda7b
Vibol Teav <vibolteav@gmail.com>
2023-08-25 15:09:45 +0700

    fix loading sidekiq assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = false
+  config.serve_static_files = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
