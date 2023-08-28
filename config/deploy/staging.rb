977514479fdbf857994ba91cb043393907d55304
Vibol Teav <vibolteav@gmail.com>
2023-08-29 04:54:07 +0700

    Revert "update deploy config"
    This reverts commit e14c72fcf1e01ac961b7b436c2ae11a487a0a6ed.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,7 +10,7 @@
 set :stage, 'staging'
 set :rails_env, :staging
 set :appsignal_env, :staging
-set :branch, proc { `git rev-parse --abbrev-ref vibol/staging`.chomp }
+set :branch, proc { `git rev-parse --abbrev-ref staging`.chomp }
 
 server '3.0.131.11', user: 'deployer', roles: %w{app web db}
 
