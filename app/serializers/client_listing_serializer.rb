2a9098e1b4d3d72e357c35646878527e2812e7c6
kirykr <kirymeas@gmail.com>
2023-08-29 15:40:45 +0700

    added updated field to client serializer

--- app/serializers/client_listing_serializer.rb
+++ app/serializers/client_listing_serializer.rb
@@ -1,5 +1,5 @@
 class ClientListingSerializer < ActiveModel::Serializer
-  attributes :id, :name, :gender, :status, :profile, :date_of_birth, :current_family_id, :assessments_count, :case_notes_count, :tasks_count
+  attributes :id, :name, :gender, :status, :profile, :date_of_birth, :current_family_id, :assessments_count, :case_notes_count, :tasks_count, :updated_at
 
   def profile
     object.profile.present? ? { uri: object.profile.url } : {}
