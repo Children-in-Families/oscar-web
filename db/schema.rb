4937b24ab4cb181462251b36bf2f105b42d40e5e
Vibol Teav <vibolteav@gmail.com>
2023-09-05 23:24:01 +0700

    split migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230905154932) do
+ActiveRecord::Schema.define(version: 20230905162246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
