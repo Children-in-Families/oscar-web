d83ea1c48914736fdf41bcfb4b41274ea9f09fdc
Vibol Teav <vibolteav@gmail.com>
2023-09-17 00:10:28 +0700

    remove usage report - organization foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230914030204) do
+ActiveRecord::Schema.define(version: 20230916170803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -195,6 +195,16 @@ ActiveRecord::Schema.define(version: 20230914030204) do
 
   add_index "attachments", ["able_screening_question_id"], name: "index_attachments_on_able_screening_question_id", using: :btree
   add_index "attachments", ["progress_note_id"], name: "index_attachments_on_progress_note_id", using: :btree
+
+  create_table "billable_reports", force: :cascade do |t|
+    t.integer  "organization_id"
+    t.integer  "year"
+    t.integer  "month"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "billable_reports", ["organization_id"], name: "index_billable_reports_on_organization_id", using: :btree
 
   create_table "calendars", force: :cascade do |t|
     t.string   "title"
@@ -2784,6 +2794,8 @@ ActiveRecord::Schema.define(version: 20230914030204) do
     t.jsonb    "cross_referral_cases",              default: {}
     t.jsonb    "cross_referral_to_primero_cases",   default: {}
     t.jsonb    "cross_referral_from_primero_cases", default: {}
+    t.string   "organization_name"
+    t.string   "organization_short_name"
   end
 
   add_index "usage_reports", ["organization_id"], name: "index_usage_reports_on_organization_id", using: :btree
@@ -2863,14 +2875,20 @@ ActiveRecord::Schema.define(version: 20230914030204) do
   add_index "version_associations", ["version_id"], name: "index_version_associations_on_version_id", using: :btree
 
   create_table "versions", force: :cascade do |t|
-    t.string   "item_type",      null: false
-    t.integer  "item_id",        null: false
-    t.string   "event",          null: false
+    t.string   "item_type",          null: false
+    t.integer  "item_id",            null: false
+    t.string   "event",              null: false
     t.string   "whodunnit"
-    t.text     "object"
+    t.text     "old_object"
     t.datetime "created_at"
-    t.text     "object_changes"
+    t.text     "old_object_changes"
     t.integer  "transaction_id"
+    t.integer  "billable_report_id"
+    t.string   "billable_status"
+    t.datetime "accepted_at"
+    t.datetime "billable_at"
+    t.jsonb    "object"
+    t.jsonb    "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
@@ -2915,6 +2933,7 @@ ActiveRecord::Schema.define(version: 20230914030204) do
   add_foreign_key "assessments", "clients", on_delete: :nullify
   add_foreign_key "attachments", "able_screening_questions"
   add_foreign_key "attachments", "progress_notes"
+  add_foreign_key "billable_reports", "organizations"
   add_foreign_key "calendars", "users"
   add_foreign_key "call_necessities", "calls"
   add_foreign_key "call_necessities", "necessities"
@@ -3073,7 +3092,6 @@ ActiveRecord::Schema.define(version: 20230914030204) do
   add_foreign_key "tasks", "goals"
   add_foreign_key "townships", "states"
   add_foreign_key "trackings", "program_streams"
-  add_foreign_key "usage_reports", "organizations"
   add_foreign_key "users", "organizations"
   add_foreign_key "villages", "communes"
   add_foreign_key "visit_clients", "users"
