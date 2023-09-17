9a51be679a4e7b953b0c5596b1ad64836edc5ba0
Vibol Teav <vibolteav@gmail.com>
2023-09-17 11:25:59 +0700

    fix

--- app/workers/billable_accepted_clients_worker.rb
+++ app/workers/billable_accepted_clients_worker.rb
@@ -2,7 +2,7 @@ class BillableAcceptedClientsWorker
   include Sidekiq::Worker
 
   def perform
-    Organization.without_shared.each do |org|
+    Organization.without_shared.where(onboarding_status: 'completed').each do |org|
       Organization.switch_to org.short_name
 
       PaperTrail::Version.where(accepted_at: Date.current.beginning_of_month..Date.current.end_of_month, billable_at: nil).ids do |version_id|
