85489af5926ce7c7ce034465ff05edf552ed6af9
Vibol Teav <vibolteav@gmail.com>
2023-09-18 14:22:00 +0700

    add support for many-many with billable report

--- app/services/client_status_checker.rb
+++ app/services/client_status_checker.rb
@@ -1,16 +1,16 @@
 class ClientStatusChecker < ServiceBase
-  attr_reader :version, :tenant
+  attr_reader :tenant, :billable_report_item
 
-  def initialize(version_id, tenant)
-    Organization.switch_to(tenant)
-    
-    @version = PaperTrail::Version.find(version_id)
+  def initialize(billable_item, tenant)
+    @billable_report_item = BillableReportItem.find(billable_item)
     @tenant = tenant
   end
-
+  
   def call
     Organization.switch_to(tenant)
+
+    version = billable_report_item.version
     # Mark as billable if the status is still accepted after 7 days
-    version.update_columns(billable_at: Time.current) if version.item.status == 'Accepted' && version.created_at <= 7.days.ago
+    billable_report_item.update_columns(billable_at: Time.current) if version.item.status == 'Accepted' && version.created_at <= 7.days.ago
   end
 end
