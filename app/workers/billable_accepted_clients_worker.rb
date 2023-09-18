b603c5ff8e98bd7ce0b22ddb94f3fcc0b448561d
Vibol Teav <vibolteav@gmail.com>
2023-09-18 12:21:21 +0700

    fix billable_accepted_clients_worker

--- app/workers/billable_accepted_clients_worker.rb
+++ app/workers/billable_accepted_clients_worker.rb
@@ -6,11 +6,12 @@ class BillableAcceptedClientsWorker
       begin
         Organization.switch_to org.short_name
         
-        PaperTrail::Version.where(accepted_at: Date.current.beginning_of_month..Date.current.end_of_month, billable_at: nil).where.not(billable_report_id: nil).ids do |version_id|
-          ClientStatusChecker.delay.call(version_id, org.short_name)
+        PaperTrail::Version.where(accepted_at: Date.current.beginning_of_month..Date.current.end_of_month, billable_at: nil).where.not(billable_report_id: nil).ids.each do |version_id|
+          puts "Checking #{org.short_name} version #{version_id}"
+          ClientStatusChecker.call(version_id, org.short_name)
         end
       rescue ActiveRecord::StatementInvalid => e
-        Rails.logger.info "#{org.short_name} not yet properly setup"
+        puts "#{org.short_name} not yet properly setup"
         # continue
       end
     end
