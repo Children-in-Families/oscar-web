58b1bd2376fe6e8eca4e275609aefb0541a66458
Vibol Teav <vibolteav@gmail.com>
2023-10-05 21:28:27 +0700

    temp disable cron to fix crontab on api instance

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,8 @@ set :appsignal_env, :production
 set :branch, proc { `git rev-parse --abbrev-ref stable`.chomp }
 # TODO: Deploy one after another by commentting out one
 
-# OSCaR
-server '52.221.46.112', user: 'deployer', roles: %w{app web db}, ssh_options: {
+# OSCaR genderal
+server '52.221.46.112', user: 'deployer', roles: %w{app web db worker cron}, ssh_options: {
   :keepalive => true,
   :keepalive_interval => 60 #seconds
 }
