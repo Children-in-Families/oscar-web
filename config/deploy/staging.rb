bfdeab7a7cb782bf3512ff3f6a285139316c9e22
Vibol Teav <vibolteav@gmail.com>
2023-10-18 12:27:21 +0700

    update config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,9 +10,9 @@
 set :stage, 'staging'
 set :rails_env, :staging
 set :appsignal_env, :staging
-# set :branch, proc { `git rev-parse --abbrev-ref staging`.chomp }
+set :branch, proc { `git rev-parse --abbrev-ref staging`.chomp }
 
-server '3.0.131.11', user: 'deployer', roles: %w{app web db}
+server '3.0.131.11', user: 'deployer', roles: %w{app web db worker}
 
 # role-based syntax
 # ==================
