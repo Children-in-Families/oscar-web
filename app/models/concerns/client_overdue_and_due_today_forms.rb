7fa10e70e49638776984304c619dc766b40f2bb1
kirykr <kirymeas@gmail.com>
2024-03-28 16:42:44 +0700

    Updated custom forms notification. Inlcuding tracking form

--- app/models/concerns/client_overdue_and_due_today_forms.rb
+++ app/models/concerns/client_overdue_and_due_today_forms.rb
@@ -13,7 +13,7 @@ module ClientOverdueAndDueTodayForms
       editable_clients = clients.where.not(id: CustomFieldProperty.where(custom_formable_type: 'Client', custom_field_id: custom_field_ids).select(:custom_formable_id))
     end
     eligible_clients = active_young_clients(editable_clients, @setting)
-    eligible_clients.each do |client|
+    eligible_clients.joins(:custom_fields).each do |client|
       custom_fields = client.custom_fields.where.not(frequency: '')
       client_active_enrollments = client.client_enrollments.active
 
