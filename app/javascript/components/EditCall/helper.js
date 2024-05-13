cbcbab0e0ca700c73f384f1f63d4b5017c562649
kirykr <kirymeas@gmail.com>
2024-05-13 16:14:15 +0700

    Added indo language to NewClientForm react form

--- app/javascript/components/EditCall/helper.js
+++ app/javascript/components/EditCall/helper.js
@@ -1,21 +1,25 @@
-import en from '../../utils/locales/en.json';
-import km from '../../utils/locales/km.json';
-import my from '../../utils/locales/my.json';
-import T from 'i18n-react'
+import en from "../../utils/locales/en.json";
+import km from "../../utils/locales/km.json";
+import my from "../../utils/locales/my.json";
+import id from "../../utils/locales/id.json";
 
+import T from "i18n-react";
 
 export const setDefaultLanguage = (url) => {
   switch (url) {
     case "km":
-      T.setTexts(km)
+      T.setTexts(km);
       break;
     case "my":
-      T.setTexts(my)
+      T.setTexts(my);
+      break;
+    case "id":
+      T.setTexts(id);
       break;
     default:
-      T.setTexts(en)
+      T.setTexts(en);
       break;
   }
-  
-  return T
-}
+
+  return T;
+};
