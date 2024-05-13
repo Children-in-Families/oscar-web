159af16735aa26bc5b02f80667f267afa6ba67d1
kirykr <kirymeas@gmail.com>
2024-05-13 14:24:46 +0700

    Fixed drop constrain. updated with IF EXISTS

--- db/migrate/20240513051054_alter_client_fk_in_case_notes.rb
+++ db/migrate/20240513051054_alter_client_fk_in_case_notes.rb
@@ -2,7 +2,7 @@ class AlterClientFkInCaseNotes < ActiveRecord::Migration
   def up
     schema = schema_search_path.split(',').first.gsub(/\"/, '')
     execute <<-SQL.squish
-      ALTER TABLE #{schema}."case_notes" DROP CONSTRAINT "fk_rails_a48f1643b7",
+      ALTER TABLE #{schema}."case_notes" DROP CONSTRAINT IF EXISTS "fk_rails_a48f1643b7",
       ADD CONSTRAINT "fk_rails_a48f1643b7" FOREIGN KEY ("client_id") REFERENCES #{schema}."clients"(id) ON DELETE CASCADE;
     SQL
   end
