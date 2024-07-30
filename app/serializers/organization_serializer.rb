b15e69cd78c2bbf45a97713237bc284d4e5d9e7f
kirykr <kirymeas@gmail.com>
2024-07-30 14:37:06 +0700

    Added field demo to organization_serializer

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,3 +1,3 @@
 class OrganizationSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :short_name, :logo, :country
+  attributes :id, :full_name, :short_name, :logo, :country, :demo
 end
