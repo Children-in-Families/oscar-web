b4224feed0ea58d094a4cba394b4e4322a41ca6e
kirykr <kirymeas@gmail.com>
2024-07-30 16:07:05 +0700

    Tried fixing /api/v1/auth/sign_in login route to get request

--- app/controllers/overrides/sessions_controller.rb
+++ app/controllers/overrides/sessions_controller.rb
@@ -1,5 +1,6 @@
 module Overrides
   class SessionsController < DeviseTokenAuth::SessionsController
+    include DeviseTokenAuth::Concerns::SetUserByToken
     before_action :switch_to_public!
 
     def create
@@ -8,8 +9,8 @@ module Overrides
 
     private
 
-      def switch_to_public!
-        Organization.switch_to 'public' if request.subdomain.blank? || request.subdomain.in?(['start', 'interoperability'])
-      end
+    def switch_to_public!
+      Organization.switch_to 'public' if request.subdomain.blank? || request.subdomain.in?(['start', 'interoperability'])
+    end
   end
 end
