47944c1a12607a15f404ef95351c77840b5ce619
kirykr <kirymeas@gmail.com>
2024-08-16 14:49:45 +0700

    updated client profile UX/UI

--- app/classes/risk_assessment_reducer.rb
+++ app/classes/risk_assessment_reducer.rb
@@ -4,7 +4,7 @@ class RiskAssessmentReducer
   def initialize(client, risk_assessment_params, type = 'create')
     @risk_assessment = client.risk_assessment.present? ? client.risk_assessment : client.build_risk_assessment(risk_assessment_params)
     @risk_assessment_params = risk_assessment_params
-    @type = type if risk_assessment_params[:assessment_date].present?
+    @type = type
   end
 
   def store
