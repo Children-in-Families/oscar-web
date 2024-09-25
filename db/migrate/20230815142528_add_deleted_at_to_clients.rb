00f81cb279aa28ae01cf1343bf55f8c0d3d02c29
kirykr <kirymeas@gmail.com>
2024-09-25 15:06:45 +0700

    updated migration added clients deleted_at field

--- db/migrate/20230815142528_add_deleted_at_to_clients.rb
+++ db/migrate/20230815142528_add_deleted_at_to_clients.rb
@@ -1,6 +1,6 @@
 class AddDeletedAtToClients < ActiveRecord::Migration
   def change
-    add_column :clients, :deleted_at, :datetime
-    add_index :clients, :deleted_at
+    add_column :clients, :deleted_at, :datetime unless column_exists?(:clients, :deleted_at)
+    add_index :clients, :deleted_at unless index_exists?(:clients, :deleted_at)
   end
 end
