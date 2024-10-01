a257984e5992c16ad97aedda42227fc71aeec350
Vibol Teav <vibolteav@gmail.com>
2024-10-02 06:35:08 +0700

    Revert "fix saving task progress note"
    This reverts commit 6a52efb2f6637344598f20dd623eb27e1c2e6a96.

--- app/controllers/concerns/case_note_concern.rb
+++ app/controllers/concerns/case_note_concern.rb
@@ -99,11 +99,9 @@ module CaseNoteConcern
         
         next if task_progress_notes_attributes.blank?
 
-        task_progress_notes_attributes.each do |task_progress_note_attr|
-          # Due to auto save sending same data, ignore saving duplicate progess note
-          progress_note = task.task_progress_notes.find_or_initialize_by(task_progress_note_attr)
-          progress_note.save
-        end
+        # Due to auto save sending same data, ignore saving duplicate progess note
+        progress_note = task.task_progress_notes.find_or_initialize_by(task_progress_notes_attributes)
+        progress_note.save
       end
     end
   end
