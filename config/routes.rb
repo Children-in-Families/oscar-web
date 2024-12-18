f99c99e835ed43eeb921a24692dae2d28219056f
kirykr <kirymeas@gmail.com>
2024-12-18 20:43:21 +0700

    Added upload_attachment to api/v1/case_notes  routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'organizations#index'
 
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions', passwords: 'passwords' }
-  
+
   use_doorkeeper do
     skip_controllers :applications, :authorized_applications
   end
@@ -433,6 +433,7 @@ Rails.application.routes.draw do
         get :listing, on: :collection
         resources :assessments
         resources :case_notes, only: [:show, :create, :update, :destroy, :delete_attachment] do
+          post :upload_attachment, on: :member
           delete 'attachments/:file_index', action: :delete_attachment, on: :member
         end
         resources :custom_field_properties, except: :show
