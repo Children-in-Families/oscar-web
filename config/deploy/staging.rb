fee4b2393b8a3ea306d773e444d04820315582be
kirykr <kirymeas@gmail.com>
2025-02-21 15:38:17 +0700

    Changed staging deployer server ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,4 +3,4 @@ set :rails_env, :staging
 set :appsignal_env, :staging
 set :branch, proc { `git rev-parse --abbrev-ref staging`.chomp }
 
-server '3.0.131.11', user: 'deployer', roles: %w{app web db worker cron}
+server '193.203.160.204', user: 'deployer', roles: %w{app web db worker cron}
