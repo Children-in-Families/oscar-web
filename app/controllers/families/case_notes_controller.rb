e5d6b6c1bdea6b0090ac9b002d38b8df2ff92a3c
kirykr <kirymeas@gmail.com>
2025-04-01 16:11:36 +0700

    worked on family case note form

--- app/controllers/families/case_notes_controller.rb
+++ app/controllers/families/case_notes_controller.rb
@@ -30,6 +30,7 @@ class Families::CaseNotesController < ::AdminController
   def create
     @case_note = @family.case_notes.new(case_note_params)
     @case_note.meeting_date = "#{@case_note.meeting_date.strftime('%Y-%m-%d')}, #{Time.now.strftime('%H:%M:%S')}"
+
     if @case_note.save
       add_more_attachments(params[:case_note][:attachments]) if params.dig(:case_note, :attachments)
       @case_note.complete_tasks(params[:case_note][:case_note_domain_groups_attributes], current_user.id) if params.dig(:case_note, :case_note_domain_groups_attributes)
@@ -90,8 +91,8 @@ class Families::CaseNotesController < ::AdminController
   private
 
   def case_note_params
-    default_params = params.require(:case_note).permit(:meeting_date, :attendee, :interaction_type, :custom, :note, :custom_assessment_setting_id, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids])
-    default_params = params.require(:case_note).permit(:meeting_date, :attendee, :interaction_type, :custom, :note, :custom_assessment_setting_id, case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids, attachments: []]) if action_name == 'create'
+    default_params = params.require(:case_note).permit(:meeting_date, :attendee, :interaction_type, :custom, :note, :custom_assessment_setting_id, domain_group_ids: [], case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids])
+    default_params = params.require(:case_note).permit(:meeting_date, :attendee, :interaction_type, :custom, :note, :custom_assessment_setting_id, domain_group_ids: [], case_note_domain_groups_attributes: [:id, :note, :domain_group_id, :task_ids, attachments: []]) if action_name == 'create'
     default_params = assign_params_to_case_note_domain_groups_params(default_params) if default_params.dig(:case_note, :domain_group_ids)
     default_params = default_params.merge(selected_domain_group_ids: params.dig(:case_note, :domain_group_ids).reject(&:blank?))
     meeting_date = "#{default_params[:meeting_date]} #{Time.now.strftime('%T %z')}"
