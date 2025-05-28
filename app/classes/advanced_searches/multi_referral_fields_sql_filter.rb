f13fc3055c49e165bda93ec68254e91ccacf3399
kirykr <kirymeas@gmail.com>
2025-05-28 15:46:46 +0700

    Fixed multi referral report builder error[Edit]

--- app/classes/advanced_searches/multi_referral_fields_sql_filter.rb
+++ app/classes/advanced_searches/multi_referral_fields_sql_filter.rb
@@ -12,30 +12,70 @@ module AdvancedSearches
 
     def get_sql
       sql_string = 'clients.slug IN (?)'
+      field_name = @field.gsub(/\d+$/, '') # Remove trailing digits from field name if present
 
       case @operator
       when 'equal'
-        clients = @clients.joins(:enter_ngos).where("(select enter_ngos.#{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) = ?", @number_of_referral.to_i - 1, @values).distinct
+        clients = find_clients_with_field_value('=', @values)
       when 'not_equal'
-        clients = @clients.joins(:enter_ngos).where("(select enter_ngos.#{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) != ?", @number_of_referral.to_i - 1, @values).distinct
+        clients = find_clients_with_field_value('!=', @values)
       when 'less'
-        clients = @clients.joins(:enter_ngos).where("(select enter_ngos.#{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) < ?", @number_of_referral.to_i - 1, @values).distinct
+        clients = find_clients_with_field_value('<', @values)
+      when 'greater'
+        clients = find_clients_with_field_value('>', @values)
       when 'less_or_equal'
-        clients = @clients.joins(:enter_ngos).where("(select enter_ngos.#{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) <= ?", @number_of_referral.to_i - 1, @values).distinct
+        clients = find_clients_with_field_value('<=', @values)
       when 'greater_or_equal'
-        clients = @clients.joins(:enter_ngos).where("(select enter_ngos.#{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) >= ?", @number_of_referral.to_i - 1, @values).distinct
+        clients = find_clients_with_field_value('>=', @values)
       when 'contains'
-        clients = @clients.joins(:enter_ngos).where("(select enter_ngos.#{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) ILIKE ?", @number_of_referral.to_i - 1, "%#{@values}%").distinct
+        clients = @clients.joins(:enter_ngos)
+                          .where(
+                            "(SELECT enter_ngos.#{field_name} FROM enter_ngos WHERE enter_ngos.client_id = clients.id ORDER BY enter_ngos.id ASC OFFSET ? LIMIT 1) #{operator} ILIKE ?",
+                            @number_of_referral.to_i - 1,
+                            "%#{@values}%"
+                          ).distinct
       when 'not_contains'
-        clients = @clients.joins(:enter_ngos).where("(select enter_ngos.#{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) NOT ILIKE ?", @number_of_referral.to_i - 1, "%#{@values.squish}%").distinct
+        clients = @clients.joins(:enter_ngos)
+                          .where(
+                            "(SELECT enter_ngos.#{field_name} FROM enter_ngos WHERE enter_ngos.client_id = clients.id ORDER BY enter_ngos.id ASC OFFSET ? LIMIT 1) #{operator} NOT ILIKE ?",
+                            @number_of_referral.to_i - 1,
+                            "%#{@values}%"
+                          ).distinct
       when 'is_empty'
-        clients = @clients.joins(:enter_ngos).where("(select #{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) = NULL", @number_of_referral.to_i - 1).distinct
+        clients = @clients.joins(:enter_ngos)
+                          .where(
+                            "(SELECT enter_ngos.#{field_name} FROM enter_ngos WHERE enter_ngos.client_id = clients.id ORDER BY enter_ngos.id ASC OFFSET ? LIMIT 1) = NULL",
+                            @number_of_referral.to_i - 1
+                          ).distinct
       when 'is_not_empty'
-        clients = @clients.joins(:enter_ngos).where("(select #{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) != NULL", @number_of_referral.to_i - 1).distinct
+        clients = @clients.joins(:enter_ngos)
+                          .where(
+                            "(SELECT enter_ngos.#{field_name} FROM enter_ngos WHERE enter_ngos.client_id = clients.id ORDER BY enter_ngos.id ASC OFFSET ? LIMIT 1) != NULL",
+                            @number_of_referral.to_i - 1
+
+                          ).distinct
       when 'between'
-        clients = @clients.joins(:enter_ngos).where("(select enter_ngos.#{@field} from enter_ngos where enter_ngos.client_id = clients.id ORDER BY enter_ngos.id asc OFFSET ?) BETWEEN ? AND ?", @number_of_referral.to_i - 1, @values.first, @values.last).distinct
+        clients = @clients.joins(:enter_ngos)
+                          .where(
+                            "(SELECT enter_ngos.#{field_name} FROM enter_ngos WHERE enter_ngos.client_id = clients.id ORDER BY enter_ngos.id ASC OFFSET ? LIMIT 1) BETWEEN ? AND ?",
+                            @number_of_referral.to_i - 1,
+                            @values.first,
+                            @values.last
+                          ).distinct
       end
       { id: sql_string, values: clients.pluck(:slug) }
+    end
+
+    private
+
+    def find_clients_with_field_value(operator, value)
+      field_name = @field.gsub(/\d+$/, '') # Remove trailing digits from field name if present
+      @clients.joins(:enter_ngos)
+              .where(
+                "(SELECT enter_ngos.#{field_name} FROM enter_ngos WHERE enter_ngos.client_id = clients.id ORDER BY enter_ngos.id ASC OFFSET ? LIMIT 1) #{operator} ?",
+                @number_of_referral.to_i - 1,
+                value
+              ).distinct
     end
   end
 end
